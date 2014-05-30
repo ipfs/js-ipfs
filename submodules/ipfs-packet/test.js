5a2d245c2dc639cde49c19af5cdcd039b431d8d3
Juan Batiz-Benet <juan@benet.ai>
2014-05-30 03:21:06 -0700

    simple try/test

---
+++ submodules/ipfs-packet/test.js
@@ -0,0 +1,31 @@
+var pkt = require('./')
+
+var bufeq = function(a, b) { return a <= b && a >= b; }
+var msgeq = function(a, b) { return bufeq(a.encode(), b.encode()); }
+var tmsgeq = function(a, b) {
+  if (msgeq(a, b))
+    return true
+
+  console.log(a)
+  console.log(b)
+  throw new Error('msgs not equal.')
+}
+
+var dm = pkt.DataMessage(new Buffer('abcd', 'hex'))
+tmsgeq(pkt.DataMessage.decode(dm.encode()), dm)
+
+var nf = pkt.NetworkFrame('1102aaaa', '1102bbbb', dm)
+tmsgeq(pkt.NetworkFrame.decode(nf.encode()), nf)
+
+var tf = pkt.IntegrityFrame(nf, 'sha1')
+tmsgeq(pkt.IntegrityFrame.decode(tf.encode()), tf)
+
+var pf = pkt.PacketFrame(tf)
+tmsgeq(pkt.PacketFrame.decode(pf.encode()), pf)
+
+var p = pf.encode()
+tmsgeq(pkt.peek.network(p), nf)
+tmsgeq(pkt.peek.integrity(p), tf)
+tmsgeq(pkt.peek.packetType(p, pkt.DataMessage), dm)
+
+console.log('ok')
---
+++ submodules/ipfs-packet/try.js
@@ -0,0 +1,31 @@
+var protobuf = require('protobufjs')
+var protobufStream = require('protobufjs-stream')
+var ps = protobuf.loadProtoFile('messages.proto')
+var mapv = require('map-values')
+var protos = ps.result.messages
+var schemas = mapv(protos, protobufStream)
+schemas.DataMessage.encode({payload: new Buffer('abababab', 'hex')})
+var b = schemas.DataMessage.encode({payload: new Buffer('ababababcd', 'hex')})
+schemas.DataMessage.decode(b)
+
+
+var bufeq = function(a, b) { return a <= b && a >= b; }
+var msgeq = function(a, b) { return bufeq(a.encode(), b.encode()); }
+var pkt = require('./')
+
+var dm = pkt.DataMessage(new Buffer('abcd', 'hex'))
+msgeq(pkt.DataMessage.decode(dm.encode()), dm)
+
+var nf = pkt.NetworkFrame('1102aaaa', '1102bbbb', dm)
+msgeq(pkt.NetworkFrame.decode(nf.encode()), nf)
+
+var tf = pkt.IntegrityFrame(nf, 'sha1')
+msgeq(pkt.IntegrityFrame.decode(tf.encode()), tf)
+
+var pf = pkt.PacketFrame(tf)
+msgeq(pkt.PacketFrame.decode(pf.encode()), pf)
+
+var p = pf.encode()
+msgeq(pkt.peek.network(p), nf)
+msgeq(pkt.peek.integrity(p), tf)
+msgeq(pkt.peek.packetType(p, pkt.DataMessage), dm)
