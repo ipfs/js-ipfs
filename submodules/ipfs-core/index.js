093919439ca9ef2de5b6be73746d863f277e749d
Juan Batiz-Benet <juan@benet.ai>
2014-06-01 06:16:45 -0700

    ipfs core begins

---
+++ submodules/ipfs-core/index.js
@@ -0,0 +1,38 @@
+var Peer = require('../ipfs-peer')
+var PeerBook = require('../ipfs-peer-book')
+var ipfsStream = require('../ipfs-message-stream')
+
+module.exports = IPFS
+
+
+// this is the IPFS Core module.
+// It is a hyper-simple hub of activity.
+// It connects the various subsystems together.
+function IPFS(config) {
+  if (!(this instanceof IPFS))
+    return new IPFS(config)
+
+  if (!(config.identity instanceof Peer))
+    throw new Error('config.identity required, must be instance of Peer')
+
+  // the local block database (kv store. recommend ipfs-leveldb)
+  // this.storage = Storage(config.storage)
+
+  // the local node's identity (a Peer instance)
+  this.identity = config.identity
+
+  // the book of other nodes (a hashtable of Peer instances)
+  this.peerbook = PeerBook(config.peerbook)
+
+  // the network message stream
+  this.network = ipfsStream(this.identity)
+
+  // the routing system. recommend ipfs-dht
+  // this.routing = DHT(config.routing)
+
+  // the block exchange + strategy. recommend ipfs-bitswap
+  // this.bitswap = BitSwap(config.bitswap)
+
+  // the name system, resolves paths to hashes.
+  // this.namesys = NameSystem(config.namesys)
+}
---
+++ submodules/ipfs-core/try/ipfs-nc.js
@@ -0,0 +1,40 @@
+var _ = require('underscore')
+var ipfs = require('../.')
+var Peer = require('../../ipfs-peer')
+var Pkt = require('../../ipfs-packet')
+var multihashing = require('multihashing')
+var transDuplex = require('duplex-transform')
+
+function setupPeer(addr) {
+  var p = Peer(multihashing(new Buffer(addr), 'sha1'))
+  p.addresses.push(addr)
+  return p
+}
+
+var peers = _.map(process.argv.slice(2), setupPeer)
+var self = peers.shift()
+
+var node = ipfs({
+  identity: self,
+  peerbook: peers,
+})
+
+function encode(data, enc, next) {
+  // send to all peers
+  for (var p in peers) {
+    this.push({
+      to: peers[p],
+      payload: Pkt.DataMessage(new Buffer(data.toString())),
+    })
+  }
+  next()
+}
+
+function decode(data, enc, next) {
+  data = data.decodePayload().decodePayload() // DataMessage
+  this.push(data.payload)
+  next()
+}
+
+var pktizer = transDuplex.obj(encode, node.network, decode)
+process.stdin.pipe(pktizer).pipe(process.stdout)
