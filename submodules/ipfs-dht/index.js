4ce0b2e30d87ec10ee5a70f2a9e5fa576ee8ef82
Juan Batiz-Benet <juan@benet.ai>
2014-06-04 07:01:33 -0700

    routing table

--- submodules/ipfs-dht/index.js
+++ submodules/ipfs-dht/index.js
@@ -1,5 +1,11 @@
+var _ = require('underscore')
+var multihashing = require('multihashing')
 
 module.exports = DHT
+
+var log = function(s) {
+  console.log('ipfs-dht: ' + s)
+}
 
 // returned when the given key (node id or value key) is not found
 var NotFound = DHT.NotFound = new Error('Not Found')
@@ -18,11 +24,31 @@ var TimeoutExceeded = DHT.TimeoutExceeded = new Error('Timeout Exceeded')
 // A combination of Coral + S/Kademlia
 
 function DHT(config) {
-  this.config = config || {}
+  this.config = _.defaults(config || {}, this.defaultConfig)
+
+  if (!config.network)
+    throw new Error('ipfs-dht requires config.network stream')
+
+  this.network = DHTStream(config.network)
+
+  // locally stored blocks to periodically republish
+  // Note: should probably query live storage.
+  this.republish = { sloppy: [], strict: [] } // [ [ key, val, timeLastPublished ] ]
+}
+
+// default configuration
+DHT.prototype.defaultConfig = {
+
+  // time to wait between republishing intervals (in ms)
+  republishInterval: 2 * 60 * 1000, // 2 minutes. TODO change to 1 hr
+
+  // hash function multihash code
+  multihashFn: 'sha1',
+
 }
 
 // findNode looks for a particular node
-// callback returns a (nodeId, nodeAddr) tuple
+// callback returns a (peer)
 // callback errors: {TimeoutExceeded, NotFound}
 DHT.prototype.findNode = function findNode(nodeId, cb) {
   throw NotImplemented
@@ -30,8 +56,9 @@ DHT.prototype.findNode = function findNode(nodeId, cb) {
 
 // ping tests connectivity + latency to a particular node
 // callback returns connectivity info
-// callback errors: {TimeoutExceeded, NotFound}
-DHT.prototype.pingNode = function ping(nodeId, nodeAddr, cb) {
+// callback errors: {TimeoutExceeded}
+DHT.prototype.pingNode = function ping(peer, timeout, cb) {
+  timeout = timeout || this.config.pingWait
   throw NotImplemented
 }
 
@@ -46,6 +73,7 @@ DHT.prototype.getValue = function getValue(key, cb) {
 // callback returns null
 // callback errors: {NotAvailable}
 DHT.prototype.setValue = function setValue(key, value, cb) {
+  key = this._coerceMultihash(key)
   throw NotImplemented
 }
 
@@ -63,3 +91,46 @@ DHT.prototype.getSloppyValues = function getSloppyValues(key, num, cb) {
 DHT.prototype.setSloppyValue = function setSloppyValue(key, value, cb) {
   throw NotImplemented
 }
+
+
+// _periodicRepublish scans + re-publishes list of published key/values
+DHT.prototype._periodicRepublish = function() {
+  var self = this
+  var now = new Date()
+
+  function shouldRepublish(lastTime) {
+    return (now - lastTime) > self.republishInterval
+  }
+
+  _.map(this.republish.strict, function(item) {
+    log('considering republishing ' + item[0])
+    if (shouldRepublish(item[2]))
+      self.setValue(item[0], item[1])
+  })
+
+  _.map(this.republish.sloppy, function(item) {
+    log('considering republishing ' + item[0])
+    if (shouldRepublish(item[2]))
+      self.setSloppyValue(item[0], item[1])
+  })
+
+  // rerun this callback every 5 seconds.
+  _.delay(this._periodicRepublish.bind(this), 5 * 1000)
+}
+
+// _handleMessage unpacks + figures out what to do with an incoming message
+DHT.prototype._handleMessage = function(msg) {
+
+}
+
+DHT.prototype._coerceMultihash = function coerceMultihash(hash) {
+  if (!(hash instanceof Buffer))
+    hash = new Buffer(hash)
+
+  if (multihashing.multihash.validate(hash) !== false)
+    hash = multihashing(hash, this.config.multihashFn)
+
+  return hash
+}
+
+
