50e1202d39acc0b23d201d7494b366f4770f963f
Juan Batiz-Benet <juan@benet.ai>
2014-06-17 03:05:15 -0700

    core for cli

--- submodules/ipfs-core/index.js
+++ submodules/ipfs-core/index.js
@@ -1,32 +1,40 @@
 var Peer = require('../ipfs-peer')
 var PeerBook = require('../ipfs-peer-book')
 var ipfsStream = require('../ipfs-message-stream')
-var ipfsBlockService = require('./blocks.js')
+var Storage = require('../ipfs-storage')
+var BlockService = require('../ipfs-blocks')
+var PathResolver = require('../ipfs-path-resolver')
 
-module.exports = IPFS
+module.exports = IPFSCore
 
 
 // this is the IPFS Core module.
 // It is a hyper-simple hub of activity.
 // It connects the various subsystems together.
-function IPFS(config) {
-  if (!(this instanceof IPFS))
-    return new IPFS(config)
+function IPFSCore(config) {
+  if (!(this instanceof IPFSCore))
+    return new IPFSCore(config)
+
+  // default to randomized port
+  // if (peer.addresses.length == 0) {
+  //   peer.addresses.push('udp4://0.0.0.0:' + randomPort())
+  // }
+  // console.log('initializing ')
 
   if (!(config.identity instanceof Peer))
     throw new Error('config.identity required, must be instance of Peer')
 
-  // the local block database (kv store. recommend ipfs-leveldb)
-  // this.storage = Storage(config.storage)
+  // the local block database (kv store)
+  this.storage = Storage(config.storage || {})
 
   // the local node's identity (a Peer instance)
-  this.identity = config.identity
+  // this.identity = config.identity
 
   // the book of other nodes (a hashtable of Peer instances)
   this.peerbook = PeerBook(config.peerbook)
 
   // the network message stream
-  this.network = ipfsStream(this.identity)
+  // this.network = ipfsStream(this.identity)
 
   // the routing system. recommend ipfs-dht
   // this.routing = DHT(config.routing)
@@ -34,9 +42,16 @@ function IPFS(config) {
   // the block exchange + strategy. recommend ipfs-bitswap
   // this.bitswap = BitSwap(config.bitswap)
 
-  // the block service, get/add blocks
-  this.blocks = ipfsBlockService(this.storage, this.bitswap)
+  // the block service, get/add blocks.
+  this.blocks = BlockService(this.storage)
+
+  // the path resolution system
+  this.resolver = PathResolver(this.blocks)
 
   // the name system, resolves paths to hashes
   // this.namesys = NameSystem(config.namesys)
+}
+
+function randomPort() {
+  return Math.floor(Math.random() * (50000 - 2000 + 1)) + 2000;
 }
