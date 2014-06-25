92b71b17f70d19b232d92e9ec5597eb853e9f7d4
Juan Batiz-Benet <juan@benet.ai>
2014-06-25 00:10:03 -0700

    handlers function.

--- submodules/ipfs-mount/fuse.js
+++ submodules/ipfs-mount/fuse.js
@@ -33,194 +33,226 @@ function ipfsMount(ipfs, ipfsPath, osPath) {
   this.ipfs = ipfs
   this.osPath = osPath
   this.ipfsPath = ipfsPath
-  this._files = {}
+  this.files = {}
+  this.mount()
+}
 
+ipfsMount.prototype.mount = function(cb) {
+  var osPath = this.osPath
+  var handlers = this._handlers()
   umount(osPath, function() {
     mkdirp(osPath, function() {
-      f4js.start(osPath, self, false, [])
+      f4js.start(osPath, handlers, false, [])
     })
   })
 }
 
-ipfsMount.prototype._getPath = function(path, cb) {
-  this.ipfs.resolver.resolve(path, function(err, obj) {
-    if (err) {
-      if (err != ipfs.resolver.errors.NotFoundError)
-        console.log('ipfs.resolver error: ' + err)
-
-      console.log('ipfs.resolver not found: ' + path)
-      return cb(ENOENT)
-    }
-
-    console.log('ipfs.resolver found: ' + path)
-    cb(null, obj)
-  })
-}
-
-ipfsMount.prototype._umount = function(cb) {
+ipfsMount.prototype.umount = function(cb) {
   umount(this.osPath, cb)
 }
 
-ipfsMount.prototype._terminate = function() {
+ipfsMount.prototype.terminate = function() {
   var args = [this.osPath, '' + process.pid]
   proc.fork(path.join(__dirname, 'destroy.js'), args)
 }
 
-ipfsMount.prototype.getattr = function(path, cb) {
-  this._getPath(path, function(err, obj) {
-    if (err === ENOENT) return cb(ENOENT)
-    if (err) throw err
+ipfsMount.prototype._handlers = function() {
+  var self = this
+  var handlers = {}
 
-    var stat = {}
+  function resolvePath(path, cb) {
+    self.ipfs.resolver.resolve(path, function(err, obj) {
+      if (err) {
+        if (err != ipfs.resolver.errors.NotFoundError)
+          console.log('ipfs.resolver error: ' + err)
 
-    // for now assume that if it has links, it's a dir.
-    // will need to do something smarter for lists, etc.
-    var links = obj.links()
-    if (links.length > 0) {
-      stat.size = 4096 // why 4096? ask mafintosh
-      stat.mode = 040555 // +rx
-    } else {
-      stat.size = obj.data().length // todo add a .dataSize() to obj
-      stat.mode = 0100444 // +r
-    }
-    console.log(stat)
-    cb(0, stat)
-  })
-}
+        console.log('ipfs.resolver not found: ' + path)
+        return cb(ENOENT)
+      }
 
-var files = {}
-
-ipfsMount.prototype.open = function(path, flags, cb) {
-  var files = this._files
-  this._getPath(path, function(err, obj) {
-    if (err === ENOENT) return cb(ENOENT)
-    if (err) throw err
-
-    var fds = files[path] = files[path] || []
-    var fd = fds.indexOf(null) // reuse fd
-    if (fd === -1) fd = fds.length // alloc new fd
-
-    fds[fd] = {offset:0}
-    cb(0, fd)
-  })
-}
-
-ipfsMount.prototype.release = function(path, handle, cb) {
-  var fds = this._files[path] || []
-  var fd = fds[handle]
-  // if (fd && fd.obj) delete fd.obj
-  fds[handle] = null
-  cb(0)
-}
-
-ipfsMount.prototype.readdir = function(path, cb) {
-  if (path ===  '/') { // root? then list local files.
-    this.ipfs.storage.list('/', function(err, list) {
-      var names = []
-      list
-        .on('data', function(name) {
-          names.push(name)
-        })
-        .on('error', function(err) {
-          console.log('ipfs mount error: ' + err) // todo fix
-          cb(ENOENT)
-        })
-        .on('end', function() {
-          if (names.length > 0)
-            cb(0, names)
-          else
-            cb(ENOENT)
-        })
-      // list.unpause()
+      console.log('ipfs.resolver found: ' + path)
+      cb(null, obj)
     })
-    return
   }
 
-  this._getPath(path, function(err, obj) {
-    if (err === ENOENT) return cb(ENOENT)
-    if (err) throw err
+  function getPath(path, cb) {
+    if (path == '/')
+      return cb(ENOENT)
 
-    var files = map(obj.links(), function(link) {
-      return link.name || link.hash
+    console.log('getPath ' + path)
+    try {
+      resolvePath(path, cb)
+    } catch(e) {
+      console.log('exception thrown while resolving path.')
+      console.log(e.stack)
+      console.trace()
+      cb(ENOENT)
+    }
+  }
+
+  handlers.getattr = function(path, cb) {
+    if (path == '/')
+      return cb(0, { size: 4096, mode: 040555 })
+
+    getPath(path, function(err, obj) {
+      if (err === ENOENT) return cb(ENOENT)
+      if (err) throw err
+      if (!obj) throw new Error('expected object')
+
+      var stat = {}
+
+      // for now assume that if it has links, it's a dir.
+      // will need to do something smarter for lists, etc.
+      var links = obj.links()
+      if (links.length > 0) {
+        stat.size = 4096 // why 4096? ask mafintosh
+        stat.mode = 040555 // +rx
+      } else {
+        stat.size = obj.data().length // todo add a .dataSize() to obj
+        stat.mode = 0100444 // +r
+      }
+      console.log(stat)
+      cb(0, stat)
     })
+  }
 
-    if (!files.length) return cb(ENOENT)
-    cb(0, files)
-  })
-}
+  handlers.open = function(path, flags, cb) {
+    var files = self.files
+    getPath(path, function(err, obj) {
+      if (err === ENOENT) return cb(ENOENT)
+      if (err) throw err
+      if (!obj) throw new Error('expected object')
 
-ipfsMount.prototype.read = function(path, offset, len, buf, handle, cb) {
-  var files = this._files
-  this._getPath(path, function(err, obj) {
-    if (err === ENOENT) return cb(ENOENT)
-    if (err) throw err
+      var fds = files[path] = files[path] || []
+      var fd = fds.indexOf(null) // reuse fd
+      if (fd === -1) fd = fds.length // alloc new fd
 
-    var fds = files[path] || []
+      fds[fd] = {offset:0}
+      cb(0, fd)
+    })
+  }
+
+  handlers.release = function(path, handle, cb) {
+    var fds = self.files[path] || []
     var fd = fds[handle]
+    // if (fd && fd.obj) delete fd.obj
+    fds[handle] = null
+    cb(0)
+  }
 
-    if (!fd) return cb(ENOENT)
+  handlers.readdir = function(path, cb) {
+    if (path ===  '/') { // root? then list local files.
+      self.ipfs.storage.list('/', function(err, list) {
+        var names = []
+        list
+          .on('data', function(name) {
+            names.push(name)
+          })
+          .on('error', function(err) {
+            console.log('ipfs mount error: ' + err) // todo fix
+            cb(ENOENT)
+          })
+          .on('end', function() {
+            if (names.length > 0)
+              cb(0, names)
+            else
+              cb(ENOENT)
+          })
+        // list.unpause()
+      })
+      return
+    }
 
-    // will need streaming logic here for lists, etc.
-    // for now, just dump out the block data.
+    getPath(path, function(err, obj) {
+      if (err === ENOENT) return cb(ENOENT)
+      if (err) throw err
+      if (!obj) throw new Error('expected object')
 
-    var data = obj.data()
-    if (len + offset > data.length)
-      len = data.length - offset
+      var files = map(obj.links(), function(link) {
+        return link.name || link.hash
+      })
 
-    var slice = data.slice(offset, len)
-    slice.copy(buf)
-    cb(result)
-  })
-};
+      if (!files.length) return cb(ENOENT)
+      cb(0, files)
+    })
+  }
 
-ipfsMount.prototype.write = function(path, offset, len, buf, handle, cb) {
-  cb(EPERM)
-}
+  handlers.read = function(path, offset, len, buf, handle, cb) {
+    var files = self.files
+    getPath(path, function(err, obj) {
+      if (err === ENOENT) return cb(ENOENT)
+      if (err) throw err
+      if (!obj) throw new Error('expected object')
 
-ipfsMount.prototype.unlink = function(path, cb) {
-  cb(EPERM)
-}
+      var fds = files[path] || []
+      var fd = fds[handle]
 
-ipfsMount.prototype.rename = function(src, dst, cb) {
-  cb(EPERM)
-}
+      if (!fd) return cb(ENOENT)
 
-ipfsMount.prototype.mkdir = function(path, mode, cb) {
-  cb(EPERM)
-}
+      // will need streaming logic here for lists, etc.
+      // for now, just dump out the block data.
 
-ipfsMount.prototype.rmdir = function(path, cb) {
-  cb(EPERM)
-}
+      var data = obj.data()
+      if (len + offset > data.length)
+        len = data.length - offset
 
-ipfsMount.prototype.create = function(path, mode, cb) {
-  cb(EPERM)
-}
+      var slice = data.slice(offset, len)
+      slice.copy(buf)
+      cb(result)
+    })
+  }
 
-ipfsMount.prototype.getxattr = function(path, cb) {
-  cb(EPERM)
-}
+  handlers.write = function(path, offset, len, buf, handle, cb) {
+    cb(EPERM)
+  }
 
-ipfsMount.prototype.setxattr = function(path, name, value, size, a, b, cb) {
-  cb(0)
-}
+  handlers.unlink = function(path, cb) {
+    cb(EPERM)
+  }
 
-ipfsMount.prototype.statfs = function(cb) {
-  cb(0, {
-    bsize: 1000000,
-    frsize: 1000000,
-    blocks: 1000000,
-    bfree: 1000000,
-    bavail: 1000000,
-    files: 1000000,
-    ffree: 1000000,
-    favail: 1000000,
-    fsid: 1000000,
-    flag: 1000000,
-    namemax: 1000000
-  })
-}
+  handlers.rename = function(src, dst, cb) {
+    cb(EPERM)
+  }
 
-ipfsMount.prototype.destroy = function(cb) {
-  cb()
+  handlers.mkdir = function(path, mode, cb) {
+    cb(EPERM)
+  }
+
+  handlers.rmdir = function(path, cb) {
+    cb(EPERM)
+  }
+
+  handlers.create = function(path, mode, cb) {
+    cb(EPERM)
+  }
+
+  handlers.getxattr = function(path, cb) {
+    cb(EPERM)
+  }
+
+  handlers.setxattr = function(path, name, value, size, a, b, cb) {
+    cb(0)
+  }
+
+  handlers.statfs = function(cb) {
+    cb(0, {
+      bsize: 1000000,
+      frsize: 1000000,
+      blocks: 1000000,
+      bfree: 1000000,
+      bavail: 1000000,
+      files: 1000000,
+      ffree: 1000000,
+      favail: 1000000,
+      fsid: 1000000,
+      flag: 1000000,
+      namemax: 1000000
+    })
+  }
+
+  handlers.destroy = function(cb) {
+    cb()
+  }
+
+  return handlers
 }
