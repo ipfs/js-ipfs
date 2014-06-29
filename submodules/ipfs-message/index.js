225839e5425dff81eb9e304e0d81766739fccd0d
Juan Batiz-Benet <juan@benet.ai>
2014-06-29 02:16:25 -0700

    v2 of messages

---
+++ submodules/ipfs-message/index.js
@@ -0,0 +1,38 @@
+var fs = require('fs')
+var msgproto = require('msgproto')
+var Message = msgproto.Message
+
+module.exports = ipfsMessage
+
+function ipfsMessage() {
+  if (!(this instanceof Message))
+    return new ipfsMessage()
+
+  Message.call(this)
+
+  this.source = new Buffer(0)
+  this.destination = new Buffer(0)
+  this.payload = []
+}
+
+Message.inherits(ipfsMessage, Message)
+
+ipfsMessage.prototype.getEncodedData = function() {
+  // no super because Message doesn't do anything.
+  return {
+    source: this.source,
+    destination: this.destination,
+    payload: this.payload,
+  }
+}
+
+ipfsMessage.prototype.setDecodedData = function(data) {
+  // no super because Message doesn't do anything.
+  this.payload = data.payload
+  this.source = data.source
+  this.destination = data.destination
+}
+
+var src = fs.readFileSync(__dirname + '/message.proto', 'utf-8')
+var protos = msgproto.ProtobufCodec.fromProtoSrc(src)
+ipfsMessage.codec = protos.Message
