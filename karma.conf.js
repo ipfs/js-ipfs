f94c628846b8c37f6599df7b1ad3c3e9c7372410
dignifiedquire <dignifiedquire@gmail.com>
2015-10-26 19:12:14 +0100

    Do not require credentials in debug mode

--- karma.conf.js
+++ karma.conf.js
@@ -1,7 +1,9 @@
 module.exports = function (config) {
-  if (!process.env.SAUCE_USERNAME || !process.env.SAUCE_ACCESS_KEY) {
-    console.log('Make sure the SAUCE_USERNAME and SAUCE_ACCESS_KEY environment variables are set.')
-    process.exit(1)
+  if (!process.env.DEBUG) {
+    if (!process.env.SAUCE_USERNAME || !process.env.SAUCE_ACCESS_KEY) {
+      console.log('Make sure the SAUCE_USERNAME and SAUCE_ACCESS_KEY environment variables are set.')
+      process.exit(1)
+    }
   }
 
   // Browsers to run on Sauce Labs
