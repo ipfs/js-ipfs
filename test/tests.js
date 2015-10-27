0986ce7f7b99f93c7a2e7c91f1077aad6654a08d
David Dias <daviddias.p@gmail.com>
2015-10-27 04:54:00 +0000

    pin add, list and remove tests

--- test/tests.js
+++ test/tests.js
@@ -494,9 +494,42 @@ describe('IPFS Node.js API wrapper tests', function () {
   })
 
   describe('.pin', function () {
-    it('.pin.add')
-    it('.pin.remove')
-    it('.pin.list')
+    it('.pin.add', function (done) {
+      apiClients['b'].pin.add('Qma4hjFTnCasJ8PVp3mZbZK5g2vGDT4LByLJ7m8ciyRFZP', function (err, res) {
+        if (err) {
+          throw err
+        }
+        assert.equal(res.Pinned[0], 'Qma4hjFTnCasJ8PVp3mZbZK5g2vGDT4LByLJ7m8ciyRFZP')
+        done()
+      })
+    })
+
+    it('.pin.list', function (done) {
+      apiClients['b'].pin.list(function (err, res) {
+        if (err) {
+          throw err
+        }
+        assert(res)
+        done()
+      })
+    })
+
+    it('.pin.remove', function (done) {
+      apiClients['b'].pin.remove('Qma4hjFTnCasJ8PVp3mZbZK5g2vGDT4LByLJ7m8ciyRFZP', {recursive: true}, function (err, res) {
+        if (err) {
+          throw err
+        }
+        assert(res)
+        apiClients['b'].pin.list(function (err, res) {
+          if (err) {
+            throw err
+          }
+          assert(res)
+          assert.equal(Object.keys(res.Keys).length, 0)
+          done()
+        })
+      })
+    })
   })
 
   describe('.gateway', function () {
@@ -585,7 +618,7 @@ describe('IPFS Node.js API wrapper tests', function () {
         if (err) {
           throw err
         }
-        assert.equal(res)
+        assert(res)
         done()
       })
     })
