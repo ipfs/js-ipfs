bbcdd5be9a96b2b6d66ad5c9d450dbb8a02a133d
Connor Keenan <ckeenan89@gmail.com>
2015-11-21 15:09:57 -0600

    new test/constructor.js parameter tests

---
+++ test/constructor.js
@@ -0,0 +1,51 @@
+'use strict'
+
+const ipfsAPI = require('../src/index.js')
+const assert = require('assert')
+
+describe('ipfsAPI constructor tests', () => {
+  describe('parameter permuations', () => {
+    const apiAddrs = require('./tmp-disposable-nodes-addrs.json')
+    const apiAddr = apiAddrs.a.split('/')
+
+    function clientWorks (client, done) {
+      client.id((err, res) => {
+        if (err) throw err
+        const id = res
+        assert(id.ID)
+        assert(id.PublicKey)
+        done()
+      })
+    }
+
+    it('opts', (done) => {
+      clientWorks(ipfsAPI({
+        host: apiAddr[2],
+        port: apiAddr[4],
+        protocol: 'http'
+      }), done)
+    })
+
+    it('mutliaddr, opts', (done) => {
+      clientWorks(ipfsAPI(
+        apiAddrs.a,
+        {protocol: 'http'}
+      ), done)
+    })
+
+    it('host, port', (done) => {
+      clientWorks(ipfsAPI(
+        apiAddr[2],
+        apiAddr[4]
+      ), done)
+    })
+
+    it('host, port, opts', (done) => {
+      clientWorks(ipfsAPI(
+        apiAddr[2],
+        apiAddr[4],
+        {protocol: 'http'}
+      ), done)
+    })
+  })
+})
