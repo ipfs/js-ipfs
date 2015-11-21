3f841b9dacb77fd2118b7035c2f14eea4c0a11ba
dignifiedquire <dignifiedquire@gmail.com>
2015-11-21 22:48:35 +0100

    fix(config): send correct user-agent

--- src/config.js
+++ src/config.js
@@ -5,7 +5,7 @@ const pkg = require('../package.json')
 exports = module.exports = () => {
   return {
     'api-path': '/api/v0/',
-    'user-agent': `/node-$pkg.name}/${pkg.version}/`,
+    'user-agent': `/node-${pkg.name}/${pkg.version}/`,
     'host': 'localhost',
     'port': '5001'
   }
