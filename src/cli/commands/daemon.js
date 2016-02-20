9a1cc56bc1279ca3a4386f7c8dc76a62066d36c7
David Dias <daviddias.p@gmail.com>
2016-02-20 15:46:28 +0000

    daemon start and shutdown

--- src/cli/commands/daemon.js
+++ src/cli/commands/daemon.js
@@ -4,15 +4,28 @@ const Command = require('ronin').Command
 const httpAPI = require('../../http-api')
 const debug = require('debug')
 const log = debug('cli:daemon')
-log.error = debug('cli:damon:error')
+log.error = debug('cli:daemon:error')
 
 module.exports = Command.extend({
   desc: 'Start a long-running daemon process',
 
   run: name => {
+    console.log('Initializing daemon...')
     httpAPI.start((err) => {
-      if (err) { return log.error(err) }
-      log('daemon started')
+      if (err) {
+        return log.error(err)
+      }
+      console.log('Daemon is ready')
+    })
+
+    process.on('SIGINT', () => {
+      console.log('Received interrupt signal, shutting down..')
+      httpAPI.stop((err) => {
+        if (err) {
+          return log.error(err)
+        }
+        process.exit(0)
+      })
     })
   }
 })
