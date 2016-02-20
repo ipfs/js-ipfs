39f518c61f0adcdfc763218970464bedfefe4bc8
David Dias <daviddias.p@gmail.com>
2016-02-20 16:08:53 +0000

    one conn for each (api and gateway)

--- src/http-api/index.js
+++ src/http-api/index.js
@@ -11,27 +11,41 @@ exports = module.exports
 exports.start = callback => {
   // start IPFS and exports.ipfs = new IPFS()
 
-  exports.ipfs = new IPFS()
+  const ipfs = exports.ipfs = new IPFS()
 
-  var server = exports.server = new Hapi.Server({
-    connections: {
-      routes: {
-        cors: true
-      }
+  ipfs.config.show((err, config) => {
+    if (err) {
+      return callback(err)
     }
-  })
 
-  server.connection({
-    port: 9001
-  })
+    // TODO: set up cors correctly, following config
+    var server = exports.server = new Hapi.Server({
+      connections: {
+        routes: {
+          cors: true
+        }
+      }
+    })
+    const api = config.Addresses.API.split('/')
+    const gateway = config.Addresses.Gateway.split('/')
 
-  // load routes
-  require('./routes')
+    // select which connection with server.select(<label>) to add routes
+    server.connection({ host: api[2], port: api[4], labels: 'API' })
+    server.connection({ host: gateway[2], port: gateway[4], labels: 'Gateway' })
 
-  server.start(err => {
-    if (err) { return callback(err) }
-    log('server started: ' + server.info.uri)
-    callback()
+    // load routes
+    require('./routes')
+
+    server.start((err) => {
+      if (err) {
+        return callback(err)
+      }
+      const api = server.select('API')
+      const gateway = server.select('Gateway')
+      console.log('API is listening on: ' + api.info.uri)
+      console.log('Gateway (readonly) is listening on: ' + gateway.info.uri)
+      callback()
+    })
   })
 }
 
