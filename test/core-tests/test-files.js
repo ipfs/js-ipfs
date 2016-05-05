0292c1fd94172825a74c4746651f78281ab9d02f
nginnever <ginneversource@gmail.com>
2016-05-05 06:38:05 -0700

    add, cat, get core + cli offline

---
+++ test/core-tests/test-files.js
@@ -0,0 +1,64 @@
+/* eslint-env mocha */
+'use strict'
+
+const bl = require('bl')
+const expect = require('chai').expect
+const Readable = require('stream').Readable
+const bs58 = require('bs58')
+
+const IPFS = require('../../src/core')
+
+describe('files', () => {
+  var ipfs
+
+  before((done) => {
+    ipfs = new IPFS(require('./repo-path'))
+    ipfs.load(done)
+  })
+
+  it('add', (done) => {
+    const buffered = new Buffer('some data')
+    var rs = new Readable()
+    rs.push(buffered)
+    rs.push(null)
+    var arr = []
+    const filePair = {path: 'data.txt', stream: rs}
+    arr.push(filePair)
+    ipfs.files.add(arr, (err, res) => {
+      expect(err).to.not.exist
+      expect(res[0].path).to.equal('data.txt')
+      expect(res[0].size).to.equal(17)
+      expect(bs58.encode(res[0].multihash).toString()).to.equal('QmVv4Wz46JaZJeH5PMV4LGbRiiMKEmszPYY3g6fjGnVXBS')
+      done()
+    })
+  })
+
+  it('cat', (done) => {
+    const hash = 'QmT78zSuBmuS4z925WZfrqQ1qHaJ56DQaTfyMUF7F8ff5o'
+    ipfs.files.cat(hash, (err, res) => {
+      expect(err).to.not.exist
+      res.on('file', (data) => {
+        data.stream.pipe(bl((err, bldata) => {
+          expect(err).to.not.exist
+          expect(bldata.toString()).to.equal('hello world\n')
+          done()
+        }))
+      })
+    })
+  })
+
+  it('get', (done) => {
+    // TODO create non-trival get test
+    const hash = 'QmT78zSuBmuS4z925WZfrqQ1qHaJ56DQaTfyMUF7F8ff5o'
+    ipfs.files.get(hash, (err, res) => {
+      expect(err).to.not.exist
+      res.on('file', (data) => {
+        data.stream.pipe(bl((err, bldata) => {
+          expect(err).to.not.exist
+          expect(bldata.toString()).to.equal('hello world\n')
+          done()
+        }))
+      })
+    })
+  })
+})
