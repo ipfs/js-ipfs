7392c65037282427ba7d979791a886be5ba8d96d
Gavin McDermott <gavinmcdermott@gmail.com>
2016-05-11 13:09:31 -0700

    extend test coverage on the files api

--- src/api/files.js
+++ src/api/files.js
@@ -9,7 +9,7 @@ module.exports = (send) => {
     mkdir: argCommand(send, 'files/mkdir'),
     stat: argCommand(send, 'files/stat'),
     rm: function (path, opts, cb) {
-      if (typeof opts === 'function' && !cb) {
+      if (typeof opts === 'function' && cb === undefined) {
         cb = opts
         opts = {}
       }
@@ -17,11 +17,10 @@ module.exports = (send) => {
     },
     read: argCommand(send, 'files/read'),
     write: function (pathDst, files, opts, cb) {
-      if (typeof (opts) === 'function' && cb === undefined) {
+      if (typeof opts === 'function' && cb === undefined) {
         cb = opts
         opts = {}
       }
-
       return send('files/write', pathDst, opts, files, cb)
     },
     mv: argCommand(send, 'files/mv')
