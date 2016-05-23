d704742c35a34a418753289af34343cc870fcb21
Stephen Whitmore <stephen.whitmore@gmail.com>
2016-05-23 12:16:57 -0700

    Make ipfs.files.add return DAGNodes.

--- src/core/ipfs/files.js
+++ src/core/ipfs/files.js
@@ -3,38 +3,68 @@
 const Importer = require('ipfs-unixfs-engine').importer
 const Exporter = require('ipfs-unixfs-engine').exporter
 const UnixFS = require('ipfs-unixfs')
+const bs58 = require('bs58')
+const through = require('through2')
+const isStream = require('isstream')
+const promisify = require('promisify-es6')
 
 module.exports = function files (self) {
   return {
-    add: (arr, callback) => {
-      if (typeof arr === 'function') {
-        callback = arr
-        arr = undefined
-      }
-      if (callback === undefined) {
-        callback = function noop () {}
-      }
-      if (arr === undefined) {
+    createAddStream: promisify((callback) => {
+      // TODO: wip
+      if (data === undefined) {
         return new Importer(self._dagS)
+      }
+    }),
+
+    add: promisify((data, callback) => {
+      // Buffer input
+      if (Buffer.isBuffer(data)) {
+        data = [{
+          path: '',
+          content: data
+        }]
+      }
+      // Readable stream input
+      if (isStream.isReadable(data)) {
+        data = [{
+          path: '',
+          content: data
+        }]
+      }
+      if (!callback || typeof callback !== 'function') {
+        callback = function oop () {}
+      }
+      if (!Array.isArray(data)) {
+        return callback(new Error('"data" must be an array of { path: string, content: Buffer|Readable } or Buffer or Readable'))
       }
 
       const i = new Importer(self._dagS)
       const res = []
 
-      i.on('data', (info) => {
-        res.push(info)
-      })
-
-      i.once('end', () => {
+      // Transform file info tuples to DAGNodes
+      i.pipe(through.obj(function transform (info, enc, next) {
+        const mh = bs58.encode(info.multihash).toString()
+        self._dagS.get(mh, (err, node) => {
+          if (err) return callback(err)
+          var obj = {
+            path: info.path || mh,
+            node: node
+          }
+          res.push(obj)
+          next()
+        })
+      }, function end (done) {
         callback(null, res)
-      })
+      }))
 
-      arr.forEach((tuple) => {
+      data.forEach((tuple) => {
         i.write(tuple)
       })
 
       i.end()
-    },
+    }),
+
     cat: (hash, callback) => {
       self._dagS.get(hash, (err, fetchedNode) => {
         if (err) {
