a4a9e3f4740d684144eeb1f785a0c7f8b3f98aba
David Dias <daviddias.p@gmail.com>
2016-07-02 17:09:41 +0100

    fix files.add promises

--- src/api/add.js
+++ src/api/add.js
@@ -2,24 +2,20 @@
 
 const isStream = require('isstream')
 const addToDagNodesTransform = require('../add-to-dagnode-transform')
+const promisify = require('promisify-es6')
 
 module.exports = (send) => {
-  return function add (files, opts, cb) {
-    if (typeof (opts) === 'function' && cb === undefined) {
-      cb = opts
-      opts = {}
-    }
-
+  return promisify((files, callback) => {
     const good = Buffer.isBuffer(files) ||
                isStream.isReadable(files) ||
                Array.isArray(files)
 
     if (!good) {
-      return cb(new Error('"files" must be a buffer, readable stream, or array of objects'))
+      callback(new Error('"files" must be a buffer, readable stream, or array of objects'))
     }
 
-    var sendWithTransform = send.withTransform(addToDagNodesTransform)
+    const sendWithTransform = send.withTransform(addToDagNodesTransform)
 
-    return sendWithTransform('add', null, opts, files, cb)
-  }
+    sendWithTransform('add', null, {}, files, callback)
+  })
 }
