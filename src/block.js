235c02f179856dea9a5f83bfa2a48a45815d833f
David Dias <daviddias.p@gmail.com>
2016-08-24 11:02:48 +0100

    feat(block): correct tests, update interface

--- src/block.js
+++ src/block.js
@@ -4,6 +4,8 @@
 'use strict'
 
 const expect = require('chai').expect
+const Block = require('ipfs-block')
+const multihash = require('multihashes')
 
 module.exports = (common) => {
   describe('.block', () => {
@@ -30,13 +32,26 @@ module.exports = (common) => {
     })
 
     describe('callback API', () => {
-      it('.put', (done) => {
+      it('.put a buffer', (done) => {
         const expectedHash = 'QmPv52ekjS75L4JmHpXVeuJ5uX2ecSfSZo88NSyxwA3rAQ'
         const blob = Buffer('blorb')
 
-        ipfs.block.put(blob, (err, res) => {
+        ipfs.block.put(blob, (err, block) => {
           expect(err).to.not.exist
-          expect(res).to.have.a.property('Key', expectedHash)
+          expect(block.key).to.eql(multihash.fromB58String(expectedHash))
+          expect(block).to.have.a.property('data', blob)
+          done()
+        })
+      })
+
+      it('.put a block', (done) => {
+        const expectedHash = 'QmPv52ekjS75L4JmHpXVeuJ5uX2ecSfSZo88NSyxwA3rAQ'
+        const blob = new Block(new Buffer('blorb'))
+
+        ipfs.block.put(blob, (err, block) => {
+          expect(err).to.not.exist
+          expect(block.key).to.eql(multihash.fromB58String(expectedHash))
+          expect(block.data).to.eql(new Buffer('blorb'))
           done()
         })
       })
@@ -52,30 +67,26 @@ module.exports = (common) => {
       it('block.get', (done) => {
         const hash = 'QmPv52ekjS75L4JmHpXVeuJ5uX2ecSfSZo88NSyxwA3rAQ'
 
-        ipfs.block.get(hash, (err, res) => {
+        ipfs.block.get(hash, (err, block) => {
           expect(err).to.not.exist
-
-          // TODO review this
-          let buf = ''
-          res
-            .on('data', function (data) { buf += data })
-            .on('end', function () {
-              expect(buf).to.be.equal('blorb')
-              done()
-            })
+          expect(block.key).to.eql(multihash.fromB58String(hash))
+          expect(block.data).to.eql(new Buffer('blorb'))
+          done()
         })
       })
 
       it('block.stat', (done) => {
         const hash = 'QmPv52ekjS75L4JmHpXVeuJ5uX2ecSfSZo88NSyxwA3rAQ'
 
-        ipfs.block.stat(hash, (err, res) => {
+        ipfs.block.stat(hash, (err, stats) => {
           expect(err).to.not.exist
-          expect(res).to.have.property('Key')
-          expect(res).to.have.property('Size')
+          expect(stats).to.have.property('key')
+          expect(stats).to.have.property('size')
           done()
         })
       })
+
+      it.skip('block.rm', (done) => {}) // TODO once block.rm is shipped in go-ipfs
     })
 
     describe('promise API', () => {
