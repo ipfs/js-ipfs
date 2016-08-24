5e6387d4586a42e673c4c14d156a282c78aae1ac
David Dias <daviddias.p@gmail.com>
2016-08-24 16:49:16 +0100

    feat(block-core): add compliance with interface-ipfs-core on block-API

--- src/core/ipfs/block.js
+++ src/core/ipfs/block.js
@@ -1,24 +1,47 @@
 'use strict'
 
+const Block = require('ipfs-block')
+const multihash = require('multihashes')
+
 module.exports = function block (self) {
   return {
-    get: (multihash, callback) => {
-      self._blockS.getBlock(multihash, callback)
+    get: (hash, callback) => {
+      if (typeof hash === 'string') {
+        hash = multihash.fromB58String(hash)
+      }
+      self._blockS.getBlock(hash, callback)
     },
     put: (block, callback) => {
-      self._blockS.addBlock(block, callback)
+      if (Array.isArray(block)) {
+        return callback(new Error('Array is not supported'))
+      }
+      if (Buffer.isBuffer(block)) {
+        block = new Block(block)
+      }
+
+      self._blockS.addBlock(block, (err) => {
+        callback(err, block)
+      })
     },
-    del: (multihash, callback) => {
-      self._blockS.deleteBlock(multihash, callback)
+    del: (hash, callback) => {
+      if (typeof hash === 'string') {
+        hash = multihash.fromB58String(hash)
+      }
+
+      self._blockS.deleteBlock(hash, callback)
     },
-    stat: (multihash, callback) => {
-      self._blockS.getBlock(multihash, (err, block) => {
+    stat: (hash, callback) => {
+      if (typeof hash === 'string') {
+        hash = multihash.fromB58String(hash)
+      }
+
+      self._blockS.getBlock(hash, (err, block) => {
         if (err) {
           return callback(err)
         }
         callback(null, {
-          Key: multihash,
-          Size: block.data.length
+          key: hash,
+          size: block.data.length
         })
       })
     }
--- test/core/both/test-block.js
+++ test/core/both/test-block.js
@@ -1,77 +1,20 @@
 /* eslint-env mocha */
+
 'use strict'
 
-const expect = require('chai').expect
-const base58 = require('bs58')
-const fs = require('fs')
-const IPFS = require('../../../src/core')
-const Block = require('ipfs-block')
-const path = require('path')
+const test = require('interface-ipfs-core')
+const IPFSFactory = require('../../utils/factory-core')
 
-const isNode = require('detect-node')
+let factory
 
-const fileA = isNode
-        ? fs.readFileSync(path.join(__dirname, '../../go-ipfs-repo/blocks/12207028/122070286b9afa6620a66f715c7020d68af3d10e1a497971629c07606bfdb812303d.data'))
-  : require('buffer!./../../go-ipfs-repo/blocks/12207028/122070286b9afa6620a66f715c7020d68af3d10e1a497971629c07606bfdb812303d.data')
+const common = {
+  setup: function (cb) {
+    factory = new IPFSFactory()
+    cb(null, factory)
+  },
+  teardown: function (cb) {
+    factory.dismantle(cb)
+  }
+}
 
-// TODO use arrow funtions again when https://github.com/webpack/webpack/issues/1944 is fixed
-describe('block', function () {
-  var ipfs
-
-  before((done) => {
-    ipfs = new IPFS(require('../../utils/repo-path'))
-    ipfs.load(done)
-  })
-
-  it('get', function (done) {
-    const b58mh = 'QmVtU7ths96fMgZ8YSZAbKghyieq7AjxNdcqyVzxTt3qVe'
-    const mh = new Buffer(base58.decode(b58mh))
-    ipfs.block.get(mh, (err, block) => {
-      expect(err).to.not.exist
-      const eq = fileA.equals(block.data)
-      expect(eq).to.equal(true)
-      done()
-    })
-  })
-
-  it('put', (done) => {
-    var b = new Block('random data')
-    ipfs.block.put(b, function (err) {
-      expect(err).to.not.exist
-      ipfs.block.get(b.key, function (err, block) {
-        expect(err).to.not.exist
-        expect(b.data.equals(block.data)).to.equal(true)
-        expect(b.key.equals(block.key)).to.equal(true)
-        done()
-      })
-    })
-  })
-
-  it('rm', (done) => {
-    var b = new Block('I will not last long enough')
-    ipfs.block.put(b, function (err) {
-      expect(err).to.not.exist
-      ipfs.block.get(b.key, function (err, block) {
-        expect(err).to.not.exist
-        ipfs.block.del(b.key, function (err) {
-          expect(err).to.not.exist
-          ipfs.block.get(b.key, function (err, block) {
-            expect(err).to.exist
-            done()
-          })
-        })
-      })
-    })
-  })
-
-  it('stat', function (done) {
-    const mh = new Buffer(base58
-      .decode('QmVtU7ths96fMgZ8YSZAbKghyieq7AjxNdcqyVzxTt3qVe'))
-    ipfs.block.stat(mh, (err, stats) => {
-      expect(err).to.not.exist
-      expect(stats.Key.equals(mh)).to.equal(true)
-      expect(stats.Size).to.equal(309)
-      done()
-    })
-  })
-})
+test.block(common)
