23b0fe9a940b1ffde7095e28fa6dcfcf6e20c445
Friedel Ziegelmayer <dignifiedquire@gmail.com>
2016-09-15 10:50:34 +0200

    test: add shim for node-forge
    Fixes #382

--- .aegir.js
+++ .aegir.js
@@ -1,7 +1,17 @@
 'use strict'
 
+const path = require('path')
+
 module.exports = {
   webpack: {
+    resolve: {
+      alias: {
+        'node-forge': path.resolve(
+          path.dirname(require.resolve('libp2p-crypto')),
+          '../vendor/forge.bundle.js'
+        )
+      }
+    },
     externals: {
       fs: '{}',
       mkdirp: '{}'
