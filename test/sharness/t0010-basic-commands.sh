0b3d34b6c3b946520481c0ef7009b15951216e38
Christian Couder <chriscool@tuxfamily.org>
2016-10-03 09:41:53 +0200

    t0010: add failing tests

--- test/sharness/t0010-basic-commands.sh
+++ test/sharness/t0010-basic-commands.sh
@@ -26,4 +26,50 @@ test_expect_success "ipfs version output looks good" '
 	test_fsh cat version.txt
 '
 
+test_expect_success "ipfs version --all has all required fields" '
+	ipfs version --all > version_all.txt &&
+	grep "go-ipfs version" version_all.txt &&
+	grep "Repo version" version_all.txt &&
+	grep "System version" version_all.txt &&
+	grep "Golang version" version_all.txt
+'
+
+test_expect_success "ipfs help succeeds" '
+	ipfs help >help.txt
+'
+
+test_expect_success "ipfs help output looks good" '
+	egrep -i "^Usage" help.txt >/dev/null &&
+	egrep "ipfs .* <command>" help.txt >/dev/null ||
+	test_fsh cat help.txt
+'
+
+test_expect_success "'ipfs commands' succeeds" '
+	ipfs commands >commands.txt
+'
+
+test_expect_success "'ipfs commands' output looks good" '
+	grep "ipfs add" commands.txt &&
+	grep "ipfs daemon" commands.txt &&
+	grep "ipfs update" commands.txt
+'
+
+test_expect_success "All commands accept --help" '
+	while read -r cmd
+	do
+		echo "running: $cmd --help"
+		$cmd --help </dev/null >/dev/null || return
+	done <commands.txt
+'
+
+test_expect_success "'ipfs commands --flags' succeeds" '
+	ipfs commands --flags >commands.txt
+'
+
+test_expect_success "'ipfs commands --flags' output looks good" '
+	grep "ipfs pin add --recursive / ipfs pin add -r" commands.txt &&
+	grep "ipfs id --format / ipfs id -f" commands.txt &&
+	grep "ipfs repo gc --quiet / ipfs repo gc -q" commands.txt
+'
+
 test_done
