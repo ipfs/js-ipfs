6ad325ba4b65699d8316b1e24d3585a9148c7715
Victor Bjelkholm <victorbjelkholm@gmail.com>
2016-11-10 13:19:25 +0100

    fix(cli): alias add, cat and get to top-level cli

--- src/cli/bin.js
+++ src/cli/bin.js
@@ -12,10 +12,23 @@ updateNotifier({
   updateCheckInterval: 1000 * 60 * 60 * 24 * 7 // 1 week
 }).notify()
 
-yargs
+const cli = yargs
   .commandDir('commands')
   .demand(1)
-  .help()
+
+// NOTE: This creates an alias of
+// `jsipfs files {add, get, cat}` to `jsipfs {add, get, cat}`.
+// This will stay until https://github.com/ipfs/specs/issues/98 is resolved.
+const addCmd = require('./commands/files/add')
+const catCmd = require('./commands/files/cat')
+const getCmd = require('./commands/files/get')
+const aliases = [addCmd, catCmd, getCmd]
+aliases.forEach((alias) => {
+  cli.command(alias.command, alias.describe, alias.builder, alias.handler)
+})
+
+// finalize cli setup
+cli.help()
   .strict()
   .completion()
-  .argv
+.argv
