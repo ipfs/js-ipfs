16ad10cdc29c07c9688f5404109de9d2da11d9c5
David Dias <daviddias.p@gmail.com>
2016-12-04 15:44:53 -0800

    feat: dag.get, dag.rm and dag.resolve

--- src/core/components/dag.js
+++ src/core/components/dag.js
@@ -1,11 +1,13 @@
 'use strict'
 
 const promisify = require('promisify-es6')
-const dagPB = require('ipld-dag-pb')
-const CID = require('cids')
+// const dagPB = require('ipld-dag-pb')
+// const dagCBOR = require('ipld-dag-cbor')
+
+// const CID = require('cids')
 // const mh = require('multihashes')
 
-module.exports = function object (self) {
+module.exports = function dag (self) {
   return {
     put: promisify((dagNode, multicodec, hashAlg, callback) => {
       // TODO
@@ -15,17 +17,28 @@ module.exports = function object (self) {
       // put in IPLD Resolver
 
       /*
-        self._ipldResolver.put({
-          node: node,
-          cid: new CID(node.multihash)
-        }
+      self._ipldResolver.put({
+        node: node,
+        cid: new CID(node.multihash)
+      }
       */
+      switch (multicodec) {
+        case 'dag-pb': {} break
+        case 'dag-cbor': {} break
+        default:
+          callback(new Error('IPLD Format not supported'))
+      }
     }),
     get: promisify((cid, callback) => {
       self.ipldResolver.get(cid, callback)
     }),
+    rm: promisify((cid, callback) => {
+      // TODO once pinning is complete, this remove operation has to first
+      // verify that some pinning chain is not broken with the operation
+      self.ipldResolver.remove(cid, callback)
+    }),
     resolve: promisify((cid, path, callback) => {
-      // TODO
+      self.ipldResolver.resolve(cid, path, callback)
     })
   }
 }
