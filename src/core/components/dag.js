eea470b74f958b34353d2f994929bd1ebaf9b895
David Dias <daviddias.p@gmail.com>
2016-12-06 17:58:19 -0800

    tests: get interface-ipfs-core tests in

--- src/core/components/dag.js
+++ src/core/components/dag.js
@@ -1,44 +1,38 @@
 'use strict'
 
 const promisify = require('promisify-es6')
-// const dagPB = require('ipld-dag-pb')
-// const dagCBOR = require('ipld-dag-cbor')
-
-// const CID = require('cids')
-// const mh = require('multihashes')
+const dagPB = require('ipld-dag-pb')
+const dagCBOR = require('ipld-dag-cbor')
 
 module.exports = function dag (self) {
   return {
     put: promisify((dagNode, multicodec, hashAlg, callback) => {
-      // TODO
-      // serialize
-      // get right hash
-      // generate cid
-      // put in IPLD Resolver
-
-      /*
-      self._ipldResolver.put({
-        node: node,
-        cid: new CID(node.multihash)
-      }
-      */
       switch (multicodec) {
-        case 'dag-pb': {} break
-        case 'dag-cbor': {} break
-        default:
-          callback(new Error('IPLD Format not supported'))
+        case 'dag-pb': dagPB.util.cid(dagNode, gotCid); break
+        case 'dag-cbor': dagCBOR.util.cid(dagNode, gotCid); break
+        default: callback(new Error('IPLD Format not supported'))
+      }
+
+      function gotCid (err, cid) {
+        if (err) {
+          return callback(err)
+        }
+        self._ipldResolver.put({
+          node: dagNode,
+          cid: cid
+        }, callback)
       }
     }),
     get: promisify((cid, callback) => {
-      self.ipldResolver.get(cid, callback)
+      self._ipldResolver.get(cid, callback)
     }),
     rm: promisify((cid, callback) => {
       // TODO once pinning is complete, this remove operation has to first
       // verify that some pinning chain is not broken with the operation
-      self.ipldResolver.remove(cid, callback)
+      self._ipldResolver.remove(cid, callback)
     }),
     resolve: promisify((cid, path, callback) => {
-      self.ipldResolver.resolve(cid, path, callback)
+      self._ipldResolver.resolve(cid, path, callback)
     })
   }
 }
