c9df6c3bc9cec243f28920c48e917b5bca59f991
James Halliday <substack@gmail.com>
2016-12-25 11:26:48 -1000

    multihashes may be followed by sub-path strings (#495)

--- src/clean-multihash.js
+++ src/clean-multihash.js
@@ -4,11 +4,14 @@ const bs58 = require('bs58')
 const isIPFS = require('is-ipfs')
 
 module.exports = function (multihash) {
-  if (!isIPFS.multihash(multihash)) {
-    throw new Error('not valid multihash')
-  }
   if (Buffer.isBuffer(multihash)) {
-    return bs58.encode(multihash)
+    multihash = bs58.encode(multihash)
+  }
+  if (typeof multihash !== 'string') {
+    throw new Error('unexpected multihash type: ' + typeof multihash)
+  }
+  if (!isIPFS.multihash(multihash.split('/')[0])) {
+    throw new Error('not valid multihash')
   }
   return multihash
 }
