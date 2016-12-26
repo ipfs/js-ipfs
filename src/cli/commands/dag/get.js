37105852a4826921079dbac28498ae93f44eaefa
kumavis <aaron@kumavis.me>
2016-12-26 13:26:06 -0800

    cli - add 'dag get' command

---
+++ src/cli/commands/dag/get.js
@@ -0,0 +1,43 @@
+'use strict'
+
+const utils = require('../../utils')
+const CID = require('cids')
+const debug = require('debug')
+const log = debug('cli:block')
+log.error = debug('cli:block:error')
+
+module.exports = {
+  command: 'get <ref>',
+
+  describe: 'Get a dag node from ipfs.',
+
+  builder: {},
+
+  handler (argv) {
+    utils.getIPFS((err, ipfs) => {
+      if (err) {
+        throw err
+      }
+
+      const refParts = argv.ref.split('/')
+      const cidString = refParts[0]
+      const pathString = refParts.slice(1).join('/')
+      const cid = new CID(cidString)
+
+      let lookupFn
+      if (pathString) {
+        lookupFn = () => ipfs.dag.resolve(cid, pathString)
+      } else {
+        lookupFn = () => ipfs.dag.get(cid)
+      }
+
+      lookupFn()
+      .then((obj) => {
+        console.log(obj)
+      })
+      .catch((err) => {
+        console.error(err)
+      })
+    })
+  }
+}
