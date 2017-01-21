037e9f10bfee238e009b39b00ed0629ea0999744
haad <haad@headbanggames.com>
2017-01-21 15:08:25 +0200

    Add connect to peer inputs

--- examples/ipfm/src/Peers.js
+++ examples/ipfm/src/Peers.js
@@ -7,6 +7,11 @@ const stopEventBubbling = (e) => {
 }
 
 class Peers extends Component {
+  connect () {
+    const addr = this.refs.addr.value
+    this.props.onConnectTo(addr)
+  }
+
   render () {
     const peers = this.props.peers 
       ? this.props.peers.map((e, idx) => {
@@ -31,6 +36,21 @@ class Peers extends Component {
       <div className='Peers' onClick={this.props.onClick.bind(this)}>
         <h2>Connected Peers</h2>
         {peers}
+        <br/>
+        <br/>
+        <h2>Connect to a Peer</h2>
+        <div onClick={stopEventBubbling}>
+          <input className="Peers-connect-input"
+                 type="text"
+                 ref="addr"
+                 placeholder='/ip4/127.0.0.1/tcp/9999/ws/ipfs/QmZGH8GeASSkSZoNLPEBu1MqtzLTERNUEwh9yTHLEF5kcW'
+                 />
+          <input className="Peers-connect-button"
+                 type="button"
+                 value="Connect"
+                 onClick={this.connect.bind(this)}
+                 />
+        </div>
       </div>
     )
   }
