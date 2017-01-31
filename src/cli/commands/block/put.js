d9833f89527ff4d6eab91c8a1af6846179658cee
David Dias <daviddias.p@gmail.com>
2017-01-31 18:18:40 +0000

    feat: add CLI support for different hash func and type

--- src/cli/commands/block/put.js
+++ src/cli/commands/block/put.js
@@ -1,7 +1,8 @@
 'use strict'
 
 const utils = require('../../utils')
-const mh = require('multihashes')
+const CID = require('cids')
+const multihashing = require('multihashing-async')
 const bl = require('bl')
 const fs = require('fs')
 const Block = require('ipfs-block')
@@ -10,35 +11,58 @@ const debug = require('debug')
 const log = debug('cli:block')
 log.error = debug('cli:block:error')
 
-function addBlock (buf) {
+function addBlock (data, opts) {
   utils.getIPFS((err, ipfs) => {
     if (err) {
       throw err
     }
 
-    waterfall([
-      (cb) => ipfs.block.put(new Block(buf), cb),
-      (block, cb) => block.key(cb)
-    ], (err, key) => {
-      if (err) {
-        throw err
-      }
+    let cid
 
-      console.log(mh.toB58String(key))
+    waterfall([
+      (cb) => multihashing(data, opts.mhtype || 'sha2-256', cb),
+      (multihash, cb) => {
+        if (!opts.version || opts.version !== 0) {
+          cid = new CID(1, opts.format || 'dag-pb', multihash)
+        } else {
+          cid = new CID(0, 'dag-pb', multihash)
+        }
+        cb(null, cid)
+      },
+      (cid, cb) => ipfs.block.put(new Block(data), cid, cb)
+    ], (err) => {
+      if (err) { throw err }
+
+      // console.log(cid)
+      console.log(cid.toBaseEncodedString())
     })
   })
 }
 
 module.exports = {
-  command: 'put [data]',
+  command: 'put [block]',
 
   describe: 'Stores input as an IPFS block',
 
-  builder: {},
+  builder: {
+    format: {
+      alias: 'f',
+      describe: 'cid format for blocks to be created with.',
+      default: 'dag-pb'
+    },
+    mhtype: {
+      describe: 'multihash hash function',
+      default: 'sha2-256'
+    },
+    mhlen: {
+      describe: 'multihash hash length',
+      default: undefined
+    }
+  },
 
   handler (argv) {
-    if (argv.data) {
-      return addBlock(fs.readFileSync(argv.data))
+    if (argv.block) {
+      return addBlock(fs.readFileSync(argv.block), argv)
     }
 
     process.stdin.pipe(bl((err, input) => {
@@ -46,7 +70,7 @@ module.exports = {
         throw err
       }
 
-      addBlock(input)
+      addBlock(input, argv)
     }))
   }
 }
--- test/cli/test-block.js
+++ test/cli/test-block.js
@@ -10,33 +10,37 @@ describe('block', () => {
   describeOnlineAndOffline(repoPath, () => {
     it('put', () => {
       return ipfs('block put test/test-data/hello').then((out) => {
-        expect(out).to.be.eql(
-          'QmZjTnYw2TFhn9Nn7tjmPSoTBoY7YRkwPzwSrSbabY24Kp'
-        )
+        expect(out).to.eql('zdj7Wgpi9yzsvjJerghrdhPFpe1p1jZFyB5GKLyXEzFQyaxVk')
       })
+    })
+
+    it('put with flags, format and mhtype', () => {
+      return ipfs('block put --format eth-block --mhtype keccak-256 test/test-data/eth-block')
+        .then((out) => expect(out).to.eql('z43AaGF23fmvRnDP56Ub9WcJCfzSfqtmzNCCvmz5eudT8dtdCDS'))
     })
 
     it('get', () => {
-      return ipfs('block get QmZjTnYw2TFhn9Nn7tjmPSoTBoY7YRkwPzwSrSbabY24Kp').then((out) => {
-        expect(out).to.be.eql('hello world\n')
-      })
+      return ipfs('block get QmZjTnYw2TFhn9Nn7tjmPSoTBoY7YRkwPzwSrSbabY24Kp')
+        .then((out) => expect(out).to.eql('hello world\n'))
     })
 
     it('stat', () => {
-      return ipfs('block stat QmZjTnYw2TFhn9Nn7tjmPSoTBoY7YRkwPzwSrSbabY24Kp').then((out) => {
-        expect(out).to.be.eql([
-          'Key: QmZjTnYw2TFhn9Nn7tjmPSoTBoY7YRkwPzwSrSbabY24Kp',
-          'Size: 12'
-        ].join('\n'))
-      })
+      return ipfs('block stat QmZjTnYw2TFhn9Nn7tjmPSoTBoY7YRkwPzwSrSbabY24Kp')
+        .then((out) => {
+          expect(out).to.eql([
+            'Key: QmZjTnYw2TFhn9Nn7tjmPSoTBoY7YRkwPzwSrSbabY24Kp',
+            'Size: 12'
+          ].join('\n'))
+        })
     })
 
     it.skip('rm', () => {
-      return ipfs('block rm QmZjTnYw2TFhn9Nn7tjmPSoTBoY7YRkwPzwSrSbabY24Kp').then((out) => {
-        expect(out).to.be.eql(
-          'removed QmZjTnYw2TFhn9Nn7tjmPSoTBoY7YRkwPzwSrSbabY24Kp'
-        )
-      })
+      return ipfs('block rm QmZjTnYw2TFhn9Nn7tjmPSoTBoY7YRkwPzwSrSbabY24Kp')
+        .then((out) => {
+          expect(out).to.eql(
+            'removed QmZjTnYw2TFhn9Nn7tjmPSoTBoY7YRkwPzwSrSbabY24Kp'
+          )
+        })
     })
   })
 })
