49cbd2d7372a7a87fbfd42c8416f44b108e700e7
David Dias <daviddias.p@gmail.com>
2017-01-31 10:46:40 +0000

    feat(dag): basics (get and put)

---
+++ src/core/components/dag.js
@@ -0,0 +1,34 @@
+'use strict'
+
+const promisify = require('promisify-es6')
+
+const dagPB = require('ipld-dag-pb')
+const dagCBOR = require('ipld-dag-cbor')
+
+module.exports = function dag (self) {
+  return {
+    put: promisify((dagNode, multicodec, hashAlg, callback) => {
+      switch (multicodec) {
+        case 'dag-pb': dagPB.util.cid(dagNode, gotCid); break
+        case 'dag-cbor': dagCBOR.util.cid(dagNode, gotCid); break
+        default: callback(new Error('IPLD Format not supported'))
+      }
+
+      function gotCid (err, cid) {
+        if (err) {
+          return callback(err)
+        }
+        self._ipldResolver.put({
+          node: dagNode,
+          cid: cid
+        }, callback)
+      }
+    }),
+    get: promisify((cid, callback) => {
+      self._ipldResolver.get(cid, callback)
+    }),
+    resolve: promisify((cid, path, callback) => {
+      self._ipldResolver.resolve(cid, path, callback)
+    })
+  }
+}
