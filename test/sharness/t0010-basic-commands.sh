7cf60bb860bf9554c4d213c90f3566368309f56c
Christian Couder <chriscool@tuxfamily.org>
2017-02-13 22:26:56 +0100

    sharness: t0010- jsipfs version (#518)
    * t0010: add failing tests
    
    * sharness

--- test/sharness/t0010-basic-commands.sh
+++ test/sharness/t0010-basic-commands.sh
@@ -9,15 +9,15 @@ test_description="Test installation and some basic commands"
 . lib/test-lib.sh
 
 test_expect_success "current dir is writable" '
-	echo "It works!" >test.txt
+	echo "It works!" > test.txt
 '
 
 test_expect_success "ipfs version succeeds" '
-	ipfs version >version.txt
+	ipfs version > version.txt
 '
 
 test_expect_success "ipfs version shows js-ipfs" '
-	grep "js-ipfs" version.txt >/dev/null ||
+	grep "js-ipfs" version.txt > /dev/null ||
 	test_fsh cat version.txt
 '
 
@@ -25,5 +25,48 @@ test_expect_success "ipfs version output looks good" '
 	egrep "^js-ipfs version: [0-9]+\.[0-9]+\.[0-9]" version.txt >/dev/null ||
 	test_fsh cat version.txt
 '
+
+test_expect_success "ipfs version --all has all required fields" '
+	ipfs version --all > version_all.txt &&
+	grep "js-ipfs version" version_all.txt
+'
+
+test_expect_success "ipfs help succeeds" '
+	ipfs help > help.txt
+'
+
+# test_expect_success "ipfs help output looks good" '
+# 	egrep -i "^Usage" help.txt > /dev/null &&
+# 	egrep "ipfs .* <command>" help.txt >/dev/null ||
+# 	test_fsh cat help.txt
+# '
+
+test_expect_success "'ipfs commands' succeeds" '
+	ipfs commands > commands.txt
+'
+
+test_expect_success "'ipfs commands' output looks good" '
+	grep "add" commands.txt &&
+	grep "daemon" commands.txt &&
+	grep "update" commands.txt
+'
+
+# test_expect_success "All commands accept --help" '
+# 	while read -r cmd
+# 	do
+# 		echo "running: $cmd --help"
+# 		$cmd --help </dev/null >/dev/null || return
+# 	done <commands.txt
+# '
+
+# test_expect_success "'ipfs commands --flags' succeeds" '
+# 	ipfs commands --flags >commands.txt
+# '
+
+# test_expect_success "'ipfs commands --flags' output looks good" '
+# 	grep "ipfs pin add --recursive / ipfs pin add -r" commands.txt &&
+# 	grep "ipfs id --format / ipfs id -f" commands.txt &&
+# 	grep "ipfs repo gc --quiet / ipfs repo gc -q" commands.txt
+# '
 
 test_done
