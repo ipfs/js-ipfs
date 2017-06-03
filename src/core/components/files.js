1debb903f25eb6ea473e02d9051f1d94775d562f
David Dias <daviddias.p@gmail.com>
2017-06-03 14:53:22 +0100

    wip

--- src/core/components/files.js
+++ src/core/components/files.js
@@ -12,9 +12,38 @@ const pushable = require('pull-pushable')
 const toStream = require('pull-stream-to-stream')
 const toPull = require('stream-to-pull-stream')
 const CID = require('cids')
+const bs58 = require('bs58')
 const waterfall = require('async/waterfall')
 const isStream = require('isstream')
 const Duplex = require('stream').Duplex
+const v = require('is-ipfs')
+
+function cleanCID (cid) {
+  if (Buffer.isBuffer(cid)) {
+    cid = bs58.encode(cid)
+  }
+  if (typeof cid !== 'string') {
+    throw new Error('unexpected cid type: ' + typeof cid)
+  }
+  CID.validateCID(new CID(cid.split('/')[0]))
+  return cid
+}
+
+function cleanPath (ipfsPath) {
+  try {
+    ipfsPath = cleanCID(ipfsPath)
+  } catch (err) {
+    if (!v.ipfsPath(ipfsPath)) {
+      throw err
+    }
+  }
+
+  if (ipfsPath.indexOf('/ipfs/') === 0) {
+    ipfsPath = ipfsPath.substring(6)
+  }
+
+  return ipfsPath
+}
 
 module.exports = function files (self) {
   const createAddPullStream = (options) => {
@@ -80,12 +109,18 @@ module.exports = function files (self) {
       )
     }),
 
-    cat: promisify((hash, callback) => {
-      if (typeof hash === 'function') {
-        return callback(new Error('You must supply a multihash'))
+    cat: promisify((ipfsPath, callback) => {
+      if (typeof ipfsPath === 'function') {
+        return callback(new Error('You must supply a ipfsPath'))
       }
 
-      self._ipldResolver.get(new CID(hash), (err, result) => {
+      try {
+        ipfsPath = cleanPath(ipfsPath)
+      } catch (err) {
+        return callback(err)
+      }
+
+      self.dag.get(ipfsPath, (err, result) => {
         if (err) {
           return callback(err)
         }
@@ -98,8 +133,10 @@ module.exports = function files (self) {
           return callback(new Error('This dag node is a directory'))
         }
 
+        const cid = new CID(ipfsPath)
+
         pull(
-          exporter(hash, self._ipldResolver),
+          exporter(cid.multihash, self._ipldResolver),
           pull.collect((err, files) => {
             if (err) {
               return callback(err)
@@ -110,9 +147,9 @@ module.exports = function files (self) {
       })
     }),
 
-    get: promisify((hash, callback) => {
+    get: promisify((cid, callback) => {
       callback(null, toStream.source(pull(
-        exporter(hash, self._ipldResolver),
+        exporter(cid, self._ipldResolver),
         pull.map((file) => {
           if (file.content) {
             file.content = toStream.source(file.content)
