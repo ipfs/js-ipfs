10c1e1b79afb3a6159ef2fa5ebd87ad1ad2a7107
Victor Bjelkholm <victorbjelkholm@gmail.com>
2017-07-26 16:31:09 +0200

    Get peers when endpoint hit

--- src/http-api/routes/debug.js
+++ src/http-api/routes/debug.js
@@ -6,14 +6,6 @@ module.exports = (server) => {
   const api = server.select('API')
   const gauge = new client.Gauge({ name: 'number_of_peers', help: 'the_number_of_currently_connected_peers' })
 
-  setInterval(() => {
-    server.app.ipfs.swarm.peers((err, res) => {
-      if (err) throw err
-      const count = res.length
-      gauge.set(count)
-    })
-  }, 5000)
-
   api.route({
     method: 'GET',
     path: '/debug/metrics/prometheus',
@@ -22,7 +14,12 @@ module.exports = (server) => {
         return reply('Monitoring is disabled. Enable it by setting environment variable IPFS_MONITORING')
           .code(501) // 501 = Not Implemented
       }
-      reply(register.metrics()).header('Content-Type', register.contentType)
+      server.app.ipfs.swarm.peers((err, res) => {
+        if (err) throw err
+        const count = res.length
+        gauge.set(count)
+        reply(register.metrics()).header('Content-Type', register.contentType)
+      })
     }
   })
 }
