f49c24bfc1c713ae5f96bf99b5d9e808044c8e85
Yahya <ya7yaz@gmail.com>
2017-09-08 13:53:20 +0200

    adding files to make tests self-contained

--- test/gateway/index.js
+++ test/gateway/index.js
@@ -8,13 +8,16 @@ chai.use(dirtyChai)
 const series = require('async/series')
 const API = require('../../src/http')
 const loadFixture = require('aegir/fixtures')
+const os = require('os')
+const path = require('path')
 
 const bigFile = loadFixture(__dirname, '../../node_modules/interface-ipfs-core/test/fixtures/15mb.random', 'ipfs')
 const directoryContent = {
   'index.html': loadFixture(__dirname, './test-folder/index.html', 'ipfs'),
   'nested-folder/hello.txt': loadFixture(__dirname, './test-folder/nested-folder/hello.txt', 'ipfs'),
   'nested-folder/ipfs.txt': loadFixture(__dirname, './test-folder/nested-folder/ipfs.txt', 'ipfs'),
-  'nested-folder/nested.html': loadFixture(__dirname, './test-folder/nested-folder/nested.html', 'ipfs')
+  'nested-folder/nested.html': loadFixture(__dirname, './test-folder/nested-folder/nested.html', 'ipfs'),
+  'cat-folder/cat.jpg': loadFixture(__dirname, './test-folder/cat-folder/cat.jpg', 'ipfs')
 }
 
 describe('HTTP Gateway', () => {
@@ -22,7 +25,12 @@ describe('HTTP Gateway', () => {
   let gateway
 
   before((done) => {
-    http.api = new API(undefined, {
+    const repoPath = path.join(
+      os.tmpdir(),
+      '/ipfs-' + Math.random().toString().substring(2, 8) + '-' + Date.now()
+    )
+
+    http.api = new API(repoPath, {
       Bootstrap: [],
       Discovery: {
         MDNS: {
@@ -31,17 +39,17 @@ describe('HTTP Gateway', () => {
       }
     })
 
+    const content = (name) => ({
+      path: `test-folder/${name}`,
+      content: directoryContent[name]
+    })
+
+    const emptyDir = (name) => ({ path: `test-folder/${name}` })
+
     series([
       (cb) => http.api.start(true, cb),
       (cb) => {
         gateway = http.api.server.select('Gateway')
-        const content = (name) => ({
-          path: `test-folder/${name}`,
-          content: directoryContent[name]
-        })
-
-        const emptyDir = (name) => ({ path: `test-folder/${name}` })
-
         const expectedRootMultihash = 'QmbQD7EMEL1zeebwBsWEfA3ndgSS6F7S6iTuwuqasPgVRi'
 
         const dirs = [
@@ -69,6 +77,32 @@ describe('HTTP Gateway', () => {
           expect(err).to.not.exist()
           const file = res[0]
           expect(file.path).to.equal(expectedMultihash)
+          expect(file.hash).to.equal(expectedMultihash)
+          cb()
+        })
+      },
+      (cb) => {
+        const expectedMultihash = 'QmT78zSuBmuS4z925WZfrqQ1qHaJ56DQaTfyMUF7F8ff5o'
+
+        http.api.node.files.add(Buffer.from('hello world' + '\n'), (err, res) => {
+          expect(err).to.not.exist()
+          const file = res[0]
+          expect(file.path).to.equal(expectedMultihash)
+          expect(file.hash).to.equal(expectedMultihash)
+          cb()
+        })
+      },
+      (cb) => {
+        const expectedMultihash = 'QmW2WQi7j6c7UgJTarActp7tDNikE4B2qXtFCfLPdsgaTQ'
+
+        let dir = [
+          content('cat-folder/cat.jpg')
+        ]
+
+        http.api.node.files.add(dir, (err, res) => {
+          expect(err).to.not.exist()
+          const file = res[1]
+          expect(file.path).to.equal('test-folder/cat-folder')
           expect(file.hash).to.equal(expectedMultihash)
           cb()
         })
