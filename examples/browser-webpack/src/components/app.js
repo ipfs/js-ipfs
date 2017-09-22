4ca318fe4d27f557abc00af2d835d2e73da596a8
Dafeng Guo <dfguo.joe@gmail.com>
2017-09-22 15:06:24 +0800

    docs: fix js dependencies in webpack example

--- examples/browser-webpack/src/components/app.js
+++ examples/browser-webpack/src/components/app.js
@@ -2,8 +2,8 @@
 
 const React = require('react')
 const Buffer = require('safe-buffer').Buffer
-const IPFS = require('../../../../src/core') // replace this by line below
-// const IPFS = require('ipfs')
+// const IPFS = require('../../../../src/core') // replace this by line below
+const IPFS = require('ipfs')
 
 const stringToUse = 'hello world from webpacked IPFS'
 
