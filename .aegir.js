18f5a9a7f51ef79fb51feb59f273218f863e434c
David Dias <daviddias.p@gmail.com>
2017-11-08 07:17:18 +0000

    chore

--- .aegir.js
+++ .aegir.js
@@ -3,7 +3,7 @@
 const parallel = require('async/parallel')
 const spawnTools = require('./test/utils/spawn-tools')
 const js = spawnTools.spawnJsNode
-const go = spawnTools.spawnGoNode
+// const go = spawnTools.spawnGoNode
 const stop = spawnTools.stopNodes
 
 /*
@@ -18,11 +18,11 @@ function pre (done) {
     (cb) => js([`${base}/10012`, `${base}/20012/ws`], true, 31012, 32012, cb),
     (cb) => js([`${base}/10013`, `${base}/20013/ws`], true, 31013, 32013, cb),
     (cb) => js([`${base}/10014`, `${base}/20014/ws`], true, 31014, 32014, cb),
-    (cb) => js([`${base}/10015`, `${base}/20015/ws`], true, 31015, 32015, cb),
-    (cb) => go([`${base}/10027`, `${base}/20027/ws`], true, 33027, 44027, cb),
-    (cb) => go([`${base}/10028`, `${base}/20028/ws`], true, 33028, 44028, cb),
-    (cb) => go([`${base}/10031`, `${base}/20031/ws`], true, 33031, 44031, cb),
-    (cb) => go([`${base}/10032`, `${base}/20032/ws`], true, 33032, 44032, cb)
+    (cb) => js([`${base}/10015`, `${base}/20015/ws`], true, 31015, 32015, cb)
+    // (cb) => go([`${base}/10027`, `${base}/20027/ws`], true, 33027, 44027, cb),
+    // (cb) => go([`${base}/10028`, `${base}/20028/ws`], true, 33028, 44028, cb),
+    // (cb) => go([`${base}/10031`, `${base}/20031/ws`], true, 33031, 44031, cb),
+    // (cb) => go([`${base}/10032`, `${base}/20032/ws`], true, 33032, 44032, cb)
   ], done)
 }
 
