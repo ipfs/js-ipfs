a632eaeac4e43fc6361ebb12cf75fc3f4601612f
Dmitriy Ryajov <dryajov@gmail.com>
2017-11-13 05:03:55 -0800

    test: adding for Addresses.Swarm empty

--- test/cli/daemon.js
+++ test/cli/daemon.js
@@ -16,6 +16,22 @@ describe('daemon', () => {
 
   afterEach(() => clean(repoPath))
 
+  it(`don't crash if Addresses.Swarm is empty`, (done) => {
+    ipfs('init').then(() => {
+      return ipfs('config', 'Addresses', JSON.stringify({
+        API: ['/ipfs/QmAbc', '/ipfs/QmDef'],
+        Gateway: ['/ipfs/QmAbc', '/ipfs/QmDef']
+      }), '--json')
+    }).then(() => {
+      return ipfs('daemon')
+    }).then((res) => {
+      expect(res).to.have.string('Daemon is ready')
+      done()
+    }).catch((err) => {
+      done(err)
+    })
+  })
+
   it('gives error if user hasn\'t run init before', (done) => {
     const expectedError = 'no initialized ipfs repo found in ' + repoPath
 
