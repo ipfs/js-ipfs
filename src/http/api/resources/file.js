bdc0729d600c45cca58f077357a7845094622898
Richard Schneider <makaretu@gmail.com>
2017-11-16 20:28:01 +1300

    refactor: talk directly to ipfs-unixfs-engine
    ipfs.ls does not provide enough information

--- src/http/api/resources/file.js
+++ src/http/api/resources/file.js
@@ -2,14 +2,30 @@
 
 const mh = require('multihashes')
 const debug = require('debug')
-const log = debug('jsipfs:http-api:files')
-log.error = debug('jsipfs:http-api:files:error')
+const log = debug('jsipfs:http-api:file')
+log.error = debug('jsipfs:http-api:file:error')
+const unixfsEngine = require('ipfs-unixfs-engine')
+const exporter = unixfsEngine.exporter
+const pull = require('pull-stream')
+const toB58String = require('multihashes').toB58String
 
 exports = module.exports
 
 const fileTypeMap = {
   file: 'File',
   dir: 'Directory'
+}
+
+function toFileObject(file) {
+  const fo = {
+    Hash: toB58String(file.hash),
+    Size: file.size,
+    Type: fileTypeMap[file.type] || file.type
+  }
+  if (fo.Hash !== file.name) {
+    fo.Name = file.name
+  }
+  return fo;
 }
 
 // common pre request handler that parses the args and returns `key` which is assigned to `request.pre.args`
@@ -42,8 +58,11 @@ exports.parseKey = (request, reply) => {
     }).code(500).takeover()
   }
 
+  const subpaths = key.split('/')
+  subpaths.shift()
   reply({
     path: request.query.arg,
+    subpaths: subpaths,
     key: key,
     hash: hash
   })
@@ -55,36 +74,38 @@ exports.ls = {
 
   // main route handler which is called after the above `parseArgs`, but only if the args were valid
   handler: (request, reply) => {
-    const key = request.pre.args.key
     const path = request.pre.args.path
-    const hash = request.pre.args.hash
     const ipfs = request.server.app.ipfs
+    const subpaths = request.pre.args.subpaths
+    const rootDepth = subpaths.length
 
-    ipfs.ls(key, (err, files) => {
-      if (err) {
-        return reply({
-          Message: 'Failed to list dir: ' + err.message,
-          Code: 0
-        }).code(500)
-      }
+    pull(
+      exporter(path, ipfs._ipldResolver, { maxDepth: rootDepth + 1 }),
+      pull.collect((err, files) => {
+        if (err) {
+          return reply({
+            Message: 'Failed to list dir: ' + err.message,
+            Code: 0
+          }).code(500)
+        }
 
-      let res = {
-        Arguments: {},
-        Objects: {}
-      }
-      res.Arguments[path] = key
-      res.Objects[key] = {
-        Hash: hash,
-        Size: 0,
-        Type: 'Directory',
-        Links: files.map((file) => ({
-          Name: file.name,
-          Hash: file.hash,
-          Size: file.size,
-          Type: fileTypeMap[file.type] || file.type
-        }))
-      }
-      reply(res)
-    })
+        let res = {
+          Arguments: {},
+          Objects: {}
+        }
+        const links = []
+        files.forEach((file) => {
+          if (file.depth === rootDepth) {
+            let id = toB58String(file.hash)
+            res.Arguments[path] = id
+            res.Objects[id] = toFileObject(file)
+            res.Objects[id].Links = file.type === 'file' ? null : links;
+          } else {
+            links.push(toFileObject(file))
+          }
+        })
+        return reply(res)
+      })
+    )
   }
 }
