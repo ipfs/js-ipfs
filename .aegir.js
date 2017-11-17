2a91ac49790e34161d240d6e70c9f480a9607b43
David Dias <daviddias.p@gmail.com>
2017-11-17 10:13:32 +0000

    test: fix bootstrapers tests

--- .aegir.js
+++ .aegir.js
@@ -11,7 +11,6 @@ const stop = ads.stopNodes
  */
 function start (done) {
   const base = '/ip4/127.0.0.1/tcp'
-
   if (!process.env.IPFS_TEST) {
     parallel([
       (cb) => js([`${base}/10007`, `${base}/20007/ws`], true, 31007, 32007, cb),
@@ -21,13 +20,15 @@ function start (done) {
       (cb) => js([`${base}/10014`, `${base}/20014/ws`], true, 31014, 32014, cb),
       (cb) => js([`${base}/10015`, `${base}/20015/ws`], true, 31015, 32015, cb)
     ], done)
-  } else {
+  } else if (process.env.IPFS_TEST === 'interop') {
     parallel([
       (cb) => go([`${base}/10027`, `${base}/20027/ws`], true, 33027, 44027, cb),
       (cb) => go([`${base}/10028`, `${base}/20028/ws`], true, 33028, 44028, cb),
       (cb) => go([`${base}/10031`, `${base}/20031/ws`], true, 33031, 44031, cb),
       (cb) => go([`${base}/10032`, `${base}/20032/ws`], true, 33032, 44032, cb)
     ], done)
+  } else if (process.env.IPFS_TEST === 'bootstrapers') {
+    done()
   }
 }
 
