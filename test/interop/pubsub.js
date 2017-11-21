5bc9ce24890d55ad9acf3284c7ef2324f1174c7c
Richard Schneider <makaretu@gmail.com>
2017-11-21 16:24:36 +1300

    test: more tests with different data types
    Note that binary data from JS to GO fails

--- test/interop/pubsub.js
+++ test/interop/pubsub.js
@@ -83,102 +83,17 @@ describe('pubsub', function () {
       ], done)
     })
   })
-
-  it('publish from Go, subscribe on Go', (done) => {
-    const topic = 'pubsub-go-go'
+  
+  describe('ascii data', () => {
     const data = Buffer.from('hello world')
-    let n = 0
 
-    function checkMessage (msg) {
-      ++n
-      expect(msg.data.toString()).to.equal(data.toString())
-      expect(msg).to.have.property('seqno')
-      expect(Buffer.isBuffer(msg.seqno)).to.be.eql(true)
-      expect(msg).to.have.property('topicIDs').eql([topic])
-      expect(msg).to.have.property('from', goId)
-    }
-
-    series([
-      (cb) => goD.api.pubsub.subscribe(topic, checkMessage, cb),
-      (cb) => goD.api.pubsub.publish(topic, data, cb),
-      (cb) => waitFor(() => n === 1, cb)
-    ], done)
-  })
-
-  it('publish from JS, subscribe on JS', (done) => {
-    const topic = 'pubsub-js-js'
-    const data = Buffer.from('hello world')
-    let n = 0
-
-    function checkMessage (msg) {
-      ++n
-      expect(msg.data.toString()).to.equal(data.toString())
-      expect(msg).to.have.property('seqno')
-      expect(Buffer.isBuffer(msg.seqno)).to.be.eql(true)
-      expect(msg).to.have.property('topicIDs').eql([topic])
-      expect(msg).to.have.property('from', jsId)
-    }
-
-    series([
-      (cb) => jsD.api.pubsub.subscribe(topic, checkMessage, cb),
-      (cb) => jsD.api.pubsub.publish(topic, data, cb),
-      (cb) => waitFor(() => n === 1, cb)
-    ], done)
-  })
-
-  it('publish from JS, subscribe on Go', (done) => {
-    const topic = 'pubsub-js-go'
-    const data = Buffer.from('hello world')
-    let n = 0
-
-    function checkMessage (msg) {
-      ++n
-      expect(msg.data.toString()).to.equal(data.toString())
-      expect(msg).to.have.property('seqno')
-      expect(Buffer.isBuffer(msg.seqno)).to.be.eql(true)
-      expect(msg).to.have.property('topicIDs').eql([topic])
-      expect(msg).to.have.property('from', jsId)
-    }
-
-    series([
-      (cb) => goD.api.pubsub.subscribe(topic, checkMessage, cb),
-      (cb) => setTimeout(() => { cb() }, 500),
-      (cb) => jsD.api.pubsub.publish(topic, data, cb),
-      (cb) => waitFor(() => n === 1, cb)
-    ], done)
-  })
-
-  it('publish from Go, subscribe on JS', (done) => {
-    const topic = 'pubsub-go-js'
-    const data = Buffer.from('hello world')
-    let n = 0
-
-    function checkMessage (msg) {
-      ++n
-      expect(msg.data.toString()).to.equal(data.toString())
-      expect(msg).to.have.property('seqno')
-      expect(Buffer.isBuffer(msg.seqno)).to.be.eql(true)
-      expect(msg).to.have.property('topicIDs').eql([topic])
-      expect(msg).to.have.property('from', goId)
-    }
-
-    series([
-      (cb) => jsD.api.pubsub.subscribe(topic, checkMessage, cb),
-      (cb) => setTimeout(() => { cb() }, 500),
-      (cb) => goD.api.pubsub.publish(topic, data, cb),
-      (cb) => waitFor(() => n === 1, cb),
-    ], done)
-  })
-
-  describe.skip('binary data', () => {
     it('publish from Go, subscribe on Go', (done) => {
-      const topic = 'pubsub-binary-go-go'
-      const data = Buffer.from('00010203040506070809', 'hex')
+      const topic = 'pubsub-go-go'
       let n = 0
 
       function checkMessage (msg) {
         ++n
-        expect(msg.data.toString('hex')).to.equal(data.toString('hex'))
+        expect(msg.data.toString()).to.equal(data.toString())
         expect(msg).to.have.property('seqno')
         expect(Buffer.isBuffer(msg.seqno)).to.be.eql(true)
         expect(msg).to.have.property('topicIDs').eql([topic])
@@ -192,9 +107,160 @@ describe('pubsub', function () {
       ], done)
     })
 
+    it('publish from JS, subscribe on JS', (done) => {
+      const topic = 'pubsub-js-js'
+      let n = 0
+
+      function checkMessage (msg) {
+        ++n
+        expect(msg.data.toString()).to.equal(data.toString())
+        expect(msg).to.have.property('seqno')
+        expect(Buffer.isBuffer(msg.seqno)).to.be.eql(true)
+        expect(msg).to.have.property('topicIDs').eql([topic])
+        expect(msg).to.have.property('from', jsId)
+      }
+
+      series([
+        (cb) => jsD.api.pubsub.subscribe(topic, checkMessage, cb),
+        (cb) => jsD.api.pubsub.publish(topic, data, cb),
+        (cb) => waitFor(() => n === 1, cb)
+      ], done)
+    })
+
     it('publish from JS, subscribe on Go', (done) => {
-      const topic = 'pubsub-binary-js-go'
-      const data = Buffer.from('00010203040506070809', 'hex')
+      const topic = 'pubsub-js-go'
+      let n = 0
+
+      function checkMessage (msg) {
+        ++n
+        expect(msg.data.toString()).to.equal(data.toString())
+        expect(msg).to.have.property('seqno')
+        expect(Buffer.isBuffer(msg.seqno)).to.be.eql(true)
+        expect(msg).to.have.property('topicIDs').eql([topic])
+        expect(msg).to.have.property('from', jsId)
+      }
+
+      series([
+        (cb) => goD.api.pubsub.subscribe(topic, checkMessage, cb),
+        (cb) => setTimeout(() => { cb() }, 500),
+        (cb) => jsD.api.pubsub.publish(topic, data, cb),
+        (cb) => waitFor(() => n === 1, cb)
+      ], done)
+    })
+
+    it('publish from Go, subscribe on JS', (done) => {
+      const topic = 'pubsub-go-js'
+      let n = 0
+
+      function checkMessage (msg) {
+        ++n
+        expect(msg.data.toString()).to.equal(data.toString())
+        expect(msg).to.have.property('seqno')
+        expect(Buffer.isBuffer(msg.seqno)).to.be.eql(true)
+        expect(msg).to.have.property('topicIDs').eql([topic])
+        expect(msg).to.have.property('from', goId)
+      }
+
+      series([
+        (cb) => jsD.api.pubsub.subscribe(topic, checkMessage, cb),
+        (cb) => setTimeout(() => { cb() }, 500),
+        (cb) => goD.api.pubsub.publish(topic, data, cb),
+        (cb) => waitFor(() => n === 1, cb),
+      ], done)
+    })
+  })
+
+  describe('non-ascii data', () => {
+    const data = Buffer.from('你好世界')
+
+    it('publish from Go, subscribe on Go', (done) => {
+      const topic = 'pubsub-non-ascii-go-go'
+      let n = 0
+
+      function checkMessage (msg) {
+        ++n
+        expect(msg.data.toString()).to.equal(data.toString())
+        expect(msg).to.have.property('seqno')
+        expect(Buffer.isBuffer(msg.seqno)).to.be.eql(true)
+        expect(msg).to.have.property('topicIDs').eql([topic])
+        expect(msg).to.have.property('from', goId)
+      }
+
+      series([
+        (cb) => goD.api.pubsub.subscribe(topic, checkMessage, cb),
+        (cb) => goD.api.pubsub.publish(topic, data, cb),
+        (cb) => waitFor(() => n === 1, cb)
+      ], done)
+    })
+
+    it('publish from JS, subscribe on JS', (done) => {
+      const topic = 'pubsub-non-ascii-js-js'
+      let n = 0
+
+      function checkMessage (msg) {
+        ++n
+        expect(msg.data.toString()).to.equal(data.toString())
+        expect(msg).to.have.property('seqno')
+        expect(Buffer.isBuffer(msg.seqno)).to.be.eql(true)
+        expect(msg).to.have.property('topicIDs').eql([topic])
+        expect(msg).to.have.property('from', jsId)
+      }
+
+      series([
+        (cb) => jsD.api.pubsub.subscribe(topic, checkMessage, cb),
+        (cb) => jsD.api.pubsub.publish(topic, data, cb),
+        (cb) => waitFor(() => n === 1, cb)
+      ], done)
+    })
+
+    it('publish from JS, subscribe on Go', (done) => {
+      const topic = 'pubsub-non-ascii-js-go'
+      let n = 0
+
+      function checkMessage (msg) {
+        ++n
+        expect(msg.data.toString()).to.equal(data.toString())
+        expect(msg).to.have.property('seqno')
+        expect(Buffer.isBuffer(msg.seqno)).to.be.eql(true)
+        expect(msg).to.have.property('topicIDs').eql([topic])
+        expect(msg).to.have.property('from', jsId)
+      }
+
+      series([
+        (cb) => goD.api.pubsub.subscribe(topic, checkMessage, cb),
+        (cb) => setTimeout(() => { cb() }, 500),
+        (cb) => jsD.api.pubsub.publish(topic, data, cb),
+        (cb) => waitFor(() => n === 1, cb)
+      ], done)
+    })
+
+    it('publish from Go, subscribe on JS', (done) => {
+      const topic = 'pubsub-non-ascii-go-js'
+      let n = 0
+
+      function checkMessage (msg) {
+        ++n
+        expect(msg.data.toString()).to.equal(data.toString())
+        expect(msg).to.have.property('seqno')
+        expect(Buffer.isBuffer(msg.seqno)).to.be.eql(true)
+        expect(msg).to.have.property('topicIDs').eql([topic])
+        expect(msg).to.have.property('from', goId)
+      }
+
+      series([
+        (cb) => jsD.api.pubsub.subscribe(topic, checkMessage, cb),
+        (cb) => setTimeout(() => { cb() }, 500),
+        (cb) => goD.api.pubsub.publish(topic, data, cb),
+        (cb) => waitFor(() => n === 1, cb),
+      ], done)
+    })
+  })
+
+  describe('binary data', () => {
+    const data = Buffer.from('a36161636179656162830103056164a16466666666f400010203040506070809', 'hex')
+    
+    it('publish from Go, subscribe on Go', (done) => {
+      const topic = 'pubsub-binary-go-go'
       let n = 0
 
       function checkMessage (msg) {
@@ -208,6 +274,70 @@ describe('pubsub', function () {
 
       series([
         (cb) => goD.api.pubsub.subscribe(topic, checkMessage, cb),
+        (cb) => setTimeout(() => { cb() }, 500),
+        (cb) => goD.api.pubsub.publish(topic, data, cb),
+        (cb) => waitFor(() => n === 1, cb)
+      ], done)
+    })
+
+    it('publish from Go, subscribe on JS', (done) => {
+      const topic = 'pubsub-binary-go-js'
+      let n = 0
+
+      function checkMessage (msg) {
+        ++n
+        expect(msg.data.toString('hex')).to.equal(data.toString('hex'))
+        expect(msg).to.have.property('seqno')
+        expect(Buffer.isBuffer(msg.seqno)).to.be.eql(true)
+        expect(msg).to.have.property('topicIDs').eql([topic])
+        expect(msg).to.have.property('from', goId)
+      }
+
+      series([
+        (cb) => jsD.api.pubsub.subscribe(topic, checkMessage, cb),
+        (cb) => setTimeout(() => { cb() }, 500),
+        (cb) => goD.api.pubsub.publish(topic, data, cb),
+        (cb) => waitFor(() => n === 1, cb)
+      ], done)
+    })
+
+    it('publish from JS, subscribe on Go', (done) => {
+      const topic = 'pubsub-binary-js-go'
+      let n = 0
+
+      function checkMessage (msg) {
+        ++n
+        expect(msg.data.toString('hex')).to.equal(data.toString('hex'))
+        expect(msg).to.have.property('seqno')
+        expect(Buffer.isBuffer(msg.seqno)).to.be.eql(true)
+        expect(msg).to.have.property('topicIDs').eql([topic])
+        expect(msg).to.have.property('from', jsId)
+      }
+
+      series([
+        (cb) => goD.api.pubsub.subscribe(topic, checkMessage, cb),
+        (cb) => setTimeout(() => { cb() }, 500),
+        (cb) => jsD.api.pubsub.publish(topic, data, cb),
+        (cb) => waitFor(() => n === 1, cb)
+      ], done)
+    })
+
+    it('publish from JS, subscribe on JS', (done) => {
+      const topic = 'pubsub-binary-js-js'
+      let n = 0
+
+      function checkMessage (msg) {
+        ++n
+        expect(msg.data.toString('hex')).to.equal(data.toString('hex'))
+        expect(msg).to.have.property('seqno')
+        expect(Buffer.isBuffer(msg.seqno)).to.be.eql(true)
+        expect(msg).to.have.property('topicIDs').eql([topic])
+        expect(msg).to.have.property('from', jsId)
+      }
+
+      series([
+        (cb) => jsD.api.pubsub.subscribe(topic, checkMessage, cb),
+        (cb) => setTimeout(() => { cb() }, 500),
         (cb) => jsD.api.pubsub.publish(topic, data, cb),
         (cb) => waitFor(() => n === 1, cb)
       ], done)
