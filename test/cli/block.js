972c498b475230b96ec1dd6236dc6cc245381459
David Dias <daviddias.p@gmail.com>
2017-11-23 09:30:58 +0000

    take it all

--- test/cli/block.js
+++ test/cli/block.js
@@ -4,57 +4,65 @@
 const expect = require('chai').expect
 const runOnAndOff = require('../utils/on-and-off')
 
-describe('block', function () {
-  runOnAndOff((thing) => {
-    this.timeout(30000)
-    let ipfs
+describe('block', () => runOnAndOff((thing) => {
+  let ipfs
 
-    before(() => {
-      ipfs = thing.ipfs
-    })
+  before(() => {
+    ipfs = thing.ipfs
+  })
 
-    it('put', () => {
-      return ipfs('block put test/fixtures/test-data/hello').then((out) => {
-        expect(out).to.eql('QmZjTnYw2TFhn9Nn7tjmPSoTBoY7YRkwPzwSrSbabY24Kp\n')
-      })
-    })
-
-    it('put with flags, format and mhtype', () => {
-      return ipfs('block put --format eth-block --mhtype keccak-256 test/fixtures/test-data/eth-block')
-        .then((out) =>
-          expect(out).to.eql('z43AaGF23fmvRnDP56Ub9WcJCfzSfqtmzNCCvmz5eudT8dtdCDS\n'))
-    })
-
-    it('get', () => {
-      return ipfs('block get QmZjTnYw2TFhn9Nn7tjmPSoTBoY7YRkwPzwSrSbabY24Kp')
-        .then((out) => expect(out).to.eql('hello world\n'))
-    })
-
-    it('get block from file without a final newline', () => {
-      return ipfs('block put test/fixtures/test-data/no-newline').then((out) => {
-        expect(out).to.eql('QmTwbQs4sGcCiPxV97SpbHS7QgmVg9SiKxcG1AcF1Ly2SL\n')
-        return ipfs('block get QmTwbQs4sGcCiPxV97SpbHS7QgmVg9SiKxcG1AcF1Ly2SL')
-      })
-        .then((out) => expect(out).to.eql('there is no newline at end of this file'))
-    })
-
-    it('stat', () => {
-      return ipfs('block stat QmZjTnYw2TFhn9Nn7tjmPSoTBoY7YRkwPzwSrSbabY24Kp')
-        .then((out) => {
-          expect(out).to.eql([
-            'Key: QmZjTnYw2TFhn9Nn7tjmPSoTBoY7YRkwPzwSrSbabY24Kp',
-            'Size: 12'
-          ].join('\n') + '\n')
-        })
-    })
-
-    it.skip('rm', () => {
-      return ipfs('block rm QmZjTnYw2TFhn9Nn7tjmPSoTBoY7YRkwPzwSrSbabY24Kp')
-        .then((out) => {
-          expect(out).to.eql(
-            'removed QmZjTnYw2TFhn9Nn7tjmPSoTBoY7YRkwPzwSrSbabY24Kp\n'
-          )
-        })
+  it('put', function () {
+    this.timeout(20 * 1000)
+    return ipfs('block put test/fixtures/test-data/hello').then((out) => {
+      expect(out).to.eql('QmZjTnYw2TFhn9Nn7tjmPSoTBoY7YRkwPzwSrSbabY24Kp\n')
     })
   })
-})
+
+  it('put with flags, format and mhtype', function () {
+    this.timeout(20 * 1000)
+
+    return ipfs('block put --format eth-block --mhtype keccak-256 test/fixtures/test-data/eth-block')
+      .then((out) =>
+        expect(out).to.eql('z43AaGF23fmvRnDP56Ub9WcJCfzSfqtmzNCCvmz5eudT8dtdCDS\n'))
+  })
+
+  it('get', function () {
+    this.timeout(20 * 1000)
+
+    return ipfs('block get QmZjTnYw2TFhn9Nn7tjmPSoTBoY7YRkwPzwSrSbabY24Kp')
+      .then((out) => expect(out).to.eql('hello world\n'))
+  })
+
+  it('get block from file without a final newline', () => {
+    this.timeout(20 * 1000)
+
+    return ipfs('block put test/fixtures/test-data/no-newline').then((out) => {
+      expect(out).to.eql('QmTwbQs4sGcCiPxV97SpbHS7QgmVg9SiKxcG1AcF1Ly2SL\n')
+      return ipfs('block get QmTwbQs4sGcCiPxV97SpbHS7QgmVg9SiKxcG1AcF1Ly2SL')
+    })
+      .then((out) => expect(out).to.eql('there is no newline at end of this file'))
+  })
+
+  it('stat', function () {
+    this.timeout(20 * 1000)
+
+    return ipfs('block stat QmZjTnYw2TFhn9Nn7tjmPSoTBoY7YRkwPzwSrSbabY24Kp')
+      .then((out) => {
+        expect(out).to.eql([
+          'Key: QmZjTnYw2TFhn9Nn7tjmPSoTBoY7YRkwPzwSrSbabY24Kp',
+          'Size: 12'
+        ].join('\n') + '\n')
+      })
+  })
+
+  it.skip('rm', function () {
+    this.timeout(20 * 1000)
+
+    return ipfs('block rm QmZjTnYw2TFhn9Nn7tjmPSoTBoY7YRkwPzwSrSbabY24Kp')
+      .then((out) => {
+        expect(out).to.eql(
+          'removed QmZjTnYw2TFhn9Nn7tjmPSoTBoY7YRkwPzwSrSbabY24Kp\n'
+        )
+      })
+  })
+}))
