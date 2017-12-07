9c9346049a51e8cea86ab8835aef8bc091d36583
David Dias <daviddias.p@gmail.com>
2017-12-07 21:23:05 +0000

    chore: fix linting

--- test/utils/interop-daemon-spawner/util.js
+++ test/utils/interop-daemon-spawner/util.js
@@ -1,14 +1,9 @@
 'use strict'
 
 const os = require('os')
-const crypto = require('libp2p-crypto')
 const path = require('path')
 const hat = require('hat')
 
 exports.tmpDir = (prefix) => {
-  return path.join(
-    os.tmpdir(),
-    prefix || 'js-ipfs-interop',
-    hat()
-  )
+  return path.join(os.tmpdir(), prefix || 'js-ipfs-interop', hat())
 }
