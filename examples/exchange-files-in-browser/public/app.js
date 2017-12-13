9e1cd3ed4dc27f832fabdfd15aefcca460010133
David Dias <daviddias.p@gmail.com>
2017-12-13 09:18:53 +0000

    docs: reorg exchange files exemple

--- examples/exchange-files-in-browser/public/app.js
+++ examples/exchange-files-in-browser/public/app.js
@@ -36,8 +36,8 @@ function start () {
       config: {
         Addresses: {
           Swarm: [
-            '/dns4/wrtc-star.discovery.libp2p.io/tcp/443/wss/p2p-webrtc-star'
-            // '/dns4/ws-star.discovery.libp2p.io/tcp/443/wss/p2p-websocket-star'
+            // '/dns4/wrtc-star.discovery.libp2p.io/tcp/443/wss/p2p-webrtc-star'
+            '/dns4/ws-star.discovery.libp2p.io/tcp/443/wss/p2p-websocket-star'
           ]
         }
       }
