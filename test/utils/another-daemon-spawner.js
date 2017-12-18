8e9496fc2f19582180685695edf30775a0a8421a
David Dias <daviddias.p@gmail.com>
2017-12-18 12:22:03 +0000

    fix

--- test/utils/another-daemon-spawner.js
+++ test/utils/another-daemon-spawner.js
@@ -7,61 +7,10 @@ const series = require('async/series')
 
 const relayConfig = require('./ipfs-factory-daemon/default-config.json')
 const Factory = require('./ipfs-factory-daemon')
-const GoDaemon = require('./interop-daemon-spawner/go')
 
 const nodes = []
 const factory = new Factory()
 exports = module.exports
-
-exports.spawnGoNode = (addrs, hop, api, gateway, callback) => {
-  if (typeof hop === 'function') {
-    callback = hop
-    hop = false
-  }
-  if (typeof api === 'function') {
-    callback = api
-    api = 0
-  }
-  if (typeof gateway === 'function') {
-    callback = gateway
-    gateway = 0
-  }
-
-  api = api || 0
-  gateway = gateway || 0
-
-  const daemon = new GoDaemon({
-    disposable: true,
-    init: true,
-    config: {
-      Addresses: {
-        Swarm: addrs,
-        API: `/ip4/0.0.0.0/tcp/${api}`,
-        Gateway: `/ip4/0.0.0.0/tcp/${gateway}`
-      },
-      Swarm: {
-        AddrFilters: null,
-        DisableBandwidthMetrics: false,
-        DisableNatPortMap: false,
-        DisableRelay: false,
-        EnableRelayHop: hop
-      }
-    }
-  })
-
-  daemon.start((err) => {
-    if (err) {
-      return callback(err)
-    }
-    daemon.api.id((err, id) => {
-      if (err) {
-        return callback(err)
-      }
-      nodes.push(daemon)
-      callback(null, daemon, id.addresses)
-    })
-  })
-}
 
 exports.spawnJsNode = (addrs, hop, api, gateway, callback) => {
   let relayPeer
