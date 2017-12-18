298e18db04f1d8bb23a85da4494dc11a94a0909c
David Dias <daviddias.p@gmail.com>
2017-12-18 11:42:44 +0000

    port interop tests to interop repo

--- .aegir.js
+++ .aegir.js
@@ -3,7 +3,6 @@
 const parallel = require('async/parallel')
 const ads = require('./test/utils/another-daemon-spawner')
 const js = ads.spawnJsNode
-const go = ads.spawnGoNode
 const stop = ads.stopNodes
 
 /*
@@ -19,13 +18,6 @@ function start (done) {
       (cb) => js([`${base}/10013`, `${base}/20013/ws`], true, 31013, 32013, cb),
       (cb) => js([`${base}/10014`, `${base}/20014/ws`], true, 31014, 32014, cb),
       (cb) => js([`${base}/10015`, `${base}/20015/ws`], true, 31015, 32015, cb)
-    ], done)
-  } else if (process.env.IPFS_TEST === 'interop') {
-    parallel([
-      (cb) => go([`${base}/10027`, `${base}/20027/ws`], true, 33027, 44027, cb),
-      (cb) => go([`${base}/10028`, `${base}/20028/ws`], true, 33028, 44028, cb),
-      (cb) => go([`${base}/10031`, `${base}/20031/ws`], true, 33031, 44031, cb),
-      (cb) => go([`${base}/10032`, `${base}/20032/ws`], true, 33032, 44032, cb)
     ], done)
   } else if (process.env.IPFS_TEST === 'bootstrapers') {
     done()
