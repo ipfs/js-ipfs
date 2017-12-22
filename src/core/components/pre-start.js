9ac70d30858b9fd03509e9fceaddeb142c679fe0
Richard Schneider <makaretu@gmail.com>
2017-12-23 03:50:40 +1300

    rebasing with master

--- src/core/components/pre-start.js
+++ src/core/components/pre-start.js
@@ -4,9 +4,7 @@ const peerId = require('peer-id')
 const PeerInfo = require('peer-info')
 const multiaddr = require('multiaddr')
 const waterfall = require('async/waterfall')
-
-// See https://github.com/ipfs/js-ipfs/pull/1133#issuecomment-351310534
-const Keychain = require('./no-keychain')
+const Keychain = require('libp2p-keychain')
 
 /*
  * Load stuff from Repo into memory
