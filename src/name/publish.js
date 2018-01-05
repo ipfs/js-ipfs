9b8ef486067b042e71042c07f2bbcef762166956
Henrique Dias <hacdias@gmail.com>
2018-01-05 18:13:17 +0000

    feat: normalize NAME API (#658)

--- src/name/publish.js
+++ src/name/publish.js
@@ -2,16 +2,24 @@
 
 const promisify = require('promisify-es6')
 
+const transform = function (res, callback) {
+  callback(null, {
+    name: res.Name,
+    value: res.Value
+  })
+}
+
 module.exports = (send) => {
   return promisify((args, opts, callback) => {
     if (typeof (opts) === 'function') {
       callback = opts
       opts = {}
     }
-    send({
+
+    send.andTransform({
       path: 'name/publish',
       args: args,
       qs: opts
-    }, callback)
+    }, transform, callback)
   })
 }
--- src/name/resolve.js
+++ src/name/resolve.js
@@ -2,16 +2,21 @@
 
 const promisify = require('promisify-es6')
 
+const transform = function (res, callback) {
+  callback(null, res.Path)
+}
+
 module.exports = (send) => {
   return promisify((args, opts, callback) => {
     if (typeof (opts) === 'function') {
       callback = opts
       opts = {}
     }
-    send({
+
+    send.andTransform({
       path: 'name/resolve',
       args: args,
       qs: opts
-    }, callback)
+    }, transform, callback)
   })
 }
