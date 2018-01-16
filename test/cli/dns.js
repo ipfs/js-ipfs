639024caefbc05a9a7af124815df3a0ca4110ff2
Henrique Dias <hacdias@gmail.com>
2018-01-16 14:01:05 +0000

    feat: /api/v0/dns (#1172)

---
+++ test/cli/dns.js
@@ -0,0 +1,22 @@
+/* eslint-env mocha */
+'use strict'
+
+const expect = require('chai').expect
+const runOnAndOff = require('../utils/on-and-off')
+
+describe('dns', () => runOnAndOff((thing) => {
+  let ipfs
+
+  before(function () {
+    this.timeout(60 * 1000)
+    ipfs = thing.ipfs
+  })
+
+  it('resolve ipfs.io dns', function () {
+    this.timeout(60 * 1000)
+
+    return ipfs('dns ipfs.io').then((res) => {
+      expect(res.substr(0, 6)).to.eql('/ipfs/')
+    })
+  })
+}))
