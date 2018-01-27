d65aac38f80374614a84563227366f28afdb68ff
Richard Schneider <makaretu@gmail.com>
2018-01-27 14:52:06 +1300

    chore: key core normalisation

--- src/core/components/key.js
+++ src/core/components/key.js
@@ -4,62 +4,39 @@
 
 const promisify = require('promisify-es6')
 
-function toKeyInfo (key) {
-  return {
-    Name: key.name,
-    Id: key.id
-  }
-}
-
 module.exports = function key (self) {
   return {
     gen: promisify((name, opts, callback) => {
-      self._keychain.createKey(name, opts.type, opts.size, (err, key) => {
-        if (err) return callback(err)
-        callback(null, toKeyInfo(key))
-      })
+      self._keychain.createKey(name, opts.type, opts.size, callback)
     }),
 
     info: promisify((name, callback) => {
-      self._keychain.findKeyByName(name, (err, key) => {
-        if (err) return callback(err)
-        callback(null, toKeyInfo(key))
-      })
+      self._keychain.findKeyByName(name, callback)
     }),
 
     list: promisify((callback) => {
-      self._keychain.listKeys((err, keys) => {
-        if (err) return callback(err)
-        keys = keys.map(toKeyInfo)
-        callback(null, { Keys: keys })
-      })
+      self._keychain.listKeys(callback)
     }),
 
     rm: promisify((name, callback) => {
-      self._keychain.removeKey(name, (err, key) => {
-        if (err) return callback(err)
-        callback(null, { Keys: [ toKeyInfo(key) ] })
-      })
+      self._keychain.removeKey(name, callback)
     }),
 
     rename: promisify((oldName, newName, callback) => {
       self._keychain.renameKey(oldName, newName, (err, key) => {
         if (err) return callback(err)
         const result = {
-          Was: oldName,
-          Now: key.name,
-          Id: key.id,
-          Overwrite: false
+          was: oldName,
+          now: key.name,
+          id: key.id,
+          overwrite: false
         }
         callback(null, result)
       })
     }),
 
     import: promisify((name, pem, password, callback) => {
-      self._keychain.importKey(name, pem, password, (err, key) => {
-        if (err) return callback(err)
-        callback(null, toKeyInfo(key))
-      })
+      self._keychain.importKey(name, pem, password, callback)
     }),
 
     export: promisify((name, password, callback) => {
