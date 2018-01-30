bab526ee1512ecdbd0467cd7dfa79f8c143c4c92
Richard Schneider <makaretu@gmail.com>
2018-01-30 17:14:26 +1300

    fix: call to daemon factory

--- test/http-api/interface/key.js
+++ test/http-api/interface/key.js
@@ -8,7 +8,7 @@ const parallel = require('async/parallel')
 const DaemonFactory = require('ipfsd-ctl')
 const df = DaemonFactory.create({ exec: 'src/cli/bin.js' })
 const options = {
-  args: ['--pass ipfs-is-awesome-software']
+  args: ['--pass', 'ipfs-is-awesome-software']
 }
 
 const nodes = []
