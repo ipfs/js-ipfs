0239f58f9f400718b3de15a7fa41d099f023b0a5
Henrique Dias <hacdias@gmail.com>
2018-02-20 19:04:41 +0000

    feat: .stats.bw*

--- src/http/api/resources/stats.js
+++ src/http/api/resources/stats.js
@@ -1,7 +1,46 @@
 'use strict'
 
+const { Transform } = require('readable-stream')
+
+const transformBandwidth = (stat) => {
+  return {
+    TotalIn: stat.totalIn,
+    TotalOut: stat.totalOut,
+    RateIn: stat.rateIn,
+    RateOut: stat.rateOut
+  }
+}
+
 exports = module.exports
 
 exports.bitswap = require('./bitswap').stat
 
 exports.repo = require('./repo').stat
+
+exports.bw = (request, reply) => {
+  const ipfs = request.server.app.ipfs
+  const options = {
+    peer: request.query.peer,
+    proto: request.query.proto,
+    poll: request.query.poll === 'true',
+    interval: request.query.interval || '1s'
+  }
+
+  const res = ipfs.stats.bwReadableStream(options)
+  const output = new Transform({
+    writableObjectMode: true,
+    transform (chunk, encoding, cb) {
+      this.push(JSON.stringify(transformBandwidth(chunk)) + '\n')
+      cb()
+    }
+  })
+
+  request.on('disconnect', () => {
+    res.destroy()
+  })
+
+  res.pipe(output)
+  reply(output)
+    .header('content-type', 'application/json')
+    .header('x-chunked-output', '1')
+}
