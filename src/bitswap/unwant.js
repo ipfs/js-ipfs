73a153eac69de2f3f0856172b5887f7b8fe3b85c
Gar <gar+gh@danger.computer>
2018-06-18 09:03:43 -0700

    feat: implement bitswap wantlist peer ID param and bitswap unwant (#761)
    * fix(bitswap.unwant) Parse CID from arg
    
    * feat(bitswap.wantlist) add peer parameter
    
    * chore: simplify CID parsing for wantlist and unwant
    
    * chore: update interface-ipfs-core dependency
    
    * chore: upgrades interface-ipfs-core dependency
    
    License: MIT
    Signed-off-by: Alan Shaw <alan@tableflip.io>

--- src/bitswap/unwant.js
+++ src/bitswap/unwant.js
@@ -1,16 +1,24 @@
 'use strict'
 
 const promisify = require('promisify-es6')
+const CID = require('cids')
 
 module.exports = (send) => {
-  return promisify((args, opts, callback) => {
+  return promisify((cid, opts, callback) => {
     if (typeof (opts) === 'function') {
       callback = opts
       opts = {}
     }
+
+    try {
+      cid = new CID(cid)
+    } catch (err) {
+      return callback(err)
+    }
+
     send({
       path: 'bitswap/unwant',
-      args: args,
+      args: cid.toBaseEncodedString(),
       qs: opts
     }, callback)
   })
--- src/bitswap/wantlist.js
+++ src/bitswap/wantlist.js
@@ -1,11 +1,30 @@
 'use strict'
 
 const promisify = require('promisify-es6')
+const CID = require('cids')
 
 module.exports = (send) => {
-  return promisify((callback) => {
+  return promisify((peerId, opts, callback) => {
+    if (typeof (peerId) === 'function') {
+      callback = peerId
+      opts = {}
+      peerId = null
+    } else if (typeof (opts) === 'function') {
+      callback = opts
+      opts = {}
+    }
+
+    if (peerId) {
+      try {
+        opts.peer = new CID(peerId).toBaseEncodedString()
+      } catch (err) {
+        return callback(err)
+      }
+    }
+
     send({
-      path: 'bitswap/wantlist'
+      path: 'bitswap/wantlist',
+      qs: opts
     }, callback)
   })
 }
