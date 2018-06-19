4e51a69cf908f10cac8570ed61c5cf87cdc99810
Alex Potsides <alex@achingbrain.net>
2018-06-19 15:58:16 +0200

    fix: do not stringify output of object data (#1398)
    Without this change:
    
    ```sh
    $ jsipfs object data `dd if=/dev/urandom bs=307200 count=1 | jsipfs files write --create /some-file && jsipfs files ls -h /some-file` | unixfs
    {"type":"file","blockSizes":[72481833443311,10909182287855]}
    ```
    
    e.g.
    
    ```
    00000000: 0802 18ef bfbd efbf bd12 20ef bfbd efbf  .......... .....
    00000010: bd10 20ef bfbd efbf bd02                 .. .......
    ```
    
    With this change:
    
    ```sh
    $ jsipfs object data `dd if=/dev/urandom bs=307200 count=1 | jsipfs files write --create /some-file && jsipfs files ls -h /some-file` | unixfs
    {"type":"file","blockSizes":[262144,45056]}
    ```
    
    e.g.
    
    ```
    00000000: 0802 1880 e012 2080 8010 2080 e002       ...... ... ...
    ```
    
    Stringifying the data changes it's encoding. Been banging my head against this all morning.

--- test/utils/ipfs-exec.js
+++ test/utils/ipfs-exec.js
@@ -29,9 +29,11 @@ module.exports = (repoPath, opts) => {
   }, opts)
 
   const exec = (args) => execa(`${process.cwd()}/src/cli/bin.js`, args, config)
+  const execRaw = (args) => execa(`${process.cwd()}/src/cli/bin.js`, args, Object.assign({}, config, {
+    encoding: null
+  }))
 
-  function ipfs () {
-    let args = Array.from(arguments)
+  const execute = (exec, args) => {
     if (args.length === 1) {
       args = args[0].split(' ')
     }
@@ -49,6 +51,15 @@ module.exports = (repoPath, opts) => {
     res.stderr = cp.stderr
 
     return res
+  }
+
+  function ipfs () {
+    return execute(exec, Array.from(arguments))
+  }
+
+  // Will return buffers instead of strings
+  ipfs.raw = function () {
+    return execute(execRaw, Array.from(arguments))
   }
 
   /**
