3acd6fd0cfcc3bb8f4309c07ae6fa22724c1d174
Alan Shaw <alan@tableflip.io>
2018-06-26 13:16:17 +0100

    fix: do not rely on discovery for ping tests
    fixes #310
    
    License: MIT
    Signed-off-by: Alan Shaw <alan@tableflip.io>

--- js/src/utils/spawn.js
+++ js/src/utils/spawn.js
@@ -1,16 +1,18 @@
 const waterfall = require('async/waterfall')
 const timesSeries = require('async/timesSeries')
+const map = require('async/map')
+
+function identify (node, cb) {
+  node.id((err, id) => {
+    if (err) return cb(err)
+    node.peerId = id
+    cb(null, node)
+  })
+}
 
 // Spawn a node, get it's id and set it as `peerId` on the node
 function spawnNodeWithId (factory, callback) {
-  waterfall([
-    (cb) => factory.spawnNode(cb),
-    (node, cb) => node.id((err, id) => {
-      if (err) return cb(err)
-      node.peerId = id
-      cb(null, node)
-    })
-  ], callback)
+  waterfall([(cb) => factory.spawnNode(cb), identify], callback)
 }
 
 exports.spawnNodeWithId = spawnNodeWithId
@@ -24,7 +26,10 @@ exports.spawnNodes = spawnNodes
 
 // Spawn n nodes, getting their id's and setting them as `peerId` on the nodes
 function spawnNodesWithId (n, factory, callback) {
-  timesSeries(n, (_, cb) => spawnNodeWithId(factory, cb), callback)
+  spawnNodes(n, factory, (err, nodes) => {
+    if (err) return callback(err)
+    map(nodes, identify, callback)
+  })
 }
 
 exports.spawnNodesWithId = spawnNodesWithId
