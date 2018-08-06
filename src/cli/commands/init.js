0140c5e58b5cdd931cf4362d85bdd45c4df2baf0
Maciej Krüger <mkg20001@gmail.com>
2018-08-06 15:04:52 +0200

    feat: Rename pregenId to privateKey

--- src/cli/commands/init.js
+++ src/cli/commands/init.js
@@ -24,6 +24,11 @@ module.exports = {
         type: 'boolean',
         describe: "Don't add and pin help files to the local storage"
       })
+      .option('privateKey', {
+        alias: 'k',
+        type: 'string',
+        describe: 'Pre-generated private key to use for the repo'
+      })
   },
 
   handler (argv) {
@@ -39,6 +44,7 @@ module.exports = {
 
     node.init({
       bits: argv.bits,
+      privateKey: argv.privateKey,
       emptyRepo: argv.emptyRepo,
       pass: argv.pass,
       log: print
