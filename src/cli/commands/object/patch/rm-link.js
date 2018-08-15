75c1a7b8b07677f56081aa16e3a098b6019bc876
Alan Shaw <alan@tableflip.io>
2018-08-15 11:50:03 +0100

    fix: object.patch.rmLink not working
    License: MIT
    Signed-off-by: Alan Shaw <alan@tableflip.io>

--- src/cli/commands/object/patch/rm-link.js
+++ src/cli/commands/object/patch/rm-link.js
@@ -1,6 +1,5 @@
 'use strict'
 
-const DAGLink = require('ipld-dag-pb').DAGLink
 const debug = require('debug')
 const log = debug('cli:object')
 log.error = debug('cli:object:error')
@@ -14,12 +13,7 @@ module.exports = {
   builder: {},
 
   handler (argv) {
-    // TODO rmLink should support removing by name and/or multihash
-    // without having to know everything, which in fact it does, however,
-    // since it expectes a DAGLink type, we have to pass some fake size and
-    // hash.
-    const link = new DAGLink(argv.link, 1, 'Qm')
-    argv.ipfs.object.patch.rmLink(argv.root, link, {
+    argv.ipfs.object.patch.rmLink(argv.root, { name: argv.link }, {
       enc: 'base58'
     }, (err, node) => {
       if (err) {
