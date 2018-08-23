4a01484d849ccbcab2b77e493d266d068dd5a25c
Alan Shaw <alan@tableflip.io>
2018-08-23 12:31:23 +0100

    fix: dnslink for ipfs.io has changed
    License: MIT
    Signed-off-by: Alan Shaw <alan@tableflip.io>

--- test/cli/dns.js
+++ test/cli/dns.js
@@ -16,7 +16,7 @@ describe('dns', () => runOnAndOff((thing) => {
     this.timeout(60 * 1000)
 
     return ipfs('dns ipfs.io').then((res) => {
-      expect(res.substr(0, 6)).to.eql('/ipfs/')
+      expect(res.substr(0, 6)).to.eql('/ipns/')
     })
   })
 }))
