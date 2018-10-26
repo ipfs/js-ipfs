893cd2e3a1a185e39d5212c34d90a50a7a477309
Jacob Heun <jacobheun@gmail.com>
2018-10-26 16:31:36 +0200

    test: remove dupe api tests
    The http-api test suite was doing redundant work. The removed
    tests were spinning up a daemon to run the http-api tests against.
    This duplicates the behavior the cli tests are already doing. Also,
    the inject http-api tests, test the api directly, without spinning up
    a daemon. This is a better approach and makes these deleted tests
    redundant and useless. This also shaves off a significant time from
    the test suite.
    
    Resolves https://github.com/ipfs/js-ipfs/issues/1666

--- test/http-api/index.js
+++ test/http-api/index.js
@@ -1,12 +1,3 @@
 'use strict'
 
-require('./block')
-require('./bootstrap')
-require('./config')
-require('./dns')
-require('./id')
 require('./inject')
-require('./interface')
-require('./object')
-require('./version')
-require('./files')
