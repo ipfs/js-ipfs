efd8411e973cc8e5116c9c430b4faea929cb176e
Alan Shaw <alan.shaw@protocol.ai>
2018-11-09 16:08:57 +0000

    perf: lazy load IPLD formats
    This PR uses the new `loadFormat` option for IPLD to lazily require IPLD formats in order to reduce the startup time for the node.
    
    If you're feeling like you've seen this before then, for reference:
    
    The PR https://github.com/ipld/js-ipld/pull/164 undid the work done in https://github.com/ipld/js-ipld/pull/145 and https://github.com/ipld/js-ipld/pull/178 re-enabled lazy loading.
    
    License: MIT
    Signed-off-by: Alan Shaw <alan.shaw@protocol.ai>

--- src/core/index.js
+++ src/core/index.js
@@ -15,21 +15,6 @@ const debug = require('debug')
 const extend = require('deep-extend')
 const EventEmitter = require('events')
 
-// All known IPLD formats
-const ipldBitcoin = require('ipld-bitcoin')
-const ipldDagCbor = require('ipld-dag-cbor')
-const ipldDagPb = require('ipld-dag-pb')
-const ipldEthAccountSnapshot = require('ipld-ethereum').ethAccountSnapshot
-const ipldEthBlock = require('ipld-ethereum').ethBlock
-const ipldEthBlockList = require('ipld-ethereum').ethBlockList
-const ipldEthStateTrie = require('ipld-ethereum').ethStateTrie
-const ipldEthStorageTrie = require('ipld-ethereum').ethStorageTrie
-const ipldEthTrie = require('ipld-ethereum').ethTxTrie
-const ipldEthTx = require('ipld-ethereum').ethTx
-const ipldGit = require('ipld-git')
-const ipldRaw = require('ipld-raw')
-const ipldZcash = require('ipld-zcash')
-
 const config = require('./config')
 const boot = require('./boot')
 const components = require('./components')
@@ -38,6 +23,40 @@ const IPNS = require('./ipns')
 const defaultRepo = require('./runtime/repo-nodejs')
 const preload = require('./preload')
 const mfsPreload = require('./mfs-preload')
+
+// All known (non-default) IPLD formats
+const IpldFormats = {
+  get 'bitcoin-block' () {
+    return require('ipld-bitcoin')
+  },
+  get 'eth-account-snapshot' () {
+    return require('ipld-ethereum').ethAccountSnapshot
+  },
+  get 'eth-block' () {
+    return require('ipld-ethereum').ethBlock
+  },
+  get 'eth-block-list' () {
+    return require('ipld-ethereum').ethBlockList
+  },
+  get 'eth-state-trie' () {
+    return require('ipld-ethereum').ethStateTrie
+  },
+  get 'eth-storage-trie' () {
+    return require('ipld-ethereum').ethStorageTrie
+  },
+  get 'eth-tx' () {
+    return require('ipld-ethereum').ethTx
+  },
+  get 'eth-tx-trie' () {
+    return require('ipld-ethereum').ethTxTrie
+  },
+  get 'git-raw' () {
+    return require('ipld-git')
+  },
+  get 'zcash-block' () {
+    return require('ipld-zcash')
+  }
+}
 
 class IPFS extends EventEmitter {
   constructor (options) {
@@ -99,11 +118,11 @@ class IPFS extends EventEmitter {
     this._blockService = new BlockService(this._repo)
     this._ipld = new Ipld({
       blockService: this._blockService,
-      formats: [
-        ipldBitcoin, ipldDagCbor, ipldDagPb, ipldEthAccountSnapshot,
-        ipldEthBlock, ipldEthBlockList, ipldEthStateTrie, ipldEthStorageTrie,
-        ipldEthTrie, ipldEthTx, ipldGit, ipldRaw, ipldZcash
-      ]
+      loadFormat: (codec, callback) => {
+        this.log('Loading IPLD format', codec)
+        if (IpldFormats[codec]) return callback(null, IpldFormats[codec])
+        callback(new Error(`Missing IPLD format "${codec}"`))
+      }
     })
     this._preload = preload(this)
     this._mfsPreload = mfsPreload(this)
