af8b5d615305f12cc29e2fe1695052ea2cae44e1
Alan Shaw <alan.shaw@protocol.ai>
2018-11-20 23:26:07 +0000

    fix: object stat CLI
    License: MIT
    Signed-off-by: Alan Shaw <alan.shaw@protocol.ai>

--- src/cli/commands/object/stat.js
+++ src/cli/commands/object/stat.js
@@ -1,21 +1,13 @@
 'use strict'
 
-const multibase = require('multibase')
 const print = require('../../utils').print
-const { cidToString } = require('../../../utils/cid')
 
 module.exports = {
   command: 'stat <key>',
 
   describe: 'Get stats for the DAG node named by <key>',
 
-  builder: {
-    'cid-base': {
-      describe: 'Number base to display CIDs in.',
-      type: 'string',
-      choices: multibase.names
-    }
-  },
+  builder: {},
 
   handler ({ ipfs, key, cidBase }) {
     ipfs.object.stat(key, { enc: 'base58' }, (err, stats) => {
@@ -26,7 +18,7 @@ module.exports = {
       delete stats.Hash // only for js-ipfs-http-client output
 
       Object.keys(stats).forEach((key) => {
-        print(`${key}: ${cidToString(stats[key], cidBase)}`)
+        print(`${key}: ${stats[key]}`)
       })
     })
   }
