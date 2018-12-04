265b610212f257cc0793fda81fa220ab6aa75943
achingbrain <alex@achingbrain.net>
2018-12-04 17:09:12 +0000

    chore: fix linting
    License: MIT
    Signed-off-by: achingbrain <alex@achingbrain.net>

--- test/ls.spec.js
+++ test/ls.spec.js
@@ -80,20 +80,19 @@ describe('ls', function () {
 
   methods.forEach(method => {
     describe(`ls ${method.name}`, function () {
-      it('lists the root directory by default', () => {
+      it('lists the root directory by default', async () => {
         const fileName = `small-file-${Math.random()}.txt`
         const content = Buffer.from('Hello world')
-    
-        return mfs.write(`/${fileName}`, content, {
+
+        await mfs.write(`/${fileName}`, content, {
           create: true
         })
-          .then(() => method.ls())
-          .then((result) => method.collect(result))
-          .then(files => {
-            expect(files.find(file => file.name === fileName)).to.be.ok()
-          })
+        const result = await method.ls()
+        const files = await method.collect(result)
+
+        expect(files.find(file => file.name === fileName)).to.be.ok()
       })
-    
+
       it('refuses to lists files with an empty path', () => {
         return method.ls('')
           .then((result) => method.collect(result))
@@ -104,7 +103,7 @@ describe('ls', function () {
             expect(error.message).to.contain('paths must not be empty')
           })
       })
-    
+
       it('refuses to lists files with an invalid path', () => {
         return method.ls('not-valid')
           .then((result) => method.collect(result))
@@ -115,12 +114,12 @@ describe('ls', function () {
             expect(error.message).to.contain('paths must start with a leading /')
           })
       })
-    
+
       it('lists files in a directory', () => {
         const dirName = `dir-${Math.random()}`
         const fileName = `small-file-${Math.random()}.txt`
         const content = Buffer.from('Hello world')
-    
+
         return mfs.write(`/${dirName}/${fileName}`, content, {
           create: true,
           parents: true
@@ -135,12 +134,12 @@ describe('ls', function () {
             expect(files[0].hash).to.equal('')
           })
       })
-    
+
       it('lists files in a directory with meta data', () => {
         const dirName = `dir-${Math.random()}`
         const fileName = `small-file-${Math.random()}.txt`
         const content = Buffer.from('Hello world')
-    
+
         return mfs.write(`/${dirName}/${fileName}`, content, {
           create: true,
           parents: true
@@ -156,11 +155,11 @@ describe('ls', function () {
             expect(files[0].size).to.equal(content.length)
           })
       })
-    
+
       it('lists a file', () => {
         const fileName = `small-file-${Math.random()}.txt`
         const content = Buffer.from('Hello world')
-    
+
         return mfs.write(`/${fileName}`, content, {
           create: true
         })
@@ -174,11 +173,11 @@ describe('ls', function () {
             expect(files[0].hash).to.equal('')
           })
       })
-    
+
       it('lists a file with meta data', () => {
         const fileName = `small-file-${Math.random()}.txt`
         const content = Buffer.from('Hello world')
-    
+
         return mfs.write(`/${fileName}`, content, {
           create: true
         })
@@ -193,11 +192,11 @@ describe('ls', function () {
             expect(files[0].size).to.equal(content.length)
           })
       })
-    
+
       it('lists a file with a base32 hash', () => {
         const fileName = `small-file-${Math.random()}.txt`
         const content = Buffer.from('Hello world')
-    
+
         return mfs.write(`/${fileName}`, content, {
           create: true
         })
@@ -214,7 +213,7 @@ describe('ls', function () {
             expect(files[0].hash.startsWith('b')).to.equal(true)
           })
       })
-    
+
       it('fails to list non-existent file', () => {
         return method.ls('/i-do-not-exist')
           .then((result) => method.collect(result))
@@ -225,53 +224,53 @@ describe('ls', function () {
             expect(error.message).to.contain('does not exist')
           })
       })
-    
+
       it('lists a sharded directory contents', async () => {
         const shardSplitThreshold = 10
         const fileCount = 11
         const dirPath = await createShardedDirectory(mfs, shardSplitThreshold, fileCount)
-    
+
         const files = await method.collect(await method.ls(dirPath, {
           long: true
         }))
-    
+
         expect(files.length).to.equal(fileCount)
-    
+
         files.forEach(file => {
           // should be a file
           expect(file.type).to.equal(0)
         })
       })
-    
+
       it('lists a file inside a sharded directory directly', async () => {
         const dirPath = await createShardedDirectory(mfs)
-    
-        const files =  await method.collect(await method.ls(dirPath, {
+
+        const files = await method.collect(await method.ls(dirPath, {
           long: true
         }))
-    
+
         const filePath = `${dirPath}/${files[0].name}`
-    
+
         // should be able to ls new file directly
         expect(await method.collect(await method.ls(filePath, {
           long: true
         }))).to.not.be.empty()
       })
-    
+
       it('lists the contents of a directory inside a sharded directory', async () => {
         const shardedDirPath = await createShardedDirectory(mfs)
         const dirPath = `${shardedDirPath}/subdir-${Math.random()}`
         const fileName = `small-file-${Math.random()}.txt`
-    
+
         await mfs.mkdir(`${dirPath}`)
         await mfs.write(`${dirPath}/${fileName}`, Buffer.from([0, 1, 2, 3]), {
           create: true
         })
-    
+
         const files = await method.collect(await method.ls(dirPath, {
           long: true
         }))
-    
+
         expect(files.length).to.equal(1)
         expect(files.filter(file => file.name === fileName)).to.be.ok()
       })
