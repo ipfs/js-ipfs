63c4897d05f08a68c5713a8d659f23998096d9aa
Maciej Krüger <mkg20001@gmail.com>
2019-01-21 13:35:19 +0100

    misc: add comment about optional listening

--- src/core/runtime/libp2p-nodejs.js
+++ src/core/runtime/libp2p-nodejs.js
@@ -14,6 +14,8 @@ const multiaddr = require('multiaddr')
 
 class Node extends libp2p {
   constructor (_options) {
+    // this can be replaced once optional listening is supported with the below code. ref: https://github.com/libp2p/interface-transport/issues/41
+    // const wsstar = new WebSocketStar({ id: _options.peerInfo.id })
     const wsstarServers = _options.peerInfo.multiaddrs.toArray().map(String).filter(addr => addr.includes('p2p-websocket-star'))
     _options.peerInfo.multiaddrs.replace(wsstarServers.map(multiaddr), '/p2p-websocket-star') // the ws-star-multi module will replace this with the chosen ws-star servers
     const wsstar = new WebSocketStarMulti({ servers: wsstarServers, id: _options.peerInfo.id, ignore_no_online: !wsstarServers.length || _options.wsStarIgnoreErrors })
