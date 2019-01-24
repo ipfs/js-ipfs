92ddb0405488fe793ff85a9e5a9eb7c9f37fcb9b
Chance Hudson <jchancehud@gmail.com>
2019-01-24 05:26:11 -0600

    Resolve comments

--- src/core/runtime/dns-nodejs.js
+++ src/core/runtime/dns-nodejs.js
@@ -1,6 +1,7 @@
 'use strict'
 
 const dns = require('dns')
+const _ = require('lodash');
 
 module.exports = (domain, opts, callback) => {
   resolveDnslink(domain)
@@ -8,49 +9,40 @@ module.exports = (domain, opts, callback) => {
       // If the code is not ENOTFOUND then throw the error
       if (err.code !== 'ENOTFOUND') throw err
 
-      if (domain.indexOf('_dnslink') === -1) {
-        // Check the _dnslink subdomain
-        const _dnslinkDomain = ['_dnslink', ...domain.split('.')].join('.')
-        // If this throws then we propagate the error
-        return resolveDnslink(_dnslinkDomain)
-      } else if (domain.split('.').indexOf('_dnslink') === 0) {
+      if (domain.startsWith('_dnslink.')) {
         // The supplied domain contains a _dnslink component
         // Check the non-_dnslink domain
-        const rootDomain = domain.split('.').slice(1).join('.')
+        const rootDomain = domain.replace('_dnslink.', '')
         return resolveDnslink(rootDomain)
       }
-      throw err
+      // Check the _dnslink subdomain
+      const _dnslinkDomain = `_dnslink.${domain}`;
+      // If this throws then we propagate the error
+      return resolveDnslink(_dnslinkDomain)
     })
     .then(dnslinkRecord => {
-      callback(null, dnslinkRecord.substr(8, dnslinkRecord.length - 1))
+      callback(null, dnslinkRecord.replace('dnslink=', ''))
     })
     .catch(callback)
 }
 
 function resolveDnslink(domain) {
   const DNSLINK_REGEX = /^dnslink=.+$/
-  return new Promise((resolve, reject) => {
+  return new Promise( (resolve, reject) => {
     dns.resolveTxt(domain, (err, records) => {
       if (err) return reject(err)
       resolve(records)
     })
   })
     .then(records => {
-      // records is an array of arrays of strings
-      // the below expression flattens it into an array of strings
-      const flatRecords = [].concat(...records)
-      return flatRecords.filter(record => {
+      return _.chain(records).flatten().filter(record => {
         return DNSLINK_REGEX.test(record)
-      })
+      }).value()
     })
     .then(dnslinkRecords => {
       // we now have dns text entries as an array of strings
       // only records passing the DNSLINK_REGEX text are included
-      if (dnslinkRecords > 1) {
-        const err = new Error(`Multiple dnslink records found for domain: ${domain}`)
-        err.code = 'EMULTFOUND'
-        throw err
-      } else if (dnslinkRecords.length === 0) {
+      if (dnslinkRecords.length === 0) {
         const err = new Error(`No dnslink records found for domain: ${domain}`)
         err.code = 'ENOTFOUND'
         throw err
