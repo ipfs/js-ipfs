37631d9b7211f3165a20d7cb9f1b1552a49c0ec2
Oli Evans <oli@tableflip.io>
2019-02-07 12:45:17 +0000

    docs: add example for js-ipfs with create-react-app v2 (#1871)
    License: MIT
    Signed-off-by: Oli Evans <oli@tableflip.io>

---
+++ examples/browser-create-react-app/src/hooks/use-ipfs-factory.js
@@ -0,0 +1,66 @@
+import Ipfs from 'ipfs'
+import { useEffect, useState } from 'react'
+
+let ipfs = null
+
+/*
+ * A quick demo using React hooks to create an ipfs instance.
+ *
+ * Hooks are brand new at the time of writing, and this pattern
+ * is intended to show it is possible. I don't know if it is wise.
+ *
+ * Next steps would be to store the ipfs instance on the context
+ * so use-ipfs calls can grab it from there rather than expecting
+ * it to be passed in.
+ */
+export default function useIpfsFactory ({commands}) {
+  const [isIpfsReady, setIpfsReady] = useState(!!ipfs)
+  const [ipfsInitError, setIpfsInitError] = useState(null)
+
+  useEffect(() => {
+    // The fn to useEffect should not return anything other than a cleanup fn,
+    // So it cannot be marked async, which causes it to return a promise,
+    // Hence we delegate to a async fn rather than making the param an async fn.
+    startIpfs()
+    return function cleanup () {
+      if (ipfs && ipfs.stop) {
+        console.log('Stopping IPFS')
+        ipfs.stop()
+        setIpfsReady(false)
+      }
+    }
+  }, [])
+
+  async function startIpfs () {
+    if (ipfs) {
+      console.log('IPFS already started')
+
+    } else if (window.ipfs && window.ipfs.enable) {
+      console.log('Found window.ipfs')
+      ipfs = await window.ipfs.enable({commands})
+
+    } else {
+      try {
+        console.time('IPFS Started')
+        ipfs = await promiseMeJsIpfs(Ipfs, {})
+        console.timeEnd('IPFS Started')
+      } catch (error) {
+        console.error('IPFS init error:', error)
+        ipfs = null
+        setIpfsInitError(error)
+      }
+    }
+
+    setIpfsReady(!!ipfs)
+  }
+
+  return {ipfs, isIpfsReady, ipfsInitError}
+}
+
+function promiseMeJsIpfs (Ipfs, opts) {
+  return new Promise((resolve, reject) => {
+    const ipfs = new Ipfs(opts)
+    ipfs.once('ready', () => resolve(ipfs))
+    ipfs.once('error', err => reject(err))
+  })
+}
