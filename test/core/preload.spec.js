cab58e116559f89fd8255dace0d4a60886bfa353
Alan Shaw <alan.shaw@protocol.ai>
2019-04-04 09:55:44 +0100

    chore: increase preload test timeouts
    License: MIT
    Signed-off-by: Alan Shaw <alan.shaw@protocol.ai>

--- test/core/preload.spec.js
+++ test/core/preload.spec.js
@@ -353,9 +353,15 @@ describe('preload disabled', function () {
 
   afterEach((done) => MockPreloadNode.clearPreloadCids(done))
 
-  after((done) => ipfs.stop(done))
+  after(function (done) {
+    this.timeout(50 * 1000)
+    ipfs.stop(done)
+  })
 
-  after((done) => repo.teardown(done))
+  after(function (done) {
+    this.timeout(50 * 1000)
+    repo.teardown(done)
+  })
 
   it('should not preload if disabled', (done) => {
     ipfs.add(Buffer.from(hat()), (err, res) => {
