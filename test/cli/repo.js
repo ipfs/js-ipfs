40ae451096c0f43a5f727161c99f6b8cddd7d2a4
Dirk McCormick <dirkmdev@gmail.com>
2019-05-10 08:29:54 -0400

    feat: expose GC to http api

--- test/cli/repo.js
+++ test/cli/repo.js
@@ -2,7 +2,12 @@
 'use strict'
 
 const expect = require('chai').expect
+const fs = require('fs')
+const os = require('os')
+const path = require('path')
 const repoVersion = require('ipfs-repo').repoVersion
+const hat = require('hat')
+const clean = require('../utils/clean')
 
 const runOnAndOff = require('../utils/on-and-off')
 
@@ -17,5 +22,35 @@ describe('repo', () => runOnAndOff((thing) => {
     return ipfs('repo version').then((out) => {
       expect(out).to.eql(`${repoVersion}\n`)
     })
+  })
+
+  // Note: There are more comprehensive GC tests in interface-js-ipfs-core
+  it('should run garbage collection', async () => {
+    // Create and add a file to IPFS
+    const filePath = path.join(os.tmpdir(), hat())
+    const content = String(Math.random())
+    fs.writeFileSync(filePath, content)
+    const cid = (await ipfs(`add -Q ${filePath}`)).trim()
+
+    // File hash should be in refs local
+    const localRefs = await ipfs('refs local')
+    expect(localRefs.split('\n')).includes(cid)
+
+    // Run GC, file should not have been removed because it's pinned
+    const gcOut = await ipfs('repo gc')
+    expect(gcOut.split('\n')).not.includes('Removed ' + cid)
+
+    // Unpin file
+    await ipfs('pin rm ' + cid)
+
+    // Run GC, file should now be removed
+    const gcOutAfterUnpin = await ipfs('repo gc')
+    expect(gcOutAfterUnpin.split('\n')).to.includes('Removed ' + cid)
+
+    const localRefsAfterGc = await ipfs('refs local')
+    expect(localRefsAfterGc.split('\n')).not.includes(cid)
+
+    // Clean up file
+    await clean(filePath)
   })
 }))
