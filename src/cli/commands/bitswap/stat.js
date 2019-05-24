d61534da1f2ef55a049089dd9cd86f4b6a7e4d5c
Prabhakar Poudel <yuvrajzohan@gmail.com>
2019-05-24 17:02:26 +0530

    feat(bitswap humanize): add humanize flag support to bitswap cli
    Add humanize option (--human) to bitswap stat cli command
    
    closes: #2057
    
    License: MIT
    Signed-off-by: Prabhakar-Poudel yuvrajzohan@gmail.com

--- src/cli/commands/bitswap/stat.js
+++ src/cli/commands/bitswap/stat.js
@@ -3,6 +3,7 @@
 const multibase = require('multibase')
 const { print } = require('../../utils')
 const { cidToString } = require('../../../utils/cid')
+const { humanize } = require('../../utils')
 
 module.exports = {
   command: 'stat',
@@ -14,20 +15,28 @@ module.exports = {
       describe: 'Number base to display CIDs in. Note: specifying a CID base for v0 CIDs will have no effect.',
       type: 'string',
       choices: multibase.names
+    },
+    human: {
+      describe: 'Print sizes in human readable format (e.g., 1K 234M 2G).',
+      type: 'boolean',
+      default: false
     }
   },
 
-  handler ({ getIpfs, cidBase, resolve }) {
+  handler ({ getIpfs, cidBase, resolve, human }) {
     resolve((async () => {
       const ipfs = await getIpfs()
       const stats = await ipfs.bitswap.stat()
       stats.wantlist = stats.wantlist.map(k => cidToString(k['/'], { base: cidBase, upgrade: false }))
       stats.peers = stats.peers || []
+      if (human) {
+        stats.dupDataReceived = humanize.Bytes(stats.dupDataReceived)
+      }
 
       print(`bitswap status
   blocks received: ${stats.blocksReceived}
   dup blocks received: ${stats.dupBlksReceived}
-  dup data received: ${stats.dupDataReceived}B
+  dup data received: ${stats.dupDataReceived}
   wantlist [${stats.wantlist.length} keys]
     ${stats.wantlist.join('\n    ')}
   partners [${stats.peers.length}]
