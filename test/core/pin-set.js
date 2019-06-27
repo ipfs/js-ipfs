c6954fd997bb546d48931d115a3a4bad641890cb
Dirk McCormick <dirkmdev@gmail.com>
2019-06-27 13:38:03 -0400

    test: pin-set add/remove tests

--- test/core/pin-set.js
+++ test/core/pin-set.js
@@ -9,6 +9,7 @@ chai.use(dirtyChai)
 
 const parallelLimit = require('async/parallelLimit')
 const series = require('async/series')
+const waterfall = require('async/waterfall')
 const {
   util: {
     cid,
@@ -108,11 +109,17 @@ describe('pinSet', function () {
       createNode('data', (err, result) => {
         expect(err).to.not.exist()
         const nodeHash = result.cid.toBaseEncodedString()
+
+        // Store a single pin
         pinSet.storeSet([nodeHash], (err, rootNode) => {
           expect(err).to.not.exist()
+
+          // Should have the correct CID
           expect(rootNode.cid.toBaseEncodedString()).to.eql(expectedRootHash)
+          // Node should have links to `defaultFanout` empty bins + one pin
           expect(rootNode.node.Links).to.have.length(defaultFanout + 1)
 
+          // Last link should be link to the pin
           const lastLink = rootNode.node.Links[rootNode.node.Links.length - 1]
           const mhash = lastLink.Hash.toBaseEncodedString()
           expect(mhash).to.eql(nodeHash)
@@ -126,22 +133,34 @@ describe('pinSet', function () {
     it('handles storing items > maxItems', function (done) {
       this.timeout(90 * 1000)
       const expectedHash = 'QmbvhSy83QWfgLXDpYjDmLWBFfGc8utoqjcXHyj3gYuasT'
+
+      // Once there are more than maxItems pins, the pins should be distributed
+      // across `defaultFanout` bins
       const count = maxItems + 1
       createNodes(count, (err, cids) => {
         expect(err).to.not.exist()
+
         pinSet.storeSet(cids, (err, result) => {
           expect(err).to.not.exist()
 
           expect(result.node.size).to.eql(3184696)
+
+          // Pins should be distributed across `defaultFanout` bins
           expect(result.node.Links).to.have.length(defaultFanout)
           expect(result.cid.toBaseEncodedString()).to.eql(expectedHash)
 
+          // Load the pins in the first bin
           pinSet.loadSetAt(result.node, 0, (err, loaded) => {
             expect(err).to.not.exist()
+
+            // Should have 30 pins in it (the way the pins are created for
+            // this test is deterministic and will result in 30 pins falling
+            // into the first bin)
             expect(loaded).to.have.length(30)
             const hashes = loaded.map(l => new CID(l).toBaseEncodedString())
 
-            // just check the first node, assume all are children if successful
+            // Make sure hasDescendant() finds a pin inside a bin.
+            // Just check the first node, assume all are children if successful
             pinSet.hasDescendant(result.node, hashes[0], (err, has) => {
               expect(err).to.not.exist()
               expect(has).to.eql(true)
@@ -184,6 +203,114 @@ describe('pinSet', function () {
     })
   })
 
+  describe('add and remove', function () {
+    function loadThenCheck (ps, ks1, ks2, cb) {
+      ps.saveSet((err, result) => {
+        expect(err).to.not.exist()
+
+        const rootNode = DAGNode.create('pins', [{ Hash: result.cid }])
+        pinSet.loadSetAt(rootNode, 0, (err, loaded) => {
+          expect(err).to.not.exist()
+
+          expect([...ks1].sort()).eql([...ks2].sort())
+
+          cb()
+        })
+      })
+    }
+
+    it('adds pins', function (done) {
+      const testPinSet = new PinSet('recursive', store)
+
+      createNodes(5, (err, keys) => {
+        expect(err).to.not.exist()
+
+        testPinSet.addPins(keys, (err) => {
+          expect(err).to.not.exist()
+
+          loadThenCheck(testPinSet, testPinSet.pinKeys, keys, done)
+        })
+      })
+    })
+
+    it('removes pins', function (done) {
+      const testPinSet = new PinSet('recursive', store)
+
+      createNodes(5, (err, keys) => {
+        expect(err).to.not.exist()
+
+        series([
+          (cb) => testPinSet.addPins(keys, cb),
+          (cb) => testPinSet.rmPins([keys[1], keys[3]], cb),
+          (cb) => loadThenCheck(testPinSet, testPinSet.pinKeys, [keys[0], keys[2], keys[4]], cb)
+        ], done)
+      })
+    })
+
+    it('add existing pins has no effect', function (done) {
+      const testPinSet = new PinSet('recursive', store)
+
+      createNodes(5, (err, keys) => {
+        expect(err).to.not.exist()
+
+        waterfall([
+          (cb) => testPinSet.addPins(keys, cb),
+          (changed, cb) => testPinSet.addPins(keys, cb)
+        ], (err, changed) => {
+          expect(err).to.not.exist()
+
+          expect(changed).equal(false)
+          done()
+        })
+      })
+    })
+
+    it('remove non-existent pins has no effect', function (done) {
+      const testPinSet = new PinSet('recursive', store)
+
+      createNodes(5, (err, keys) => {
+        expect(err).to.not.exist()
+
+        waterfall([
+          (cb) => testPinSet.addPins(keys, cb),
+          (changed, cb) => testPinSet.rmPins([keys[1], keys[3]], cb),
+          (changed, cb) => testPinSet.rmPins([keys[1], keys[3]], cb)
+        ], (err, changed) => {
+          expect(err).to.not.exist()
+
+          expect(changed).equal(false)
+          done()
+        })
+      })
+    })
+
+    it('several adds and removes results in correct pin set', function (done) {
+      const testPinSet = new PinSet('recursive', store)
+
+      createNodes(5, (err, keys) => {
+        expect(err).to.not.exist()
+
+        series([
+          // [0, 1, 2, 3, 4]
+          (cb) => testPinSet.addPins(keys, cb),
+
+          // [0, -, -, -, 4]
+          (cb) => testPinSet.rmPins([keys[1], keys[2], keys[3]], cb),
+
+          // [0, 1, -, 3, 4]
+          (cb) => testPinSet.addPins([keys[0], keys[1], keys[3]], cb),
+
+          // [0, -, -, 3, 4]
+          (cb) => testPinSet.rmPins([keys[1], keys[2]], cb)
+        ], (err) => {
+          expect(err).to.not.exist()
+
+          loadThenCheck(testPinSet, testPinSet.pinKeys, [keys[0], keys[3], keys[4]], done)
+        })
+      })
+    })
+  })
+
   describe('walkItems', function () {
     it(`fails if node doesn't have a pin-set protobuf header`, function (done) {
       createNode('datum', (err, node) => {
@@ -200,45 +327,38 @@ describe('pinSet', function () {
     it('visits all links of a root node', function (done) {
       this.timeout(90 * 1000)
 
+      const binsPerNode = 5
+      const itemsPerNode = 20
+      const testPinSet = new PinSet('recursive', store, binsPerNode, itemsPerNode)
+
       const seenPins = []
       const stepPin = (link, idx, data) => seenPins.push({ link, idx, data })
       const seenBins = []
       const stepBin = (link, idx, data) => seenBins.push({ link, idx, data })
 
-      createNodes(maxItems + 1, (err, nodes) => {
+      // Generate enough pins that they will be distributed across a structure
+      // of bins that is a few levels deep
+      const numPins = binsPerNode * itemsPerNode * 5
+      createNodes(numPins, (err, nodes) => {
         expect(err).to.not.exist()
 
-        pinSet.storeSet(nodes, (err, result) => {
+        testPinSet.storeSet(nodes, (err, result) => {
           expect(err).to.not.exist()
 
-          pinSet.walkItems(result.node, { stepPin, stepBin }, err => {
+          testPinSet.walkItems(result.node, { stepPin, stepBin }, err => {
             expect(err).to.not.exist()
-            expect(seenPins).to.have.length(maxItems + 1)
-            expect(seenBins).to.have.length(defaultFanout)
-            done()
-          })
-        })
-      })
-    })
 
-    it('visits all non-fanout links of a root node', function (done) {
-      const seen = []
-      const stepPin = (link, idx, data) => seen.push({ link, idx, data })
+            // Walking the structure we should see every pin
+            expect(seenPins).to.have.length(numPins)
+            // The way pins are generated for the tests is deterministic, and
+            // will result in 80 bins being created
+            expect(seenBins).to.have.length(80)
 
-      createNodes(defaultFanout, (err, nodes) => {
-        expect(err).to.not.exist()
-
-        pinSet.storeSet(nodes, (err, result) => {
-          expect(err).to.not.exist()
-
-          pinSet.walkItems(result.node, { stepPin }, err => {
-            expect(err).to.not.exist()
-            expect(seen).to.have.length(defaultFanout)
-            expect(seen[0].idx).to.eql(defaultFanout)
-            seen.forEach(item => {
+            for (const item of seenPins) {
               expect(item.data).to.eql(Buffer.alloc(0))
               expect(item.link).to.exist()
-            })
+            }
+
             done()
           })
         })
@@ -248,19 +368,34 @@ describe('pinSet', function () {
 
   describe('getInternalCids', function () {
     it('gets all links and empty key CID', function (done) {
-      createNodes(defaultFanout, (err, nodes) => {
+      const binsPerNode = 5
+      const itemsPerNode = 20
+      const testPinSet = new PinSet('recursive', store, binsPerNode, itemsPerNode)
+
+      // Generate enough pins that they will be distributed across a structure
+      // of bins that is a few levels deep
+      const numPins = binsPerNode * itemsPerNode * 5
+
+      createNodes(numPins, (err, nodes) => {
         expect(err).to.not.exist()
 
-        pinSet.storeSet(nodes, (err, result) => {
+        testPinSet.storeSet(nodes, (err, result) => {
           expect(err).to.not.exist()
 
           const rootNode = DAGNode.create('pins', [{ Hash: result.cid }])
           PinSet.getInternalCids(store, rootNode, (err, cids) => {
             expect(err).to.not.exist()
-            expect(cids.length).to.eql(2)
+
+            // The way pins are generated for the tests is deterministic, and
+            // will result in 82 internal cids being created
+            expect(cids.length).to.eql(82)
+
             const cidStrs = cids.map(c => c.toString())
+            // Should include the empty key hash
             expect(cidStrs).includes(emptyKeyHash)
+            // Should include the set root node
             expect(cidStrs).includes(result.cid.toString())
+
             done()
           })
         })
