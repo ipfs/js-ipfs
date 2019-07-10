63d21250f9d6dc08d40f615e5cd341c08d5e3b50
dirkmc <dirkmdev@gmail.com>
2019-07-10 04:59:17 -0400

    GC endpoint (#992)
    * feat: gc
    
    * fix: better repo.gc() response handling
    
    * fix: repo.gc() cid handling
    
    * chore: update interface-ipfs-core

--- src/repo/gc.js
+++ src/repo/gc.js
@@ -1,6 +1,15 @@
 'use strict'
 
 const promisify = require('promisify-es6')
+const streamToValueWithTransformer = require('../utils/stream-to-value-with-transformer')
+const CID = require('cids')
+
+const transform = function (res, callback) {
+  callback(null, res.map(r => ({
+    err: r.Err ? new Error(r.Err) : null,
+    cid: (r.Key || {})['/'] ? new CID(r.Key['/']) : null
+  })))
+}
 
 module.exports = (send) => {
   return promisify((opts, callback) => {
@@ -8,9 +17,17 @@ module.exports = (send) => {
       callback = opts
       opts = {}
     }
-    send({
+
+    const request = {
       path: 'repo/gc',
       qs: opts
-    }, callback)
+    }
+    send(request, (err, result) => {
+      if (err) {
+        return callback(err)
+      }
+
+      streamToValueWithTransformer(result, transform, callback)
+    })
   })
 }
