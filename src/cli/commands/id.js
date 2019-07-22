54f9b78acd88b7d35092b3e024664d6e7961362f
Hugo Dias <hugomrdias@gmail.com>
2019-07-22 15:39:47 +0100

    fix:  rewrite cli id tests

--- src/cli/commands/id.js
+++ src/cli/commands/id.js
@@ -15,11 +15,7 @@ module.exports = {
   handler (argv) {
     argv.resolve((async () => {
       const ipfs = await argv.getIpfs()
-      // eslint-disable-next-line no-console
-      console.log('TCL: handler -> ipfs', ipfs)
       const id = await ipfs.id()
-      // eslint-disable-next-line no-console
-      console.log('TCL: handler -> id', id)
 
       return JSON.stringify(id, '', 2)
     })())
--- test/cli/id.js
+++ test/cli/id.js
@@ -1,22 +1,44 @@
 /* eslint-env mocha */
 'use strict'
 
-const expect = require('chai').expect
-const parser = require('../../src/cli/parser')
+const sinon = require('sinon')
+const chai = require('chai')
+const dirtyChai = require('dirty-chai')
+const expect = chai.expect
 const YargsPromise = require('yargs-promise')
+const clearModule = require('clear-module')
+chai.use(dirtyChai)
 
 // eslint-disable-next-line no-only-tests/no-only-tests
 describe.only('id', () => {
-  const cli = new YargsPromise(parser)
+  let cli
+  let cliUtils
+  beforeEach(() => {
+    cliUtils = require('../../src/cli/utils')
+    cli = new YargsPromise(require('../../src/cli/parser'))
+  })
+  afterEach(() => {
+    sinon.restore()
+    // TODO: the lines below shouldn't be necessary, cli needs refactor to simplify testability
+    // Force the next require to not use require cache
+    clearModule('../../src/cli/utils')
+    clearModule('../../src/cli/parser')
+  })
 
-  it('get the id', () => {
-    return cli.parse('id')
-      .then(({ data }) => {
-        const id = JSON.parse(data)
-        expect(id).to.have.property('id')
-        expect(id).to.have.property('publicKey')
-        expect(id).to.have.property('addresses')
-        return data
-      })
+  it('should output formatted json string', async () => {
+    const fakeId = sinon.fake.returns(
+      { id: 'id', publicKey: 'publicKey' }
+    )
+
+    sinon
+      .stub(cliUtils, 'getIPFS')
+      .callsArgWith(1, null, { id: fakeId })
+
+    // TODO: the lines below shouldn't be necessary, cli needs refactor to simplify testability
+    // Force the next require to not use require cache
+    clearModule('../../src/cli/commands/id.js')
+    const { data } = await cli.parse('id')
+
+    expect(data).to.eq('{\n  "id": "id",\n  "publicKey": "publicKey"\n}')
   })
 })
