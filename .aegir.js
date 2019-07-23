e18316e058f56e87aea7754e1c17d81aed1ca593
Hugo Dias <hugomrdias@gmail.com>
2019-07-23 10:49:46 +0100

    chore: reduce the bundle size max size

--- .aegir.js
+++ .aegir.js
@@ -8,7 +8,7 @@ const ipfsdServer = IPFSFactory.createServer()
 const preloadNode = MockPreloadNode.createNode()
 
 module.exports = {
-  bundlesize: { maxSize: '881kB' },
+  bundlesize: { maxSize: '689kB' },
   webpack: {
     resolve: {
       mainFields: ['browser', 'main'],
