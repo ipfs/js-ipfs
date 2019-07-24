e3405e0b86ac5306b8c8bf4f921ccbc85b0eacea
Hugo Dias <hugomrdias@gmail.com>
2019-07-24 15:55:09 +0100

    fix: feedback changes

--- src/cli/bin.js
+++ src/cli/bin.js
@@ -3,32 +3,32 @@
 /* eslint-disable no-console */
 'use strict'
 
+// Handle any uncaught errors
+process.once('uncaughtException', (err, origin) => {
+  if (origin === 'uncaughtException') {
+    console.error(err)
+    process.exit(1)
+  }
+})
+process.once('unhandledRejection', (err) => {
+  console.error(err)
+  process.exit(1)
+})
+
 const semver = require('semver')
+const pkg = require('../../package.json')
+// Check for node version
+if (!semver.satisfies(process.versions.node, pkg.engines.node)) {
+  console.error(`Please update your Node.js version to ${pkg.engines.node}`)
+  process.exit(1)
+}
+
 const YargsPromise = require('yargs-promise')
 const updateNotifier = require('update-notifier')
 const debug = require('debug')('ipfs:cli')
 const parser = require('./parser')
 const commandAlias = require('./command-alias')
 const { print } = require('./utils')
-const pkg = require('../../package.json')
-
-// Handle any uncaught error
-process.once('uncaughtException', (err, origin) => {
-  if (origin === 'uncaughtException') {
-    print(err.message)
-    debug(err)
-  }
-})
-process.once('unhandledRejection', (err) => {
-  print(err.message)
-  debug(err)
-})
-
-// Check for node version
-if (!semver.satisfies(process.versions.node, pkg.engines.node)) {
-  console.error(`Please update your Node.js version to ${pkg.engines.node}`)
-  process.exit(1)
-}
 
 // Check if an update is available and notify
 const oneWeek = 1000 * 60 * 60 * 24 * 7
