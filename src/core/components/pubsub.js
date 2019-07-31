406563ec51cd9dfa811ef586e566569ecf6154c8
Jacob Heun <jacobheun@gmail.com>
2019-07-31 10:03:59 +0200

    fix: correct pubsub methods for next libp2p release

--- src/core/components/pubsub.js
+++ src/core/components/pubsub.js
@@ -16,65 +16,56 @@ module.exports = function pubsub (self) {
         options = {}
       }
 
-      if (!self._options.EXPERIMENTAL.pubsub) {
+      if (!self.libp2p.pubsub) {
         return callback
           ? setImmediate(() => callback(errPubsubDisabled()))
           : Promise.reject(errPubsubDisabled())
       }
 
       if (!callback) {
-        return new Promise((resolve, reject) => {
-          self.libp2p.pubsub.subscribe(topic, options, handler, (err) => {
-            if (err) {
-              return reject(err)
-            }
-            resolve()
-          })
-        })
+        return self.libp2p.pubsub.subscribe(topic, handler, options)
       }
 
       self.libp2p.pubsub.subscribe(topic, options, handler, callback)
     },
 
     unsubscribe: (topic, handler, callback) => {
-      if (!self._options.EXPERIMENTAL.pubsub) {
+      if (!self.libp2p.pubsub) {
         return callback
           ? setImmediate(() => callback(errPubsubDisabled()))
           : Promise.reject(errPubsubDisabled())
       }
 
-      self.libp2p.pubsub.unsubscribe(topic, handler)
-
       if (!callback) {
-        return Promise.resolve()
+        return self.libp2p.pubsub.unsubscribe(topic, handler)
       }
 
-      setImmediate(() => callback())
+      self.libp2p.pubsub.unsubscribe(topic, handler, callback)
     },
 
     publish: promisify((topic, data, callback) => {
-      if (!self._options.EXPERIMENTAL.pubsub) {
+      if (!self.libp2p.pubsub) {
         return setImmediate(() => callback(errPubsubDisabled()))
       }
       self.libp2p.pubsub.publish(topic, data, callback)
     }),
 
     ls: promisify((callback) => {
-      if (!self._options.EXPERIMENTAL.pubsub) {
+      if (!self.libp2p.pubsub) {
         return setImmediate(() => callback(errPubsubDisabled()))
       }
       self.libp2p.pubsub.ls(callback)
     }),
 
     peers: promisify((topic, callback) => {
-      if (!self._options.EXPERIMENTAL.pubsub) {
+      if (!self.libp2p.pubsub) {
         return setImmediate(() => callback(errPubsubDisabled()))
       }
       self.libp2p.pubsub.peers(topic, callback)
     }),
 
     setMaxListeners (n) {
-      if (!self._options.EXPERIMENTAL.pubsub) {
+      if (!self.libp2p.pubsub) {
         throw errPubsubDisabled()
       }
       self.libp2p.pubsub.setMaxListeners(n)
