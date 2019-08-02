4af85605d31ecd4045403f44318ddaa836bae07b
Marcin Rataj <lidel@lidel.org>
2019-08-02 14:17:27 +0200

    fix: use node-fetch in electron-renderer
    `fetch` needs to be polyfilled before importing ky-universal:
    ky-universal won't use node-fetch because electron-renderer already has
    global.fetch defined and we can't use the one from electron-renderer as
    it returns different errors and makes HTTPS mocking impossible in tests
    
    License: MIT
    Signed-off-by: Marcin Rataj <lidel@lidel.org>

--- src/core/components/files-regular/add-from-url.js
+++ src/core/components/files-regular/add-from-url.js
@@ -1,8 +1,17 @@
 'use strict'
 
 const { URL } = require('iso-url')
-const { default: ky } = require('ky-universal')
 const nodeify = require('promise-nodeify')
+const { isElectronRenderer } = require('ipfs-utils/src/env')
+if (isElectronRenderer) {
+  // `fetch` needs to be polyfilled before importing ky-universal:
+  // ky-universal won't use node-fetch because electron-renderer already has global.fetch defined
+  // and we can't use the one from electron-renderer as it returns different
+  // errors and makes HTTPS mocking impossible in tests
+  // TODO: remove when upstream fix lands
+  global.fetch = require('node-fetch')
+}
+const { default: ky } = require('ky-universal')
 
 module.exports = (ipfs) => {
   const addFromURL = async (url, opts = {}) => {
