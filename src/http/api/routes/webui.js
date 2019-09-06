6876ffbac5ba7653f98c7886749c508e04fe51dc
Henrique Dias <hacdias@gmail.com>
2019-09-06 09:03:20 +0100

    feat: web ui 2.5.1

--- src/http/api/routes/webui.js
+++ src/http/api/routes/webui.js
@@ -26,7 +26,7 @@ module.exports = [
     method: '*',
     path: '/webui',
     handler (request, h) {
-      return h.redirect('/ipfs/QmNyMYhwJUS1cVvaWoVBhrW8KPj1qmie7rZcWo8f1Bvkhz')
+      return h.redirect('/ipfs/QmVTiRTQ72qiH4usAGT4c6qVxCMv4hFMUH9fvU6mktaXdP')
     }
   }
 ]
