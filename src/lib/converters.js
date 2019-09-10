0ff4d52478e6a1583edf917801c3bcaeb3036ec1
Alan Shaw <alan.shaw@protocol.ai>
2019-09-10 22:41:09 +0100

    refactor: use fetch for ipfs.cat
    License: MIT
    Signed-off-by: Alan Shaw <alan.shaw@protocol.ai>

--- src/lib/converters.js
+++ src/lib/converters.js
@@ -3,8 +3,11 @@
 const toPull = require('async-iterator-to-pull-stream')
 const all = require('async-iterator-all')
 const toStream = require('it-to-stream')
+const { Buffer } = require('buffer')
 
 exports.collectify = fn => (...args) => all(fn(...args))
+
+exports.concatify = fn => async (...args) => Buffer.concat(await all(fn(...args)))
 
 exports.pullify = {
   source: fn => (...args) => toPull(fn(...args)),
@@ -12,5 +15,6 @@ exports.pullify = {
 }
 
 exports.streamify = {
+  readable: fn => (...args) => toStream(fn(...args), { objectMode: true }),
   transform: fn => (...args) => toStream.transform(source => fn(source, ...args), { objectMode: true })
 }
--- src/lib/error-handler.js
+++ src/lib/error-handler.js
@@ -2,13 +2,25 @@
 
 const { HTTPError } = require('ky-universal')
 const log = require('debug')('ipfs-http-client:lib:error-handler')
+const { isNode } = require('ipfs-utils/src/env')
 
 function isJsonResponse (res) {
   return (res.headers.get('Content-Type') || '').startsWith('application/json')
 }
 
 module.exports = async function errorHandler (input, options, response) {
-  if (response.ok) return
+  if (response.ok) {
+    // FIXME: remove when fixed https://github.com/sindresorhus/ky-universal/issues/8
+    //
+    // ky clones the response for each handler. In Node.js the response body is
+    // piped to 2 PassThroughs, one becomes the real body and the other is used
+    // in the clone.
+    //
+    // If the body in the clone is not consumed or destroyed the highwater mark
+    // will be reached (for large payloads) and stop the real body from flowing.
+    if (isNode) response.body.destroy()
+    return
+  }
 
   let msg
 
