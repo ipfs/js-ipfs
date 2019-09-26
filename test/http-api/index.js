8f505a50e25380c1b5194786266dd05819457c04
achingbrain <alex@achingbrain.net>
2019-09-26 19:52:52 +0100

    refactor: remove reundant http tests

--- test/http-api/index.js
+++ test/http-api/index.js
@@ -1,12 +1,4 @@
 'use strict'
 
-require('./block')
-require('./bootstrap')
-require('./config')
-require('./dns')
-require('./id')
 require('./routes')
 require('./interface')
-require('./object')
-require('./version')
-require('./files')
