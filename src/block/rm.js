98bdff235a5ee4c1c5e2f6377bebbaadf540966e
achingbrain <alex@achingbrain.net>
2019-10-05 10:59:03 +0100

    chore: fix up block.rm tests

--- src/block/rm.js
+++ src/block/rm.js
@@ -30,29 +30,32 @@ module.exports = (createCommon, options) => {
     after((done) => common.teardown(done))
 
     it('should remove by CID object', async () => {
-      const cid = await ipfs.dag.put(Buffer.from(hat()))
-
-      // block should be present in the local store
-      expect(await ipfs.refs.local()).to.deep.include({
-        ref: cid.toString(),
-        err: ''
+      const cid = await ipfs.dag.put(Buffer.from(hat()), {
+        format: 'raw',
+        hashAlg: 'sha2-256'
       })
 
-      const result = await ipfs.block.rm(cid)
+      // block should be present in the local store
+      const localRefs = await ipfs.refs.local()
+      expect(localRefs).to.have.property('length').that.is.greaterThan(0)
+      expect(localRefs.find(ref => ref.ref === cid.toString())).to.be.ok()
 
+      const result = await ipfs.block.rm(cid)
       expect(result).to.be.an('array').and.to.have.lengthOf(1)
       expect(result[0]).to.have.property('hash', cid.toString())
       expect(result[0]).to.not.have.property('error')
 
       // did we actually remove the block?
-      expect(await ipfs.refs.local()).to.not.deep.include({
-        ref: cid.toString(),
-        err: ''
-      })
+      const localRefsAfterRemove = await ipfs.refs.local()
+      expect(localRefsAfterRemove).to.have.property('length').that.is.greaterThan(0)
+      expect(localRefsAfterRemove.find(ref => ref.ref === cid.toString())).to.not.be.ok()
     })
 
     it('should remove by CID in string', async () => {
-      const cid = await ipfs.dag.put(Buffer.from(hat()))
+      const cid = await ipfs.dag.put(Buffer.from(hat()), {
+        format: 'raw',
+        hashAlg: 'sha2-256'
+      })
       const result = await ipfs.block.rm(cid.toString())
 
       expect(result).to.be.an('array').and.to.have.lengthOf(1)
@@ -61,7 +64,10 @@ module.exports = (createCommon, options) => {
     })
 
     it('should remove by CID in buffer', async () => {
-      const cid = await ipfs.dag.put(Buffer.from(hat()))
+      const cid = await ipfs.dag.put(Buffer.from(hat()), {
+        format: 'raw',
+        hashAlg: 'sha2-256'
+      })
       const result = await ipfs.block.rm(cid.buffer)
 
       expect(result).to.be.an('array').and.to.have.lengthOf(1)
@@ -71,9 +77,18 @@ module.exports = (createCommon, options) => {
 
     it('should remove multiple CIDs', async () => {
       const cids = [
-        await ipfs.dag.put(Buffer.from(hat())),
-        await ipfs.dag.put(Buffer.from(hat())),
-        await ipfs.dag.put(Buffer.from(hat()))
+        await ipfs.dag.put(Buffer.from(hat()), {
+          format: 'raw',
+          hashAlg: 'sha2-256'
+        }),
+        await ipfs.dag.put(Buffer.from(hat()), {
+          format: 'raw',
+          hashAlg: 'sha2-256'
+        }),
+        await ipfs.dag.put(Buffer.from(hat()), {
+          format: 'raw',
+          hashAlg: 'sha2-256'
+        })
       ]
 
       const result = await ipfs.block.rm(cids)
@@ -87,7 +102,10 @@ module.exports = (createCommon, options) => {
     })
 
     it('should error when removing non-existent blocks', async () => {
-      const cid = await ipfs.dag.put(Buffer.from(hat()))
+      const cid = await ipfs.dag.put(Buffer.from(hat()), {
+        format: 'raw',
+        hashAlg: 'sha2-256'
+      })
 
       // remove it
       await ipfs.block.rm(cid)
@@ -100,7 +118,10 @@ module.exports = (createCommon, options) => {
     })
 
     it('should not error when force removing non-existent blocks', async () => {
-      const cid = await ipfs.dag.put(Buffer.from(hat()))
+      const cid = await ipfs.dag.put(Buffer.from(hat()), {
+        format: 'raw',
+        hashAlg: 'sha2-256'
+      })
 
       // remove it
       await ipfs.block.rm(cid)
@@ -114,10 +135,26 @@ module.exports = (createCommon, options) => {
     })
 
     it('should return empty output when removing blocks quietly', async () => {
-      const cid = await ipfs.dag.put(Buffer.from(hat()))
+      const cid = await ipfs.dag.put(Buffer.from(hat()), {
+        format: 'raw',
+        hashAlg: 'sha2-256'
+      })
       const result = await ipfs.block.rm(cid, { quiet: true })
 
       expect(result).to.be.an('array').and.to.have.lengthOf(0)
+    })
+
+    it('should error when removing pinned blocks', async () => {
+      const cid = await ipfs.dag.put(Buffer.from(hat()), {
+        format: 'raw',
+        hashAlg: 'sha2-256'
+      })
+      await ipfs.pin.add(cid.toString())
+
+      const result = await ipfs.block.rm(cid)
+
+      expect(result).to.be.an('array').and.to.have.lengthOf(1)
+      expect(result[0]).to.have.property('error').and.to.include('pinned')
     })
   })
 }
