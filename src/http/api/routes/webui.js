568ebebc0847acb525a754595981a566399789be
achingbrain <alex@achingbrain.net>
2019-10-08 16:07:18 +0100

    fix: redirect to public gateway if local not configured

--- src/http/api/routes/webui.js
+++ src/http/api/routes/webui.js
@@ -1,21 +1,36 @@
 'use strict'
 
 const multiaddr = require('multiaddr')
+const debug = require('debug')
+const log = debug('ipfs:webui:info')
+log.error = debug('ipfs:webui:error')
 
 module.exports = [
   {
     method: '*',
     path: '/webui',
     async handler (request, h) {
-      const { ipfs } = request.server.app
-      const gateway = await ipfs.config.get('Addresses.Gateway')
-      const addr = multiaddr(gateway)
-      const {
-        port,
-        address
-      } = addr.nodeAddress()
+      let scheme = 'http'
+      let port
+      let host
 
-      return h.redirect(`http://${address}:${port}/ipns/webui.ipfs.io`)
+      try {
+        const { ipfs } = request.server.app
+        const gateway = await ipfs.config.get('Addresses.Gateway')
+        const address = multiaddr(gateway).nodeAddress()
+
+        port = address.port
+        host = address.host
+      } catch (err) {
+        // may not have gateway configured
+        log.error(err)
+
+        scheme = 'https'
+        port = 443
+        host = 'gateway.ipfs.io'
+      }
+
+      return h.redirect(`${scheme}://${host}:${port}/ipns/webui.ipfs.io`)
     }
   }
 ]
