bc1a355e37354e277f5fa58261236668179601b4
Marcin Rataj <lidel@lidel.org>
2019-10-26 00:26:28 +0200

    feat: support CIDs in /ipns/ content paths
    This adds support for resolving PeerIDs as CIDs in /ipns/ paths.
    See https://github.com/libp2p/specs/issues/216 for full context.
    
    License: MIT
    Signed-off-by: Marcin Rataj <lidel@lidel.org>

--- test/http-api/inject/name.js
+++ test/http-api/inject/name.js
@@ -2,6 +2,7 @@
 /* eslint-env mocha */
 'use strict'
 
+const CID = require('cids')
 const { expect } = require('interface-ipfs-core/src/utils/mocha')
 const checkAll = (bits) => string => bits.every(bit => string.includes(bit))
 
@@ -40,6 +41,32 @@ module.exports = (http) => {
       res = await api.inject({
         method: 'GET',
         url: '/api/v0/name/resolve'
+      })
+
+      expect(res).to.exist()
+      expect(res.result.Path).to.satisfy(checkAll([`/ipfs/${cid}`]))
+    })
+
+    it('should publish and resolve a record with explicit CIDv1 in Base32', async function () {
+      this.timeout(160 * 1000)
+
+      // ensure PeerID is represented as CIDv1 in Base32
+      const { id } = await http.api._ipfs.id()
+      let cidv1 = new CID(id)
+      if (cidv1.version === 0) cidv1 = cidv1.toV1() // future-proofing
+      const peerIdAsCidv1b32 = cidv1.toString('base32')
+
+      let res = await api.inject({
+        method: 'GET',
+        url: `/api/v0/name/publish?arg=${cid}&resolve=false`
+      })
+
+      expect(res).to.exist()
+      expect(res.result.Value).to.equal(`/ipfs/${cid}`)
+
+      res = await api.inject({
+        method: 'GET',
+        url: `/api/v0/name/resolve?arg=${peerIdAsCidv1b32}`
       })
 
       expect(res).to.exist()
