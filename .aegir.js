12941acb8166e98134516e22c4f78c0a47463be6
Alan Shaw <alan.shaw@protocol.ai>
2019-11-08 12:10:17 +0000

    chore: update max bundle size

--- .aegir.js
+++ .aegir.js
@@ -10,7 +10,7 @@ const preloadNode = MockPreloadNode.createNode()
 const echoServer = EchoServer.createServer()
 
 module.exports = {
-  bundlesize: { maxSize: '689kB' },
+  bundlesize: { maxSize: '650kB' },
   webpack: {
     resolve: {
       mainFields: ['browser', 'main'],
