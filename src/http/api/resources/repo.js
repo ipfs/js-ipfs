39bc5b46b709049d443e0b90cf0b034d35e9e0e4
Pedro Santos <pedro.santos@moxy.studio>
2019-11-27 06:37:10 +0000

    feat: add human flag to repo stat cli command (#2630)

--- src/http/api/resources/repo.js
+++ src/http/api/resources/repo.js
@@ -35,8 +35,7 @@ exports.version = async (request, h) => {
 
 exports.stat = async (request, h) => {
   const { ipfs } = request.server.app
-  const human = request.query.human === 'true'
-  const stat = await ipfs.repo.stat({ human })
+  const stat = await ipfs.repo.stat()
 
   return h.response({
     NumObjects: stat.numObjects,
