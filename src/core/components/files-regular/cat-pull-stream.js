4074cfe627e263d67bba3152bc4298762f89c2d4
Alan Shaw <alan.shaw@protocol.ai>
2020-01-20 09:59:17 +0000

    refactor: renames and deletes prior to async/await

--- src/core/components/files-regular/cat-pull-stream.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-
-const toPullStream = require('async-iterator-to-pull-stream')
-
-module.exports = function (self) {
-  return function catPullStream (ipfsPath, options) {
-    return toPullStream.source(self._catAsyncIterator(ipfsPath, options))
-  }
-}
