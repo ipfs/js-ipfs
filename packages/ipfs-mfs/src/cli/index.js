28dfa93ec707f871db026f8df783c4c31b8c10b3
achingbrain <alex@achingbrain.net>
2020-02-27 21:13:22 +0000

    chore: restore whitespace

--- packages/ipfs-mfs/src/cli/index.js
+++ packages/ipfs-mfs/src/cli/index.js
@@ -2,10 +2,13 @@
 
 const command = {
   command: 'files <command>',
+
   description: 'Operations over mfs files (ls, mkdir, rm, etc)',
+
   builder (yargs) {
     return yargs.commandDir('.')
   },
+
   handler (argv) {
     argv.print('Type `jsipfs files --help` for more instructions')
   }
