bd40a9e555e68fb25b40fbab9fd62ed05c95a2b4
achingbrain <alex@achingbrain.net>
2020-02-28 21:10:18 +0000

    chore: print out data

--- examples/test-ipfs-example/utils.js
+++ examples/test-ipfs-example/utils.js
@@ -156,6 +156,8 @@ async function waitForOutput (expectedOutput, command, args = [], opts = {}) {
       foundExpectedOutput = true
       proc.kill()
       cancelTimeout()
+    } else {
+      console.info(expectedOutput, 'not in', output)
     }
   })
 
