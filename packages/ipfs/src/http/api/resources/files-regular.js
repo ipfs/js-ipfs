d4826e1075039e9ca4cb4bb8feb3919c3df50c4f
Paul Cowgill <pauldavidcowgill@gmail.com>
2020-03-09 12:59:47 -0500

    Logging incoming requests

--- packages/ipfs/src/http/api/resources/files-regular.js
+++ packages/ipfs/src/http/api/resources/files-regular.js
@@ -1,128 +1,148 @@
-'use strict'
+"use strict";
 
-const multipart = require('ipfs-multipart')
-const debug = require('debug')
-const tar = require('it-tar')
-const log = debug('ipfs:http-api:files')
-log.error = debug('ipfs:http-api:files:error')
-const toIterable = require('stream-to-it')
-const Joi = require('@hapi/joi')
-const Boom = require('@hapi/boom')
-const { PassThrough } = require('stream')
-const multibase = require('multibase')
-const isIpfs = require('is-ipfs')
-const { cidToString } = require('../../../utils/cid')
-const { Format } = require('../../../core/components/refs')
-const pipe = require('it-pipe')
-const all = require('it-all')
-const ndjson = require('iterable-ndjson')
-const { map } = require('streaming-iterables')
-const streamResponse = require('../../utils/stream-response')
+const multipart = require("ipfs-multipart");
+const debug = require("debug");
+const tar = require("it-tar");
+const log = debug("ipfs:http-api:files");
+log.error = debug("ipfs:http-api:files:error");
+const toIterable = require("stream-to-it");
+const Joi = require("@hapi/joi");
+const Boom = require("@hapi/boom");
+const { PassThrough } = require("stream");
+const multibase = require("multibase");
+const isIpfs = require("is-ipfs");
+const { cidToString } = require("../../../utils/cid");
+const { Format } = require("../../../core/components/refs");
+const pipe = require("it-pipe");
+const all = require("it-all");
+const ndjson = require("iterable-ndjson");
+const { map } = require("streaming-iterables");
+const streamResponse = require("../../utils/stream-response");
 
-const toBuffer = async function * (source) {
+const toBuffer = async function*(source) {
   for await (const chunk of source) {
-    yield chunk.slice()
+    yield chunk.slice();
   }
-}
+};
 
-function numberFromQuery (query, key) {
+function numberFromQuery(query, key) {
   if (query && query[key] !== undefined) {
-    const value = parseInt(query[key], 10)
+    const value = parseInt(query[key], 10);
 
     if (isNaN(value)) {
-      return undefined
+      return undefined;
     }
 
-    return value
+    return value;
   }
 }
 
 // common pre request handler that parses the args and returns `key` which is assigned to `request.pre.args`
 exports.parseKey = (request, h) => {
-  const { arg } = request.query
+  const { arg } = request.query;
 
   if (!arg) {
-    throw Boom.badRequest("Argument 'key' is required")
+    throw Boom.badRequest("Argument 'key' is required");
   }
 
-  const isArray = Array.isArray(arg)
-  const args = isArray ? arg : [arg]
+  const isArray = Array.isArray(arg);
+  const args = isArray ? arg : [arg];
   for (const arg of args) {
-    if (!isIpfs.ipfsPath(arg) && !isIpfs.cid(arg) && !isIpfs.ipfsPath('/ipfs/' + arg)) {
-      throw Boom.badRequest(`invalid ipfs ref path '${arg}'`)
+    if (
+      !isIpfs.ipfsPath(arg) &&
+      !isIpfs.cid(arg) &&
+      !isIpfs.ipfsPath("/ipfs/" + arg)
+    ) {
+      throw Boom.badRequest(`invalid ipfs ref path '${arg}'`);
     }
   }
 
   return {
     key: isArray ? args : arg,
     options: {
-      offset: numberFromQuery(request.query, 'offset'),
-      length: numberFromQuery(request.query, 'length')
+      offset: numberFromQuery(request.query, "offset"),
+      length: numberFromQuery(request.query, "length")
     }
-  }
-}
+  };
+};
 
 exports.cat = {
   // uses common parseKey method that returns a `key`
   parseArgs: exports.parseKey,
 
   // main route handler which is called after the above `parseArgs`, but only if the args were valid
-  handler (request, h) {
-    const { ipfs } = request.server.app
-    const { key, options } = request.pre.args
+  handler(request, h) {
+    const { ipfs } = request.server.app;
+    const { key, options } = request.pre.args;
 
     return streamResponse(request, h, () => ipfs.cat(key, options), {
-      onError (err) {
-        err.message = err.message === 'file does not exist'
-          ? err.message
-          : 'Failed to cat file: ' + err.message
+      onError(err) {
+        err.message =
+          err.message === "file does not exist"
+            ? err.message
+            : "Failed to cat file: " + err.message;
       }
-    })
+    });
   }
-}
+};
 
 exports.get = {
   // uses common parseKey method that returns a `key`
   parseArgs: exports.parseKey,
 
   // main route handler which is called after the above `parseArgs`, but only if the args were valid
-  handler (request, h) {
-    const { ipfs } = request.server.app
-    const { key } = request.pre.args
+  handler(request, h) {
+    const { ipfs } = request.server.app;
+    const { key } = request.pre.args;
 
-    return streamResponse(request, h, () => pipe(
-      ipfs.get(key),
-      async function * (source) {
-        for await (const file of source) {
-          const header = {
-            name: file.path
-          }
+    return streamResponse(request, h, () =>
+      pipe(
+        ipfs.get(key),
+        async function*(source) {
+          for await (const file of source) {
+            const header = {
+              name: file.path
+            };
 
-          if (file.content) {
-            yield { header: { ...header, size: file.size }, body: toBuffer(file.content) }
-          } else {
-            yield { header: { ...header, type: 'directory' } }
+            if (file.content) {
+              yield {
+                header: { ...header, size: file.size },
+                body: toBuffer(file.content)
+              };
+            } else {
+              yield { header: { ...header, type: "directory" } };
+            }
           }
-        }
-      },
-      tar.pack(),
-      toBuffer
-    ))
+        },
+        tar.pack(),
+        toBuffer
+      )
+    );
   }
-}
+};
 
 exports.add = {
   validate: {
     query: Joi.object()
       .keys({
-        'cid-version': Joi.number().integer().min(0).max(1).default(0),
-        'cid-base': Joi.string().valid(...multibase.names),
-        'raw-leaves': Joi.boolean(),
-        'only-hash': Joi.boolean(),
+        "cid-version": Joi.number()
+          .integer()
+          .min(0)
+          .max(1)
+          .default(0),
+        "cid-base": Joi.string().valid(...multibase.names),
+        "raw-leaves": Joi.boolean(),
+        "only-hash": Joi.boolean(),
         pin: Joi.boolean().default(true),
-        'wrap-with-directory': Joi.boolean(),
-        'file-import-concurrency': Joi.number().integer().min(0).default(50),
-        'block-write-concurrency': Joi.number().integer().min(0).default(10),
+        "wrap-with-directory": Joi.boolean(),
+        "file-import-concurrency": Joi.number()
+          .integer()
+          .min(0)
+          .default(50),
+        "block-write-concurrency": Joi.number()
+          .integer()
+          .min(0)
+          .default(10),
         chunker: Joi.string(),
         trickle: Joi.boolean(),
         preload: Joi.boolean().default(true)
@@ -131,58 +151,78 @@ exports.add = {
       .options({ allowUnknown: true })
   },
 
-  handler (request, h) {
+  handler(request, h) {
     if (!request.payload) {
-      throw Boom.badRequest('Array, Buffer, or String is required.')
+      throw Boom.badRequest("Array, Buffer, or String is required.");
     }
+    console.log("========");
+    console.log("========");
+    console.log("========");
+    console.log("========");
+    console.log("========");
+    console.log("Incoming request!");
+    console.log("========");
+    // console.log(request.payload);
 
-    const { ipfs } = request.server.app
-    let filesParsed = false
-    let currentFileName
-    const output = new PassThrough()
+    const { ipfs } = request.server.app;
+    let filesParsed = false;
+    let currentFileName;
+    const output = new PassThrough();
     const progressHandler = bytes => {
-      output.write(JSON.stringify({
-        Name: currentFileName,
-        Bytes: bytes
-      }) + '\n')
-    }
+      output.write(
+        JSON.stringify({
+          Name: currentFileName,
+          Bytes: bytes
+        }) + "\n"
+      );
+    };
 
     pipe(
       multipart(request),
-      async function * (source) {
+      async function*(source) {
+        console.log("-------");
+        console.log({ source });
         for await (const entry of source) {
-          currentFileName = entry.name || 'unknown'
+          console.log("-------");
+          // console.log({ entry });
+          currentFileName = entry.name || "unknown";
 
-          if (entry.type === 'file') {
-            filesParsed = true
+          if (entry.type === "file") {
+            filesParsed = true;
+
+            // Just for logging
+            for await (const chunk of entry.content) {
+              console.log({ chunk });
+            }
+            // end of extra logging code
 
             yield {
               path: entry.name,
               content: entry.content,
               mode: entry.mode,
               mtime: entry.mtime
-            }
+            };
           }
 
-          if (entry.type === 'directory') {
-            filesParsed = true
+          if (entry.type === "directory") {
+            filesParsed = true;
 
             yield {
               path: entry.name,
               mode: entry.mode,
               mtime: entry.mtime
-            }
+            };
           }
         }
       },
-      function (source) {
+      function(source) {
         return ipfs.add(source, {
-          cidVersion: request.query['cid-version'],
-          rawLeaves: request.query['raw-leaves'],
+          cidVersion: request.query["cid-version"],
+          rawLeaves: request.query["raw-leaves"],
           progress: request.query.progress ? progressHandler : () => {},
-          onlyHash: request.query['only-hash'],
+          onlyHash: request.query["only-hash"],
           hashAlg: request.query.hash,
-          wrapWithDirectory: request.query['wrap-with-directory'],
+          wrapWithDirectory: request.query["wrap-with-directory"],
           pin: request.query.pin,
           chunker: request.query.chunker,
           trickle: request.query.trickle,
@@ -192,72 +232,78 @@ exports.add = {
           // at a time from a http request and we have to consume it completely
           // before we can read the next file
           fileImportConcurrency: 1,
-          blockWriteConcurrency: request.query['block-write-concurrency']
-        })
+          blockWriteConcurrency: request.query["block-write-concurrency"]
+        });
       },
       map(file => {
         const entry = {
           Name: file.path,
-          Hash: cidToString(file.cid, { base: request.query['cid-base'] }),
+          Hash: cidToString(file.cid, { base: request.query["cid-base"] }),
           Size: file.size,
-          Mode: file.mode === undefined ? undefined : file.mode.toString(8).padStart(4, '0')
-        }
+          Mode:
+            file.mode === undefined
+              ? undefined
+              : file.mode.toString(8).padStart(4, "0")
+        };
 
         if (file.mtime) {
-          entry.Mtime = file.mtime.secs
-          entry.MtimeNsecs = file.mtime.nsecs
+          entry.Mtime = file.mtime.secs;
+          entry.MtimeNsecs = file.mtime.nsecs;
         }
 
-        return entry
+        return entry;
       }),
       ndjson.stringify,
       toIterable.sink(output)
     )
       .then(() => {
         if (!filesParsed) {
-          throw new Error("File argument 'data' is required.")
+          throw new Error("File argument 'data' is required.");
         }
       })
       .catch(err => {
         if (!filesParsed) {
-          output.write(' ')
+          output.write(" ");
         }
 
         request.raw.res.addTrailers({
-          'X-Stream-Error': JSON.stringify({
+          "X-Stream-Error": JSON.stringify({
             Message: err.message,
             Code: 0
           })
-        })
+        });
       })
       .then(() => {
-        output.end()
-      })
+        output.end();
+      });
 
-    return h.response(output)
-      .header('x-chunked-output', '1')
-      .header('content-type', 'application/json')
-      .header('Trailer', 'X-Stream-Error')
+    return h
+      .response(output)
+      .header("x-chunked-output", "1")
+      .header("content-type", "application/json")
+      .header("Trailer", "X-Stream-Error");
   }
-}
+};
 
 exports.ls = {
   validate: {
-    query: Joi.object().keys({
-      'cid-base': Joi.string().valid(...multibase.names),
-      stream: Joi.boolean()
-    }).unknown()
+    query: Joi.object()
+      .keys({
+        "cid-base": Joi.string().valid(...multibase.names),
+        stream: Joi.boolean()
+      })
+      .unknown()
   },
 
   // uses common parseKey method that returns a `key`
   parseArgs: exports.parseKey,
 
   // main route handler which is called after the above `parseArgs`, but only if the args were valid
-  async handler (request, h) {
-    const { ipfs } = request.server.app
-    const { key } = request.pre.args
-    const recursive = request.query && request.query.recursive === 'true'
-    const cidBase = request.query['cid-base']
+  async handler(request, h) {
+    const { ipfs } = request.server.app;
+    const { key } = request.pre.args;
+    const recursive = request.query && request.query.recursive === "true";
+    const cidBase = request.query["cid-base"];
 
     const mapLink = link => {
       const output = {
@@ -266,94 +312,106 @@ exports.ls = {
         Size: link.size,
         Type: toTypeCode(link.type),
         Depth: link.depth,
-        Mode: link.mode.toString(8).padStart(4, '0')
-      }
+        Mode: link.mode.toString(8).padStart(4, "0")
+      };
 
       if (link.mtime) {
-        output.Mtime = link.mtime.secs
+        output.Mtime = link.mtime.secs;
 
         if (link.mtime.nsecs !== null && link.mtime.nsecs !== undefined) {
-          output.MtimeNsecs = link.mtime.nsecs
+          output.MtimeNsecs = link.mtime.nsecs;
         }
       }
 
-      return output
-    }
+      return output;
+    };
 
     if (!request.query.stream) {
-      let links
+      let links;
       try {
-        links = await all(ipfs.ls(key, { recursive }))
+        links = await all(ipfs.ls(key, { recursive }));
       } catch (err) {
-        throw Boom.boomify(err, { message: 'Failed to list dir' })
+        throw Boom.boomify(err, { message: "Failed to list dir" });
       }
 
-      return h.response({ Objects: [{ Hash: key, Links: links.map(mapLink) }] })
+      return h.response({
+        Objects: [{ Hash: key, Links: links.map(mapLink) }]
+      });
     }
 
-    return streamResponse(request, h, () => pipe(
-      ipfs.ls(key, { recursive }),
-      map(link => ({ Objects: [{ Hash: key, Links: [mapLink(link)] }] })),
-      ndjson.stringify
-    ))
+    return streamResponse(request, h, () =>
+      pipe(
+        ipfs.ls(key, { recursive }),
+        map(link => ({ Objects: [{ Hash: key, Links: [mapLink(link)] }] })),
+        ndjson.stringify
+      )
+    );
   }
-}
+};
 
-function toTypeCode (type) {
+function toTypeCode(type) {
   switch (type) {
-    case 'dir':
-      return 1
-    case 'file':
-      return 2
+    case "dir":
+      return 1;
+    case "file":
+      return 2;
     default:
-      return 0
+      return 0;
   }
 }
 
 exports.refs = {
   validate: {
-    query: Joi.object().keys({
-      recursive: Joi.boolean().default(false),
-      format: Joi.string().default(Format.default),
-      edges: Joi.boolean().default(false),
-      unique: Joi.boolean().default(false),
-      'max-depth': Joi.number().integer().min(-1)
-    }).unknown()
+    query: Joi.object()
+      .keys({
+        recursive: Joi.boolean().default(false),
+        format: Joi.string().default(Format.default),
+        edges: Joi.boolean().default(false),
+        unique: Joi.boolean().default(false),
+        "max-depth": Joi.number()
+          .integer()
+          .min(-1)
+      })
+      .unknown()
   },
 
   // uses common parseKey method that returns a `key`
   parseArgs: exports.parseKey,
 
   // main route handler which is called after the above `parseArgs`, but only if the args were valid
-  handler (request, h) {
-    const { ipfs } = request.server.app
-    const { key } = request.pre.args
+  handler(request, h) {
+    const { ipfs } = request.server.app;
+    const { key } = request.pre.args;
 
     const options = {
       recursive: request.query.recursive,
       format: request.query.format,
       edges: request.query.edges,
       unique: request.query.unique,
-      maxDepth: request.query['max-depth']
-    }
+      maxDepth: request.query["max-depth"]
+    };
 
-    return streamResponse(request, h, () => pipe(
-      ipfs.refs(key, options),
-      map(({ ref, err }) => ({ Ref: ref, Err: err })),
-      ndjson.stringify
-    ))
+    return streamResponse(request, h, () =>
+      pipe(
+        ipfs.refs(key, options),
+        map(({ ref, err }) => ({ Ref: ref, Err: err })),
+        ndjson.stringify
+      )
+    );
   }
-}
+};
 
 exports.refs.local = {
   // main route handler
-  handler (request, h) {
-    const { ipfs } = request.server.app
+  handler(request, h) {
+    const { ipfs } = request.server.app;
 
-    return streamResponse(request, h, () => pipe(
-      ipfs.refs.local(),
-      map(({ ref, err }) => ({ Ref: ref, Err: err })),
-      ndjson.stringify
-    ))
+    return streamResponse(request, h, () =>
+      pipe(
+        ipfs.refs.local(),
+        map(({ ref, err }) => ({ Ref: ref, Err: err })),
+        ndjson.stringify
+      )
+    );
   }
-}
+};
