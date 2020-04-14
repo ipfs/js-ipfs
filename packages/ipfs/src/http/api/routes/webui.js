4136a885860668be4794214f78db3676bd3ceffc
Marcin Rataj <lidel@lidel.org>
2020-04-15 00:44:35 +0200

    feat: webui v2.7.4
    This switched to latest webui and ensures it is loaded from the same
    Origin as the RPC API.
    
    License: MIT
    Signed-off-by: Marcin Rataj <lidel@lidel.org>

--- packages/ipfs/src/http/api/routes/webui.js
+++ packages/ipfs/src/http/api/routes/webui.js
@@ -1,36 +1,31 @@
 'use strict'
 
-const multiaddr = require('multiaddr')
 const debug = require('debug')
+const { gateway } = require('../../gateway/resources')
 const log = debug('ipfs:webui:info')
 log.error = debug('ipfs:webui:error')
 
+const webuiCid = 'bafybeigxqbvc6qxk2wkdyzpkh7mr7zh5pxbvpjb6a6mxdtpwhlqaf4qj5a' // v2.7.4
+
 module.exports = [
   {
-    method: '*',
-    path: '/webui',
-    async handler (request, h) {
-      let scheme = 'http'
-      let port
-      let host
-
-      try {
-        const { ipfs } = request.server.app
-        const gateway = await ipfs.config.get('Addresses.Gateway')
-        const address = multiaddr(gateway).nodeAddress()
-
-        port = address.port
-        host = address.address
-      } catch (err) {
-        // may not have gateway configured
-        log.error(err)
-
-        scheme = 'https'
-        port = 443
-        host = 'gateway.ipfs.io'
+    method: 'GET',
+    path: `/ipfs/${webuiCid}/{path*}`, // only the whitelisted webui is allowed on API port
+    options: {
+      handler: gateway.handler,
+      response: {
+        ranges: false // disable built-in support, handler does it manually
+      },
+      ext: {
+        onPostHandler: { method: gateway.afterHandler }
       }
-
-      return h.redirect(`${scheme}://${host}:${port}/ipfs/Qmexhq2sBHnXQbvyP2GfUdbnY7HCagH2Mw5vUNSBn2nxip`)
+    }
+  },
+  {
+    method: 'GET',
+    path: '/webui/{slug?}', // optional slug makes it work with and without slash
+    handler (request, h) {
+      return h.redirect(`/ipfs/${webuiCid}/`)
     }
   }
 ]
