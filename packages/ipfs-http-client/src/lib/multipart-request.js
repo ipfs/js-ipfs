038bbb57ce2c3c0296c3c960ec8a63c0b4c70bd4
Hugo Dias <hugomrdias@gmail.com>
2020-05-20 14:38:17 +0100

    fix: allow empty publish and normalise error
    - return same error in core for empty publish

--- packages/ipfs-http-client/src/lib/multipart-request.js
+++ packages/ipfs-http-client/src/lib/multipart-request.js
@@ -7,7 +7,7 @@ const modeToString = require('../lib/mode-to-string')
 const mtimeToObject = require('../lib/mtime-to-object')
 const merge = require('merge-options').bind({ ignoreUndefined: true })
 
-async function multipartRequest (source, abortController, headers = {}, boundary = `-----------------------------${nanoid()}`) {
+async function multipartRequest (source = '', abortController, headers = {}, boundary = `-----------------------------${nanoid()}`) {
   async function * streamFiles (source) {
     try {
       let index = 0
