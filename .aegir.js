0c61a36ad35e60f932a46dd338310f8be334cc3c
Vasco Santos <vasco.santos@moxy.studio>
2020-08-14 10:10:31 +0200

    fix: webpack build (#56)

--- .aegir.js
+++ .aegir.js
@@ -2,4 +2,12 @@
 
 module.exports = {
   bundlesize: { maxSize: '140kB' },
+  webpack: {
+    node: {
+      // needed by the mime-types module
+      path: true,
+      // needed by readable-web-to-node-stream module used by file-type
+      stream: true
+    }
+  }
 }
