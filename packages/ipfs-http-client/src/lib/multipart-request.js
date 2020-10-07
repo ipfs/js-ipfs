11c65d73d0b58fa77a2be6eaa0e87b424152d28b
Irakli Gozalishvili <contact@gozala.io>
2020-10-07 16:00:59 -0700

    fix: use browser code paths in electron-renderrer

--- packages/ipfs-http-client/src/lib/multipart-request.js
+++ packages/ipfs-http-client/src/lib/multipart-request.js
@@ -1,77 +1,9 @@
 'use strict'
 
-const normaliseInput = require('ipfs-core-utils/src/files/normalise-input')
-const { nanoid } = require('nanoid')
-const modeToString = require('../lib/mode-to-string')
-const mtimeToObject = require('../lib/mtime-to-object')
-const merge = require('merge-options').bind({ ignoreUndefined: true })
-const toStream = require('it-to-stream')
-
-async function multipartRequest (source = '', abortController, headers = {}, boundary = `-----------------------------${nanoid()}`) {
-  async function * streamFiles (source) {
-    try {
-      let index = 0
-
-      for await (const { content, path, mode, mtime } of normaliseInput(source)) {
-        let fileSuffix = ''
-        const type = content ? 'file' : 'dir'
-
-        if (index > 0) {
-          yield '\r\n'
-
-          fileSuffix = `-${index}`
-        }
-
-        let fieldName = type + fileSuffix
-        const qs = []
-
-        if (mode !== null && mode !== undefined) {
-          qs.push(`mode=${modeToString(mode)}`)
-        }
-
-        if (mtime != null) {
-          const {
-            secs, nsecs
-          } = mtimeToObject(mtime)
-
-          qs.push(`mtime=${secs}`)
-
-          if (nsecs != null) {
-            qs.push(`mtime-nsecs=${nsecs}`)
-          }
-        }
-
-        if (qs.length) {
-          fieldName = `${fieldName}?${qs.join('&')}`
-        }
-
-        yield `--${boundary}\r\n`
-        yield `Content-Disposition: form-data; name="${fieldName}"; filename="${encodeURIComponent(path)}"\r\n`
-        yield `Content-Type: ${content ? 'application/octet-stream' : 'application/x-directory'}\r\n`
-        yield '\r\n'
-
-        if (content) {
-          yield * content
-        }
-
-        index++
-      }
-    } catch (err) {
-      // workaround for https://github.com/node-fetch/node-fetch/issues/753
-      abortController.abort(err)
-    } finally {
-      yield `\r\n--${boundary}--\r\n`
-    }
-  }
-
-  return {
-    lengthComputable: false,
-    total: -1,
-    headers: merge(headers, {
-      'Content-Type': `multipart/form-data; boundary=${boundary}`
-    }),
-    body: await toStream(streamFiles(source))
-  }
+// In electron-renderer we use native fetch and should encode body using native
+// form data.
+if (typeof fetch === 'function') {
+  module.exports = require('./multipart-request.browser')
+} else {
+  module.exports = require('./multipart-request.node')
 }
-
-module.exports = multipartRequest
