e0101ed4f72d955247b9b12709c1464847a5252e
Alex Potsides <alex@achingbrain.net>
2020-10-28 11:55:42 +0000

    chore: update packages/ipfs/src/index.js
    Add TODO
    
    Co-authored-by: Irakli Gozalishvili <contact@gozala.io>

--- packages/ipfs/src/index.js
+++ packages/ipfs/src/index.js
@@ -1,5 +1,7 @@
 'use strict'
 
+// TODO: Fix TS issue that prevents use of require('ipfs-core') instead
+// https://github.com/ipfs/js-ipfs/issues/3358
 const IPFS = require('ipfs-core/src')
 
 module.exports = IPFS
