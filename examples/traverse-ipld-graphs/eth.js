286e8c330e3fd89b9f646f8696c17963f9566b65
achingbrain <alex@achingbrain.net>
2020-10-29 14:08:38 +0000

    chore: fix up ipld graphs example

--- examples/traverse-ipld-graphs/eth.js
+++ examples/traverse-ipld-graphs/eth.js
@@ -9,7 +9,13 @@ const fs = require('fs').promises
 const uint8ArrayToString = require('uint8arrays/to-string')
 
 async function main () {
-  const ipfs = await createNode()
+  const ipfs = await createNode({
+    ipld: {
+      formats: [
+        ...Object.values(require('ipld-etherum'))
+      ]
+    }
+  })
 
   console.log('\nStart of the example:')
 
