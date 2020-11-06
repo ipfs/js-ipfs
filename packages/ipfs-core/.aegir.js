7a0f1a295a21b84119e4c8b65bc3f27b7fbccaca
Vasco Santos <vasco.santos@moxy.studio>
2020-11-06 10:41:26 +0100

    chore: use dnsaddr for bootstrap nodes (#3370)
    Adds the new `dnsaddr` multiaddrs for browser bootstrapping peers per https://github.com/libp2p/js-libp2p/pull/772 and https://github.com/libp2p/js-libp2p/pull/782

--- packages/ipfs-core/.aegir.js
+++ packages/ipfs-core/.aegir.js
@@ -15,7 +15,7 @@ let sigServerB
 let ipfsdServer
 
 module.exports = {
-  bundlesize: { maxSize: '518kB' },
+  bundlesize: { maxSize: '520kB' },
   karma: {
     files: [{
       pattern: 'node_modules/interface-ipfs-core/test/fixtures/**/*',
