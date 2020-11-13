7750c8b1c0d7505ca0a4648c6597ae8c7e62ff8c
achingbrain <alex@achingbrain.net>
2020-11-13 13:27:29 +0000

    chore: restore ipfsd setup

--- packages/ipfs-core/.aegir.js
+++ packages/ipfs-core/.aegir.js
@@ -1,7 +1,23 @@
 'use strict'
 
+const { createServer } = require('ipfsd-ctl')
+const MockPreloadNode = require('./test/utils/mock-preload-node')
+const path = require('path')
+
+let preloadNode = MockPreloadNode.createNode()
+let ipfsdServer
+
 module.exports = {
   bundlesize: { maxSize: '523kB' },
+  karma: {
+    files: [{
+      pattern: 'node_modules/interface-ipfs-core/test/fixtures/**/*',
+      watched: false,
+      served: true,
+      included: false
+    }],
+    browserNoActivityTimeout: 600 * 1000
+  },
   webpack: {
     node: {
       // required by the nofilter module
@@ -9,6 +25,45 @@ module.exports = {
 
       // required by the core-util-is module
       Buffer: true
+    }
+  },
+  hooks: {
+    node: {
+      pre: async () => {
+        await preloadNode.start()
+      },
+      post: async () => {
+        await preloadNode.stop()
+      }
+    },
+    browser: {
+      pre: async () => {
+        await preloadNode.start()
+        ipfsdServer = await createServer({
+          host: '127.0.0.1',
+          port: 57483
+        }, {
+          type: 'js',
+          ipfsModule: require(__dirname),
+          ipfsHttpModule: require('ipfs-http-client'),
+          ipfsBin: path.resolve(path.join(__dirname, '..', 'ipfs', 'src', 'cli.js')),
+          ipfsOptions: {
+            libp2p: {
+              dialer: {
+                dialTimeout: 60e3 // increase timeout because travis is slow
+              }
+            }
+          }
+        }, {
+          go: {
+            ipfsBin: require('go-ipfs').path()
+          }
+        }).start()
+      },
+      post: async () => {
+        await ipfsdServer.stop()
+        await preloadNode.stop()
+      }
     }
   }
 }
