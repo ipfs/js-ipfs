fc420fb820446cb5cb077588603aa9c9ec96ac93
achingbrain <alex@achingbrain.net>
2020-11-13 14:00:56 +0000

    chore: need that too

--- packages/ipfs-http-client/.aegir.js
+++ packages/ipfs-http-client/.aegir.js
@@ -1,5 +1,26 @@
 'use strict'
 
+const { createServer } = require('ipfsd-ctl')
+
+const server = createServer({
+  host: '127.0.0.1',
+  port: 48372
+}, {
+  type: 'go',
+  ipfsHttpModule: require('./'),
+  ipfsBin: require('go-ipfs').path()
+})
+
 module.exports = {
-  bundlesize: { maxSize: '83kB' }
+  bundlesize: { maxSize: '83kB' },
+  hooks: {
+    browser: {
+      pre: async () => {
+        await server.start()
+      },
+      post: async () => {
+        await server.stop()
+      }
+    }
+  }
 }
