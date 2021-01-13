fe93ba01a0c62cead7cc4e0023de2d2a00adbc02
Alex Potsides <alex@achingbrain.net>
2021-01-13 11:46:45 +0000

    feat: allow passing a http.Agent to ipfs-http-client in node (#3474)
    Right now no `http.Agent` is used for requests made using the http client in node, which means each request opens a new connection which can end up hitting process resource limits which means connections get dropped.
    
    The change here sets a default `http.Agent` with a `keepAlive: true` and `maxSockets` of 6 which is consistent with [browsers](https://tools.ietf.org/html/rfc2616#section-8.1.4) and [native apps](https://developer.apple.com/documentation/foundation/nsurlsessionconfiguration/1407597-httpmaximumconnectionsperhost?language=objc).
    
    The user can override the agent passed to the `ipfs-http-client` constructor to restore the previous functionality:
    
    ```js
    const http = require('http')
    const createClient = require('ipfs-http-client')
    
    const client = createClient({
      url: 'http://127.0.0.1:5002',
      agent: new http.Agent({
        keepAlive: false,
        maxSockets: Infinity
      })
    })
    ```
    
    Refs: #3464

---
+++ packages/ipfs-http-client/test/node/agent.js
@@ -0,0 +1,111 @@
+/* eslint-env mocha */
+'use strict'
+
+const { expect } = require('aegir/utils/chai')
+const ipfsClient = require('../../src')
+const delay = require('delay')
+
+function startServer (handler) {
+  return new Promise((resolve) => {
+    // spin up a test http server to inspect the requests made by the library
+    const server = require('http').createServer((req, res) => {
+      req.on('data', () => {})
+      req.on('end', async () => {
+        const out = await handler(req)
+
+        res.writeHead(200)
+        res.write(JSON.stringify(out))
+        res.end()
+      })
+    })
+
+    server.listen(0, () => {
+      resolve({
+        port: server.address().port,
+        close: () => server.close()
+      })
+    })
+  })
+}
+
+describe('agent', function () {
+  let agent
+
+  before(() => {
+    const { Agent } = require('http')
+
+    agent = new Agent({
+      maxSockets: 2
+    })
+  })
+
+  it('restricts the number of concurrent connections', async () => {
+    const responses = []
+
+    const server = await startServer(() => {
+      const p = new Promise((resolve) => {
+        responses.push(resolve)
+      })
+
+      return p
+    })
+
+    const ipfs = ipfsClient({
+      url: `http://localhost:${server.port}`,
+      agent
+    })
+
+    // make three requests
+    const requests = Promise.all([
+      ipfs.id(),
+      ipfs.id(),
+      ipfs.id()
+    ])
+
+    // wait for the first two to arrive
+    for (let i = 0; i < 5; i++) {
+      await delay(100)
+
+      if (responses.length === 2) {
+        // wait a little longer, the third should not arrive
+        await delay(1000)
+
+        expect(responses).to.have.lengthOf(2)
+
+        // respond to the in-flight requests
+        responses[0]({
+          res: 0
+        })
+        responses[1]({
+          res: 1
+        })
+
+        break
+      }
+    }
+
+    // wait for the final request to arrive
+    for (let i = 0; i < 5; i++) {
+      await delay(100)
+
+      if (responses.length === 3) {
+        // respond to it
+        responses[2]({
+          res: 2
+        })
+      }
+    }
+
+    const results = await requests
+
+    expect(results).to.deep.equal([{
+      res: 0
+    }, {
+      res: 1
+    }, {
+      res: 2
+    }])
+
+    server.close()
+  })
+})
