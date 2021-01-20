ipfs/js-ipfs

commit fe93ba01a0c62cead7cc4e0023de2d2a00adbc02

feat: allow passing a http.Agent to ipfs-http-client in node (#3474)
Right now no `http.Agent` is used for requests made using the http client in node, which means each request opens a new connection which can end up hitting process resource limits which means connections get dropped.

The change here sets a default `http.Agent` with a `keepAlive: true` and `maxSockets` of 6 which is consistent with [browsers](https://tools.ietf.org/html/rfc2616#section-8.1.4) and [native apps](https://developer.apple.com/documentation/foundation/nsurlsessionconfiguration/1407597-httpmaximumconnectionsperhost?language=objc).

The user can override the agent passed to the `ipfs-http-client` constructor to restore the previous functionality:

```js
const http = require('http')
const createClient = require('ipfs-http-client')

const client = createClient({
  url: 'http://127.0.0.1:5002',
  agent: new http.Agent({
    keepAlive: false,
    maxSockets: Infinity
  })
})
```

Refs: #3464

packages/ipfs-http-client/src/lib/core.js
@@ -1,12 +1,13 @@
 'use strict'
 /* eslint-env browser */
 const Multiaddr = require('multiaddr')
-const { isBrowser, isWebWorker } = require('ipfs-utils/src/env')
+const { isBrowser, isWebWorker, isNode } = require('ipfs-utils/src/env')
 const parseDuration = require('parse-duration').default
 const log = require('debug')('ipfs-http-client:lib:error-handler')
 const HTTP = require('ipfs-utils/src/http')
 const merge = require('merge-options')
 const toUrlString = require('ipfs-core-utils/src/to-url-string')
+const http = require('http')
 
 const DEFAULT_PROTOCOL = isBrowser || isWebWorker ? location.protocol : 'http'
 const DEFAULT_HOST = isBrowser || isWebWorker ? location.hostname : 'localhost'
@@ -19,6 +20,7 @@ const DEFAULT_PORT = isBrowser || isWebWorker ? location.port : '5001'
 const normalizeOptions = (options = {}) => {
   let url
   let opts = {}
+  let agent
 
   if (typeof options === 'string' || Multiaddr.isMultiaddr(options)) {
     url = new URL(toUrlString(options))
@@ -46,13 +48,22 @@ const normalizeOptions = (options = {}) => {
     url.pathname = 'api/v0'
   }
 
+  if (isNode) {
+    agent = opts.agent || new http.Agent({
+      keepAlive: true,
+      // Similar to browsers which limit connections to six per host
+      maxSockets: 6
+    })
+  }
+
   return {
     ...opts,
     host: url.host,
     protocol: url.protocol.replace(':', ''),
     port: Number(url.port),
     apiPath: url.pathname,
-    url
+    url,
+    agent
   }
 }
 
@@ -105,6 +116,8 @@ const parseTimeout = (value) => {
 }
 
 /**
+ * @typedef {import('http').Agent} Agent
+ *
  * @typedef {Object} ClientOptions
  * @property {string} [host]
  * @property {number} [port]
@@ -116,6 +129,7 @@ const parseTimeout = (value) => {
  * @property {object} [ipld]
  * @property {any[]} [ipld.formats] - An array of additional [IPLD formats](https://github.com/ipld/interface-ipld-format) to support
  * @property {(format: string) => Promise<any>} [ipld.loadFormat] - an async function that takes the name of an [IPLD format](https://github.com/ipld/interface-ipld-format) as a string and should return the implementation of that codec
+ * @property {Agent} [agent] - A [http.Agent](https://nodejs.org/api/http.html#http_class_http_agent) used to control connection persistence and reuse for HTTP clients (only supported in node.js)
  */
 class Client extends HTTP {
   /**
@@ -149,7 +163,8 @@ class Client extends HTTP {
         }
 
         return out
-      }
+      },
+      agent: opts.agent
     })
 
     delete this.get

packages/ipfs-daemon/src/index.js
@@ -33,7 +33,7 @@ class Daemon {
   /**
    * Starts the IPFS HTTP server
    *
-   * @returns {Promise<Daemon>}
+   * @returns {Promise<Daemon>} - A promise that resolves to a Daemon instance
    */
   async start () {
     log('starting')

.travis.yml
@@ -16,7 +16,11 @@ stages:
 
 node_js:
   - 'lts/*'
-  - 'node'
+# node 15.5.1 broke Hapi - https://github.com/hapijs/hapi/issues/4208
+# The change has been reverted - https://github.com/nodejs/node/pull/36647
+# Will be released in 15.6.x - https://github.com/nodejs/node/pull/36889
+# Disable testing on node 15.x.x until that is released
+#  - 'node'
 
 os:
   - linux

packages/ipfs-daemon/test/index.spec.js
@@ -5,12 +5,36 @@ const { expect } = require('aegir/utils/chai')
 const Daemon = require('../')
 const fetch = require('node-fetch')
 const WebSocket = require('ws')
+const os = require('os')
 
-describe('daemon', () => {
+function createDaemon () {
+  return new Daemon({
+    init: {
+      bits: 512
+    },
+    repo: `${os.tmpdir()}/ipfs-test-${Math.random()}`,
+    config: {
+      Addresses: {
+        Swarm: [
+          '/ip4/0.0.0.0/tcp/0',
+          '/ip4/127.0.0.1/tcp/0/ws'
+        ],
+        API: '/ip4/127.0.0.1/tcp/0',
+        Gateway: '/ip4/127.0.0.1/tcp/0',
+        RPC: '/ip4/127.0.0.1/tcp/0'
+      }
+    }
+  })
+}
+
+describe('daemon', function () {
+  // slow ci is slow
+  this.timeout(60 * 1000)
+
   let daemon
 
   it('should start a http api server', async () => {
-    daemon = new Daemon({})
+    daemon = createDaemon()
 
     await daemon.start()
 
@@ -18,19 +42,19 @@ describe('daemon', () => {
       uri
     } = daemon._httpApi._apiServers[0].info
 
-    const httpId = (await fetch(`${uri}/api/v0/id`, {
+    const idFromCore = await daemon._ipfs.id()
+
+    const httpId = await fetch(`${uri}/api/v0/id`, {
       method: 'POST'
-    })).json()
+    })
 
-    const apiId = await daemon._ipfs.id()
-
-    await expect(httpId).to.eventually.have.property('PublicKey', apiId.publicKey)
+    await expect(httpId.json()).to.eventually.have.property('PublicKey', idFromCore.publicKey)
 
     await daemon.stop()
   })
 
   it('should start a http gateway server', async () => {
-    daemon = new Daemon({})
+    daemon = createDaemon()
 
     await daemon.start()
 
@@ -38,17 +62,17 @@ describe('daemon', () => {
       uri
     } = daemon._httpGateway._gatewayServers[0].info
 
-    const result = await (await fetch(`${uri}/ipfs/QmUNLLsPACCz1vLxQVkXqqLX5R1X345qqfHbsf67hvA3Nn`, {
+    const result = await fetch(`${uri}/ipfs/QmUNLLsPACCz1vLxQVkXqqLX5R1X345qqfHbsf67hvA3Nn`, {
       method: 'POST'
-    })).text()
+    })
 
-    expect(result).to.include('Index of /ipfs/QmUNLLsPACCz1vLxQVkXqqLX5R1X345qqfHbsf67hvA3Nn/')
+    await expect(result.text()).to.eventually.include('Index of /ipfs/QmUNLLsPACCz1vLxQVkXqqLX5R1X345qqfHbsf67hvA3Nn/')
 
     await daemon.stop()
   })
 
   it('should start a gRPC server', async () => {
-    daemon = new Daemon({})
+    daemon = createDaemon()
 
     await daemon.start()
 
@@ -83,7 +107,7 @@ describe('daemon', () => {
   })
 
   it('should stop', async () => {
-    daemon = new Daemon({})
+    daemon = createDaemon()
 
     await daemon.start()
     await daemon.stop()

packages/ipfs-http-client/test/node.js
@@ -1,5 +1,6 @@
 'use strict'
 
+require('./node/agent')
 require('./node/swarm')
 require('./node/request-api')
 require('./node/custom-headers')

packages/ipfs-http-client/test/node/agent.js
@@ -1,0 +1,111 @@
+/* eslint-env mocha */
+'use strict'
+
+const { expect } = require('aegir/utils/chai')
+const ipfsClient = require('../../src')
+const delay = require('delay')
+
+function startServer (handler) {
+  return new Promise((resolve) => {
+    // spin up a test http server to inspect the requests made by the library
+    const server = require('http').createServer((req, res) => {
+      req.on('data', () => {})
+      req.on('end', async () => {
+        const out = await handler(req)
+
+        res.writeHead(200)
+        res.write(JSON.stringify(out))
+        res.end()
+      })
+    })
+
+    server.listen(0, () => {
+      resolve({
+        port: server.address().port,
+        close: () => server.close()
+      })
+    })
+  })
+}
+
+describe('agent', function () {
+  let agent
+
+  before(() => {
+    const { Agent } = require('http')
+
+    agent = new Agent({
+      maxSockets: 2
+    })
+  })
+
+  it('restricts the number of concurrent connections', async () => {
+    const responses = []
+
+    const server = await startServer(() => {
+      const p = new Promise((resolve) => {
+        responses.push(resolve)
+      })
+
+      return p
+    })
+
+    const ipfs = ipfsClient({
+      url: `http://localhost:${server.port}`,
+      agent
+    })
+
+    // make three requests
+    const requests = Promise.all([
+      ipfs.id(),
+      ipfs.id(),
+      ipfs.id()
+    ])
+
+    // wait for the first two to arrive
+    for (let i = 0; i < 5; i++) {
+      await delay(100)
+
+      if (responses.length === 2) {
+        // wait a little longer, the third should not arrive
+        await delay(1000)
+
+        expect(responses).to.have.lengthOf(2)
+
+        // respond to the in-flight requests
+        responses[0]({
+          res: 0
+        })
+        responses[1]({
+          res: 1
+        })
+
+        break
+      }
+    }
+
+    // wait for the final request to arrive
+    for (let i = 0; i < 5; i++) {
+      await delay(100)
+
+      if (responses.length === 3) {
+        // respond to it
+        responses[2]({
+          res: 2
+        })
+      }
+    }
+
+    const results = await requests
+
+    expect(results).to.deep.equal([{
+      res: 0
+    }, {
+      res: 1
+    }, {
+      res: 2
+    }])
+
+    server.close()
+  })
+})

commit ec9934d04aa2774028a7ad5cad23bce946f08b0f

fix: use https agent for https requests
Fixes regression introduced in #3474 - if we make https requests,
use a https agent.

packages/ipfs-grpc-client/src/grpc/transport.node.js
@@ -7,6 +7,11 @@
 const WebSocket = require('ws')
 const debug = require('debug')('ipfs:grpc-client:websocket-transport')
 
+/**
+ * @typedef {import('http').Agent} HttpAgent
+ * @typedef {import('https').Agent} HttpsAgent
+ */
+
 const WebsocketSignal = {
   FINISH_SEND: 1
 }
@@ -15,7 +20,7 @@ const finishSendFrame = new Uint8Array([1])
 
 /**
  * @param {object} options
- * @param {import('http').Agent} [options.agent] - http.Agent used to control HTTP client behaviour
+ * @param {HttpAgent|HttpsAgent} [options.agent] - http.Agent used to control HTTP client behaviour
  */
 function WebsocketTransport (options) {
   /**
@@ -33,7 +38,7 @@ function WebsocketTransport (options) {
 
 /**
  * @typedef {object} NodeTransportOptions
- * @property {import('http').Agent} [options.agent]
+ * @property {HttpAgent|HttpsAgent} [options.agent]
  *
  * @typedef {NodeTransportOptions & import('@improbable-eng/grpc-web').grpc.TransportOptions} WebSocketTransportOptions
  *

packages/ipfs-grpc-client/src/index.js
@@ -20,9 +20,12 @@ function normaliseUrls (opts) {
 }
 
 /**
+ * @typedef {import('http').Agent} HttpAgent
+ * @typedef {import('https').Agent} HttpsAgent
+ *
  * @param {object} opts
  * @param {string} opts.url - The URL to connect to as a URL or Multiaddr
- * @param {import('http').Agent} [opts.agent] - http.Agent used to control HTTP client behaviour (node.js only)
+ * @param {HttpAgent|HttpsAgent} [opts.agent] - http.Agent used to control HTTP client behaviour (node.js only)
  */
 module.exports = function createClient (opts = { url: '' }) {
   opts.url = toUrlString(opts.url)

packages/ipfs-grpc-client/src/utils/bidi-to-duplex.js
@@ -5,6 +5,11 @@ const errCode = require('err-code')
 const toHeaders = require('./to-headers')
 const transport = require('../grpc/transport')
 
+/**
+ * @typedef {import('http').Agent} HttpAgent
+ * @typedef {import('https').Agent} HttpsAgent
+ */
+
 async function sendMessages (service, client, source) {
   for await (const obj of source) {
     client.send({
@@ -24,7 +29,7 @@ async function sendMessages (service, client, source) {
  * @param {string} options.host - The remote host
  * @param {boolean} [options.debug] - Whether to print debug messages
  * @param {object} [options.metadata] - Metadata sent as headers
- * @param {import('http').Agent} [options.agent] - http.Agent used to control HTTP client behaviour (node.js only)
+ * @param {HttpAgent|HttpsAgent} [options.agent] - http.Agent used to control HTTP client behaviour (node.js only)
  * @returns {{ source: AsyncIterable<object>, sink: { push: Function, end: Function } }}
  **/
 module.exports = function bidiToDuplex (grpc, service, options) {

packages/ipfs-grpc-client/src/utils/client-stream-to-promise.js
@@ -4,6 +4,11 @@ const first = require('it-first')
 const bidiToDuplex = require('./bidi-to-duplex')
 
 /**
+ * @typedef {import('http').Agent} HttpAgent
+ * @typedef {import('https').Agent} HttpsAgent
+ */
+
+/**
  * Client streaming methods are a many-to-one operation so this
  * function takes a source that can emit multiple messages and
  * returns a promise that resolves to the server response.
@@ -15,7 +20,7 @@ const bidiToDuplex = require('./bidi-to-duplex')
  * @param {string} options.host - The remote host
  * @param {boolean} [options.debug] - Whether to print debug messages
  * @param {object} [options.metadata] - Metadata sent as headers
- * @param {import('http').Agent} [options.agent] - http.Agent used to control HTTP client behaviour (node.js only)
+ * @param {HttpAgent|HttpsAgent} [options.agent] - http.Agent used to control HTTP client behaviour (node.js only)
  * @returns {Promise<Object>} - A promise that resolves to a response object
  **/
 module.exports = async function clientStreamToPromise (grpc, service, source, options) {

packages/ipfs-grpc-client/src/utils/server-stream-to-iterator.js
@@ -3,6 +3,11 @@
 const bidiToDuplex = require('./bidi-to-duplex')
 
 /**
+ * @typedef {import('http').Agent} HttpAgent
+ * @typedef {import('https').Agent} HttpsAgent
+ */
+
+/**
  * Server stream methods are one-to-many operations so this
  * function accepts a client message and returns a source
  * from which multiple server messages can be read.
@@ -14,7 +19,7 @@ const bidiToDuplex = require('./bidi-to-duplex')
  * @param {string} options.host - The remote host
  * @param {boolean} [options.debug] - Whether to print debug messages
  * @param {object} [options.metadata] - Metadata sent as headers
- * @param {import('http').Agent} [options.agent] - http.Agent used to control HTTP client behaviour (node.js only)
+ * @param {HttpAgent|HttpsAgent} [options.agent] - http.Agent used to control HTTP client behaviour (node.js only)
  * @returns {AsyncIterable<object>}
  **/
 module.exports = function serverStreamToIterator (grpc, service, request, options) {

packages/ipfs-grpc-client/src/utils/unary-to-promise.js
@@ -4,6 +4,11 @@ const first = require('it-first')
 const bidiToDuplex = require('./bidi-to-duplex')
 
 /**
+ * @typedef {import('http').Agent} HttpAgent
+ * @typedef {import('https').Agent} HttpsAgent
+ */
+
+/**
  * Unary calls are one-to-one operations so this function
  * takes a client message and returns a promise that resolves
  * to the server response.
@@ -15,7 +20,7 @@ const bidiToDuplex = require('./bidi-to-duplex')
  * @param {string} options.host - The remote host
  * @param {boolean} [options.debug] - Whether to print debug messages
  * @param {object} [options.metadata] - Metadata sent as headers
- * @param {import('http').Agent} [options.agent] - http.Agent used to control HTTP client behaviour (node.js only)
+ * @param {HttpAgent|HttpsAgent} [options.agent] - http.Agent used to control HTTP client behaviour (node.js only)
  * @returns {Promise<Object>} - A promise that resolves to a response object
  **/
 module.exports = function unaryToPromise (grpc, service, request, options) {

packages/ipfs-http-client/src/lib/core.js
@@ -8,6 +8,7 @@ const HTTP = require('ipfs-utils/src/http')
 const merge = require('merge-options')
 const toUrlString = require('ipfs-core-utils/src/to-url-string')
 const http = require('http')
+const https = require('https')
 
 const DEFAULT_PROTOCOL = isBrowser || isWebWorker ? location.protocol : 'http'
 const DEFAULT_HOST = isBrowser || isWebWorker ? location.hostname : 'localhost'
@@ -49,7 +50,9 @@ const normalizeOptions = (options = {}) => {
   }
 
   if (isNode) {
-    agent = opts.agent || new http.Agent({
+    const Agent = url.protocol.startsWith('https') ? https.Agent : http.Agent
+
+    agent = opts.agent || new Agent({
       keepAlive: true,
       // Similar to browsers which limit connections to six per host
       maxSockets: 6
@@ -116,7 +119,8 @@ const parseTimeout = (value) => {
 }
 
 /**
- * @typedef {import('http').Agent} Agent
+ * @typedef {import('http').Agent} HttpAgent
+ * @typedef {import('https').Agent} HttpsAgent
  *
  * @typedef {Object} ClientOptions
  * @property {string} [host]
@@ -129,7 +133,7 @@ const parseTimeout = (value) => {
  * @property {object} [ipld]
  * @property {any[]} [ipld.formats] - An array of additional [IPLD formats](https://github.com/ipld/interface-ipld-format) to support
  * @property {(format: string) => Promise<any>} [ipld.loadFormat] - an async function that takes the name of an [IPLD format](https://github.com/ipld/interface-ipld-format) as a string and should return the implementation of that codec
- * @property {Agent} [agent] - A [http.Agent](https://nodejs.org/api/http.html#http_class_http_agent) used to control connection persistence and reuse for HTTP clients (only supported in node.js)
+ * @property {HttpAgent|HttpsAgent} [agent] - A [http.Agent](https://nodejs.org/api/http.html#http_class_http_agent) used to control connection persistence and reuse for HTTP clients (only supported in node.js)
  */
 class Client extends HTTP {
   /**