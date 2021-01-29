48b74a290e3dd588bd2b0730dd93aa73331bc272
achingbrain <alex@achingbrain.net>
2021-01-29 08:10:43 +0000

    chore: bundle size

--- packages/ipfs/.aegir.js
+++ packages/ipfs/.aegir.js
@@ -15,7 +15,7 @@ let sigServerB
 let ipfsdServer
 
 module.exports = {
-  bundlesize: { maxSize: '600kB' },
+  bundlesize: { maxSize: '612kB' },
   karma: {
     files: [{
       pattern: 'node_modules/interface-ipfs-core/test/fixtures/**/*',
