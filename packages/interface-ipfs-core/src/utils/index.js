0dd53d2b50df452001ac97a5a0faeeec951882c0
Hugo Dias <hugomrdias@gmail.com>
2021-02-10 13:05:47 +0000

    fix: ping test cid

--- packages/interface-ipfs-core/src/utils/index.js
+++ packages/interface-ipfs-core/src/utils/index.js
@@ -22,9 +22,10 @@ exports.fixtures = Object.freeze({
     data: fromString('Plz add me!\n')
   }),
   bigFile: Object.freeze({
-    cid: 'QmcKEs7mbxbGPPc2zo77E6CPwgaSbY4SmD2MFh16AqaR9e',
+    cid: 'QmR4nFjTu18TyANgC65ArNWp5Yaab1gPzQ4D8zp7Kx3vhr',
     data: loadFixture('test/fixtures/test-folder/holmes.txt', 'interface-ipfs-core')
     // TODO check https://github.com/ipfs/js-ipfs/issues/3542
+    // cid: 'QmcKEs7mbxbGPPc2zo77E6CPwgaSbY4SmD2MFh16AqaR9e',
     // data: Uint8Array.from(new Array(ONE_MEG * 15).fill(0))
   }),
   emptyFile: Object.freeze({
