892ae82c5b4cad1c4ce17507623526c8beea4367
achingbrain <alex@achingbrain.net>
2021-06-17 18:46:11 +0100

    chore: fix on node 14

--- packages/ipfs-http-server/src/utils/stream-response.js
+++ packages/ipfs-http-server/src/utils/stream-response.js
@@ -1,5 +1,6 @@
 'use strict'
 
+const { PassThrough } = require('stream')
 const { pipe } = require('it-pipe')
 const log = require('debug')('ipfs:http-api:utils:stream-response')
 // @ts-ignore no types
@@ -8,54 +9,69 @@ const toIterable = require('stream-to-it')
 const ERROR_TRAILER = 'X-Stream-Error'
 
 /**
+ *
  * @param {import('../types').Request} request
  * @param {import('@hapi/hapi').ResponseToolkit} h
  * @param {() => AsyncIterable<any>} getSource
- * @param {{onError?: (error: Error) => void }} [options]
+ * @param {{ onError?: (error: Error) => void }} [options]
  */
 async function streamResponse (request, h, getSource, options = {}) {
-  request.raw.res.setHeader('X-Chunked-Output', '1')
-  request.raw.res.setHeader('Content-Type', 'application/json')
-  request.raw.res.setHeader('Trailer', ERROR_TRAILER)
+  // eslint-disable-next-line no-async-promise-executor
+  const stream = await new Promise(async (resolve, reject) => {
+    let started = false
+    const stream = new PassThrough()
 
-  pipe(
-    async function * () {
-      try {
-        for await (const chunk of getSource()) {
-          if (chunk instanceof Uint8Array || typeof chunk === 'string') {
-            yield chunk
-          } else {
-            yield JSON.stringify(chunk) + '\n'
+    try {
+      await pipe(
+        (async function * () {
+          try {
+            for await (const chunk of getSource()) {
+              if (!started) {
+                started = true
+                resolve(stream)
+              }
+
+              if (chunk instanceof Uint8Array || typeof chunk === 'string') {
+                yield chunk
+              } else {
+                yield JSON.stringify(chunk) + '\n'
+              }
+            }
+
+            if (!started) { // Maybe it was an empty source?
+              started = true
+              resolve(stream)
+            }
+          } catch (err) {
+            log(err)
+
+            if (options.onError) {
+              options.onError(err)
+            }
+
+            if (request.raw.res.headersSent) {
+              request.raw.res.addTrailers({
+                [ERROR_TRAILER]: JSON.stringify({
+                  Message: err.message,
+                  Code: 0
+                })
+              })
+            }
+
+            throw err
           }
-        }
-      } catch (err) {
-        log(err)
+        })(),
+        toIterable.sink(stream)
+      )
+    } catch (err) {
+      reject(err)
+    }
+  })
 
-        if (options.onError) {
-          options.onError(err)
-        }
-
-        if (request.raw.res.headersSent) {
-          request.raw.res.addTrailers({
-            [ERROR_TRAILER]: JSON.stringify({
-              Message: err.message,
-              Code: 0
-            })
-          })
-        } else {
-          request.raw.res.statusCode = 500
-
-          yield JSON.stringify({
-            Message: err.message,
-            Code: 0
-          }) + '\n'
-        }
-      }
-    },
-    toIterable.sink(request.raw.res)
-  )
-
-  return h.abandon
+  return h.response(stream)
+    .header('X-Chunked-Output', '1')
+    .header('Content-Type', 'application/json')
+    .header('Trailer', ERROR_TRAILER)
 }
 
 module.exports = streamResponse
