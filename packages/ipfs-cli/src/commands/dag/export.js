de2ab6ed2b88a4d440baadb12ec30401cd84cb1c
Rod Vagg <rod@vagg.org>
2021-06-28 22:15:47 +1000

    fix: types, make tests pass, don't allow partial DAG

--- packages/ipfs-cli/src/commands/dag/export.js
+++ packages/ipfs-cli/src/commands/dag/export.js
@@ -5,13 +5,17 @@ const { default: parseDuration } = require('parse-duration')
 const LegacyCID = require('cids')
 const { CID } = require('multiformats/cid')
 const Block = require('multiformats/block')
-const { base58 } = require('multiformats/bases/base58')
+const { base58btc } = require('multiformats/bases/base58')
 const { CarWriter } = require('@ipld/car/writer')
+/**
+ * @typedef {import('multiformats/codecs/interface').BlockCodec<number, any>} BlockCodec
+ */
+/** @type {BlockCodec[]} */
 const codecs = [
   require('@ipld/dag-pb'),
   require('@ipld/dag-cbor'),
   require('@ipld/dag-json')
-].reduce((codecs, codec) => {
+].reduce((/** @type {BlockCodec[]} */ codecs, /** @type {BlockCodec} */ codec) => {
   codecs[codec.code] = codec
   return codecs
 }, [])
@@ -53,12 +57,8 @@ module.exports = {
     const { writer, out } = await CarWriter.create([cid])
     Readable.from(out).pipe(process.stdout)
 
-    const complete = await traverseWrite(ipfs, options, cid, writer)
+    await traverseWrite(ipfs, options, cid, writer)
     writer.close()
-
-    if (!complete) {
-      print('cannot decode links in all blocks, DAG may be incomplete')
-    }
   }
 }
 
@@ -66,21 +66,28 @@ module.exports = {
  * @param {IPFS} ipfs
  * @param {{ timeout?: number}} options
  * @param {CID} cid
- * @returns {Promise<{cid:CID, bytes:Uint8Array, links:CID[]|null}>}
+ * @returns {Promise<{cid:CID, bytes:Uint8Array, links:CID[]}>}
  */
 const getBlock = async (ipfs, options, cid) => {
-  cid = CID.asCID(cid)
   const result = await ipfs.block.get(new LegacyCID(cid.bytes), options)
+  if (!result) {
+    throw new Error(`Failed to fetch block ${cid}`)
+  }
+  const resultCid = CID.asCID(result.cid)
+  if (!resultCid || !resultCid.equals(cid)) {
+    // shouldn't happen, but let's sanity check
+    throw new Error(`Fetched CID ${result.cid} does not match requested ${cid}`)
+  }
   const bytes = result.data
-  let links = null
+  /** @type {CID[]} */
+  let links = []
   const codec = codecs[result.cid.code]
   if (codec) {
-    const block = Block.createUnsafe({ bytes: result.data, cid: CID.asCID(result.cid), codec })
+    const block = Block.createUnsafe({ bytes: result.data, cid, codec })
     links = [...block.links()].map((l) => l[1])
-  } else if (NO_LINKS_CODECS.includes(result.cid.code)) {
-    // these blocks are known not to contain any IPLD links
-    links = []
-  } // else we may have a block with links that we can't decode
+  } else if (!NO_LINKS_CODECS.includes(result.cid.code)) {
+    throw new Error(`Can't decode links in block with codec 0x${result.cid.code.toString(16)} to form complete DAG`)
+  }
   return { cid, bytes, links }
 }
 
@@ -90,26 +97,19 @@ const getBlock = async (ipfs, options, cid) => {
  * @param {CID} cid
  * @param {BlockWriter} writer
  * @param {Set<string>} seen
- * @returns {boolean} complete DAG
+ * @returns {Promise<void>}
  */
 async function traverseWrite (ipfs, options, cid, writer, seen = new Set()) {
-  const b58Cid = cid.toString(base58)
+  const b58Cid = cid.toString(base58btc)
   if (seen.has(b58Cid)) {
-    return true
+    return
   }
   const block = await getBlock(ipfs, options, cid)
   await writer.put(block)
   seen.add(b58Cid)
-  if (block.links === null) {
-    return false // potentially incomplete DAG
-  }
 
   // recursive traversal of all links
-  let complete = true
   for (const link of block.links) {
-    if (!await traverseWrite(ipfs, options, link, writer, seen)) {
-      complete = false
-    }
+    await traverseWrite(ipfs, options, link, writer, seen)
   }
-  return complete
 }
