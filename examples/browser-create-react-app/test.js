28bebac1fc2b1cb7c7928e4d0acda4f1f9edec7c
Marcin Rataj <lidel@lidel.org>
2021-07-03 01:03:10 +0200

    test: examples/browser-create-react-app/test.js

--- examples/browser-create-react-app/test.js
+++ examples/browser-create-react-app/test.js
@@ -7,7 +7,7 @@ module.exports = {
     browser
       .url(process.env.IPFS_EXAMPLE_TEST_URL)
       .waitForElementVisible('[data-test=title]')
-      .assert.containsText('[data-test=title]', 'Connected to IPFS')
+      .assert.containsText('[data-test=title]', 'Connected – properties of current IPFS node')
       .assert.elementPresent('[data-test=id')
       .assert.elementPresent('[data-test=agentVersion')
       .end()
