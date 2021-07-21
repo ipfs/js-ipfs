6ad7a3b5e149434f67dacf58926529b5d06bcaf2
bluelovers <codelovers@users.sourceforge.net>
2021-07-21 14:04:08 +0800

    feat(ipfs-core-types): add `DAGLinkRawLike`

--- packages/ipfs-core-types/src/object/patch/index.d.ts
+++ packages/ipfs-core-types/src/object/patch/index.d.ts
@@ -2,9 +2,23 @@ import type CID from 'cids';
 import type { AbortOptions } from '../../utils'
 import type { DAGLink } from 'ipld-dag-pb'
 
+export type DAGLinkRawLike = ({
+  Name: string
+  Hash: CID | string
+  name?: undefined
+  cid?: undefined
+} | {
+  Name?: undefined
+  Hash?: undefined
+  name: string
+  cid: CID | string
+}) & {
+  size: number
+}
+
 export interface API<OptionExtension = {}> {
-  addLink: (cid: CID, link: DAGLink, options?: AbortOptions & OptionExtension) => Promise<CID>
-  rmLink: (cid: CID, link: DAGLink | string, options?: AbortOptions & OptionExtension) => Promise<CID>
+  addLink: (cid: CID, link: DAGLink | DAGLinkRawLike, options?: AbortOptions & OptionExtension) => Promise<CID>
+  rmLink: (cid: CID, link: DAGLink | DAGLinkRawLike, options?: AbortOptions & OptionExtension) => Promise<CID>
   appendData: (cid: CID, data: Uint8Array, options?: AbortOptions & OptionExtension) => Promise<CID>
   setData: (cid: CID, data: Uint8Array, options?: AbortOptions & OptionExtension) => Promise<CID>
 }
