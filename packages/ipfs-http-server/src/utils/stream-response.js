7ba22a2dfcf50253170c6761614788bb5285075a
achingbrain <alex@achingbrain.net>
2021-07-26 17:28:51 +0100

    chore: revert streaming response refactor

--- packages/ipfs-http-server/src/utils/stream-response.js
+++ packages/ipfs-http-server/src/utils/stream-response.js
@@ -1,8 +1,10 @@
 'use strict'
 
 const { PassThrough } = require('stream')
+const { pipe } = require('it-pipe')
 const log = require('debug')('ipfs:http-api:utils:stream-response')
-const Boom = require('@hapi/boom')
+// @ts-ignore no types
+const toIterable = require('stream-to-it')
 
 const ERROR_TRAILER = 'X-Stream-Error'
 
@@ -14,58 +16,66 @@ const ERROR_TRAILER = 'X-Stream-Error'
  * @param {{ onError?: (error: Error) => void, onEnd?: () => void }} [options]
  */
 async function streamResponse (request, h, getSource, options = {}) {
-  const stream = new PassThrough()
-  const res = h.response(stream)
+  // eslint-disable-next-line no-async-promise-executor
+  const stream = await new Promise(async (resolve, reject) => {
+    let started = false
+    const stream = new PassThrough()
+
+    try {
+      await pipe(
+        (async function * () {
+          try {
+            for await (const chunk of getSource()) {
+              if (!started) {
+                started = true
+                resolve(stream)
+              }
+
+              if (chunk instanceof Uint8Array || typeof chunk === 'string') {
+                yield chunk
+              } else {
+                yield JSON.stringify(chunk) + '\n'
+              }
+            }
+
+            if (options.onEnd) {
+              options.onEnd()
+            }
+
+            if (!started) { // Maybe it was an empty source?
+              started = true
+              resolve(stream)
+            }
+          } catch (err) {
+            log(err)
+
+            if (options.onError) {
+              options.onError(err)
+            }
+
+            if (request.raw.res.headersSent) {
+              request.raw.res.addTrailers({
+                [ERROR_TRAILER]: JSON.stringify({
+                  Message: err.message,
+                  Code: 0
+                })
+              })
+            }
+
+            throw err
+          }
+        })(),
+        toIterable.sink(stream)
+      )
+    } catch (err) {
+      reject(err)
+    }
+  })
+
+  return h.response(stream)
     .header('X-Chunked-Output', '1')
     .header('Content-Type', 'application/json')
     .header('Trailer', ERROR_TRAILER)
-
-  Promise.resolve()
-    .then(async () => {
-      try {
-        for await (const chunk of getSource()) {
-          if (chunk instanceof Uint8Array || typeof chunk === 'string') {
-            stream.write(chunk)
-          } else {
-            stream.write(JSON.stringify(chunk) + '\n')
-          }
-        }
-
-        if (options.onEnd) {
-          options.onEnd()
-        }
-      } catch (err) {
-        log(err)
-
-        if (options.onError) {
-          options.onError(err)
-        }
-
-        if (request.raw.res.headersSent) {
-          request.raw.res.addTrailers({
-            [ERROR_TRAILER]: JSON.stringify({
-              Message: err.message,
-              Code: 0
-            })
-          })
-        } else {
-          if (Boom.isBoom(err)) {
-            res.code(err.output.statusCode)
-          } else {
-            res.code(500)
-          }
-
-          stream.write(JSON.stringify({
-            Message: err.message,
-            Code: 0
-          }) + '\n')
-        }
-      } finally {
-        stream.end()
-      }
-    })
-
-  return res
 }
 
 module.exports = streamResponse
