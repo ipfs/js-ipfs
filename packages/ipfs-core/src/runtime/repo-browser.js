8380d7160e7205bed9cc4aecfc46882bc97d42c3
Alex Potsides <alex@achingbrain.net>
2021-07-30 17:51:18 +0100

    fix: restore default level-js options (#3779)
    Restores level-js options from https://github.com/ipfs/js-ipfs-repo/blob/b82938fcef0f949517fbd6f63700c2c0178ef117/src/default-options-browser.js
    
    The ones that are valid anyway.

--- packages/ipfs-core/src/runtime/repo-browser.js
+++ packages/ipfs-core/src/runtime/repo-browser.js
@@ -20,13 +20,28 @@ module.exports = (print, codecs, options) => {
   const repoPath = options.path || 'ipfs'
 
   return createRepo(repoPath, (codeOrName) => codecs.getCodec(codeOrName), {
-    root: new DatastoreLevel(repoPath),
+    root: new DatastoreLevel(repoPath, {
+      prefix: '',
+      version: 2
+    }),
     blocks: new BlockstoreDatastoreAdapter(
-      new DatastoreLevel(`${repoPath}/blocks`)
+      new DatastoreLevel(`${repoPath}/blocks`, {
+        prefix: '',
+        version: 2
+      })
     ),
-    datastore: new DatastoreLevel(`${repoPath}/datastore`),
-    keys: new DatastoreLevel(`${repoPath}/keys`),
-    pins: new DatastoreLevel(`${repoPath}/pins`)
+    datastore: new DatastoreLevel(`${repoPath}/datastore`, {
+      prefix: '',
+      version: 2
+    }),
+    keys: new DatastoreLevel(`${repoPath}/keys`, {
+      prefix: '',
+      version: 2
+    }),
+    pins: new DatastoreLevel(`${repoPath}/pins`, {
+      prefix: '',
+      version: 2
+    })
   }, {
     autoMigrate: options.autoMigrate,
     onMigrationProgress: options.onMigrationProgress || print
