b7fc9e851d5e1624c80bef8eef7b27b8061f217d
Alex Potsides <alex@achingbrain.net>
2021-08-09 16:08:25 +0100

    docs: update files.rm example (#3797)
    The argumuent type for multiple files was wrong.
    
    Closes #3794

--- packages/ipfs-core-types/src/files/index.d.ts
+++ packages/ipfs-core-types/src/files/index.d.ts
@@ -79,7 +79,7 @@ export interface API<OptionExtension = {}> {
    * await ipfs.files.rm('/my/beautiful/file.txt')
    *
    * // To remove multiple files
-   * await ipfs.files.rm('/my/beautiful/file.txt', '/my/other/file.txt')
+   * await ipfs.files.rm(['/my/beautiful/file.txt', '/my/other/file.txt'])
    *
    * // To remove a directory
    * await ipfs.files.rm('/my/beautiful/directory', { recursive: true })
