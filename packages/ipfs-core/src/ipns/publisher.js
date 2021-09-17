57587c969402cf3b0cc1beeb3204fb8078d9ea2c
achingbrain <alex@achingbrain.net>
2021-09-17 17:06:04 +0200

    fix: use Key.asKey instead of instanceOf
    Using `instanceOf` is not reliable since instances of classes can
    cross the CLS/ESM boundary and/or come from different browser bundles.
    
    Instead see if we can treat what we've been handed as a `Key` by
    using the `Key.asKey` method.
    
    Fixes #3852

--- packages/ipfs-core/src/ipns/publisher.js
+++ packages/ipfs-core/src/ipns/publisher.js
@@ -92,7 +92,9 @@ class IpnsPublisher {
    * @param {IPNSEntry} entry
    */
   async _publishEntry (key, entry) {
-    if (!(key instanceof Key)) {
+    const k = Key.asKey(key)
+
+    if (!k) {
       const errMsg = 'datastore key does not have a valid format'
 
       log.error(errMsg)
@@ -112,12 +114,12 @@ class IpnsPublisher {
 
     // Add record to routing (buffer key)
     try {
-      const res = await this._routing.put(key.uint8Array(), entryData)
-      log(`ipns record for ${uint8ArrayToString(key.uint8Array(), 'base64')} was stored in the routing`)
+      const res = await this._routing.put(k.uint8Array(), entryData)
+      log(`ipns record for ${uint8ArrayToString(k.uint8Array(), 'base64')} was stored in the routing`)
 
       return res
     } catch (err) {
-      const errMsg = `ipns record for ${uint8ArrayToString(key.uint8Array(), 'base64')} could not be stored in the routing`
+      const errMsg = `ipns record for ${uint8ArrayToString(k.uint8Array(), 'base64')} could not be stored in the routing`
       log.error(errMsg)
       log.error(err)
 
@@ -130,7 +132,9 @@ class IpnsPublisher {
    * @param {PublicKey} publicKey
    */
   async _publishPublicKey (key, publicKey) {
-    if (!(key instanceof Key)) {
+    const k = Key.asKey(key)
+
+    if (!k) {
       const errMsg = 'datastore key does not have a valid format'
       log.error(errMsg)
 
@@ -146,12 +150,12 @@ class IpnsPublisher {
 
     // Add public key to routing (buffer key)
     try {
-      const res = await this._routing.put(key.uint8Array(), publicKey.bytes)
+      const res = await this._routing.put(k.uint8Array(), publicKey.bytes)
       log(`public key for ${uint8ArrayToString(key.uint8Array(), 'base64')} was stored in the routing`)
 
       return res
     } catch (err) {
-      const errMsg = `public key for ${uint8ArrayToString(key.uint8Array(), 'base64')} could not be stored in the routing`
+      const errMsg = `public key for ${uint8ArrayToString(k.uint8Array(), 'base64')} could not be stored in the routing`
       log.error(errMsg)
       log.error(err)
 
