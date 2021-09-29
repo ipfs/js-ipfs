de7616a1e18b085cfd162afc66bc63eb4ce12c59
achingbrain <alex@achingbrain.net>
2021-09-29 09:35:49 +0100

    chore: validate first stream item outside of body loop

--- packages/ipfs-core-utils/src/multipart-request.node.js
+++ packages/ipfs-core-utils/src/multipart-request.node.js
@@ -5,6 +5,7 @@ import mergeOpts from 'merge-options'
 // @ts-expect-error no types
 import toStream from 'it-to-stream'
 import debug from 'debug'
+import itPeekable from 'it-peekable'
 
 const merge = mergeOpts.bind({ ignoreUndefined: true })
 const log = debug('ipfs:core-utils:multipart-request')
@@ -27,8 +28,8 @@ export async function multipartRequest (source, abortController, headers = {}, b
     try {
       let index = 0
 
-      // @ts-ignore wrong input type for normaliseInput
-      for await (const { content, path, mode, mtime } of normaliseInput(source)) {
+      // @ts-ignore
+      for await (const { content, path, mode, mtime } of source) {
         let fileSuffix = ''
         const type = content ? 'file' : 'dir'
 
@@ -79,12 +80,26 @@ export async function multipartRequest (source, abortController, headers = {}, b
     }
   }
 
+  // peek at the first value in order to get the input stream moving
+  // and to validate its contents.
+  // We cannot do this in the `for await..of` in streamFiles due to
+  // https://github.com/node-fetch/node-fetch/issues/753
+  const peekable = itPeekable(normaliseInput(source))
+
+  /** @type {any} value **/
+  const { value, done } = await peekable.peek()
+
+  if (!done) {
+    peekable.push(value)
+  }
+
   return {
     parts: null,
     total: -1,
     headers: merge(headers, {
       'Content-Type': `multipart/form-data; boundary=${boundary}`
     }),
-    body: await toStream(streamFiles(source))
+    // @ts-expect-error normaliseInput returns unixfs importer import candidates
+    body: toStream(streamFiles(peekable))
   }
 }
