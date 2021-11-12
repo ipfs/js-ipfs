605c83b3177507ffcbc2ba0097ffa640815b13a3
achingbrain <alex@achingbrain.net>
2021-11-12 14:11:36 +0000

    chore: updated versions

--- packages/ipfs-core/src/version.js
+++ packages/ipfs-core/src/version.js
@@ -1,4 +1,4 @@
 
-export const ipfsCore = '0.11.1'
-export const commit = ''
-export const interfaceIpfsCore = '^0.151.1'
+export const ipfsCore = '0.12.0'
+export const commit = '568aeadd8f308cc230de06a956c1a9e6102094b6'
+export const interfaceIpfsCore = '^0.152.0'
--- packages/ipfs/src/package.js
+++ packages/ipfs/src/package.js
@@ -1,4 +1,4 @@
 
 export const name = 'ipfs'
-export const version = '0.59.1'
+export const version = '0.60.0'
 export const node = '>=14.0.0'
