970211f9ad707f3e61d5bdb456d1c9b917f90c0b
sevenrats <sevenrats@tutanota.com>
2022-12-28 12:03:21 -0500

    improve dockerfiles

---
+++ entrypoint.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# Proxy signals
+_term() { 
+  echo "Caught SIGTERM signal!"
+  pkill -TERM jsipfs
+  pkill -TERM -P1
+  exit 0
+}
+
+trap _term SIGTERM
+
+export IPFS_PATH=$IPFS_PATH
+
+if ! [[ -f "$IPFS_PATH" ]]; then
+    jsipfs init    
+fi
+
+jsipfs daemon &
+wait -n ${!}
