d324a3208e5bb5157d1616efda9fe5be04c2c42a
sevenrats <sevenrats@tutanota.com>
2023-01-07 22:47:26 -0500

    handle stale repo locks

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
+if [[ -d "$IPFS_PATH/repo.lock" ]]; then
+    echo "Automatically removing a stale repo lock."
+    rm -rf $IPFS_PATH/repo.lock    
+fi
 
 if ! [[ -f "$IPFS_PATH" ]]; then
     jsipfs init    
