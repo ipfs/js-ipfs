05cc62075f80cbb1fc0470cf31a185c7ebecfb6e
sevenrats <sevenrats@tutanota.com>
2023-01-07 21:17:13 -0500

    no need to manually proxy signals

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,17 +1,5 @@
 #!/usr/bin/env bash
 
-# Proxy signals
-_term() { 
-  echo "Caught SIGTERM signal!"
-  pkill -TERM jsipfs
-  pkill -TERM -P1
-  exit 0
-}
-
-trap _term SIGTERM
-
-export IPFS_PATH=$IPFS_PATH
-
 if ! [[ -f "$IPFS_PATH" ]]; then
     jsipfs init    
 fi
