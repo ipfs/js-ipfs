0cfcaf65998bdc2af0cc29ac48229bb3bc35c5b8
RangerMauve <RangerMauve@hotmail.com>
2023-01-11 12:24:07 -0500

    fix: allow reading rawLeaves in MFS (#4282)
    If you write to an MFS directory with `rawLeaves: true`, it'll error out when doing a read saying `Error: /example-0/example.txt was not a file`.
    
    This accounts for that case by handling the `raw` type the same as a file.
    
    Co-authored-by: Mauve Signweaver <RangerMauve@mauve.moe>
    Co-authored-by: Alex Potsides <alex@achingbrain.net>

--- packages/interface-ipfs-core/src/files/read.js
+++ packages/interface-ipfs-core/src/files/read.js
@@ -112,6 +112,15 @@ export function testRead (factory, options) {
       expect(testFileData).to.eql(fixtures.smallFile.data)
     })
 
+    it('should be able to read rawLeaves files', async () => {
+      const { cid } = await ipfs.add(fixtures.smallFile.data, {
+        rawLeaves: true
+      })
+      await ipfs.files.cp(`/ipfs/${cid}`, '/raw-leaves.txt')
+      const testFileData = uint8ArrayConcat(await all(ipfs.files.read('/raw-leaves.txt')))
+      expect(testFileData).to.eql(fixtures.smallFile.data)
+    })
+
     describe('with sharding', () => {
       /** @type {import('ipfs-core-types').IPFS} */
       let ipfs
